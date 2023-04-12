480515b324a3a848811c8054294d78b14bbf5e7f
Raphile14 <rllddalangin@gmail.com>
2023-04-12 17:52:18 +0800

    feat: created User.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/domain/model/User.kt
@@ -0,0 +1,72 @@
+package com.chatchatabc.parking.domain.model
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.*
+import lombok.Data
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.Date
+
+@Data
+@Entity
+@Table(name = "users")
+open class User : UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    open lateinit var id: String
+
+    @Column(unique = true)
+    open lateinit var email: String
+
+    @Column(unique = true)
+    private lateinit var username: String
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private lateinit var password: String
+
+    @CreationTimestamp
+    open lateinit var createdAt: Date
+
+    @UpdateTimestamp
+    open lateinit var updatedAt: Date
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = [JoinColumn(name = "user_id")],
+        inverseJoinColumns = [JoinColumn(name = "role_id")]
+    )
+    open var roles: MutableList<Role> = mutableListOf()
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return this.roles.stream().map { role -> role as GrantedAuthority }.toList().toMutableList()
+    }
+
+    override fun getPassword(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUsername(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
