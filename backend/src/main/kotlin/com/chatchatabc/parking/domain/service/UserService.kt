8cd9ca8d3ce64a89145746d5900ca45d8a222b06
Raphile14 <rllddalangin@gmail.com>
2023-04-13 10:22:14 +0800

    feat: created register service for User

--- backend/src/main/kotlin/com/chatchatabc/parking/domain/service/UserService.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/domain/service/UserService.kt
@@ -1,7 +1,14 @@
 package com.chatchatabc.parking.domain.service
 
+import com.chatchatabc.parking.domain.model.RoleNames
+import com.chatchatabc.parking.domain.model.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-interface UserService {
+interface UserService : UserDetailsService {
+    /**
+     * Register a new user
+     */
+    fun register(user: User, roleName: RoleNames): User
 }
--- backend/src/main/kotlin/com/chatchatabc/parking/impl/domain/service/UserServiceImpl.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/impl/domain/service/UserServiceImpl.kt
@@ -1,8 +1,37 @@
 package com.chatchatabc.parking.impl.domain.service
 
+import com.chatchatabc.parking.domain.model.RoleNames
+import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.parking.domain.repository.RoleRepository
+import com.chatchatabc.parking.domain.repository.UserRepository
 import com.chatchatabc.parking.domain.service.UserService
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
-class UserServiceImpl : UserService {
+class UserServiceImpl (
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val roleRepository: RoleRepository
+) : UserService {
+    /**
+     * Register a new user
+     */
+    override fun register(user: User, roleName: RoleNames): User {
+        // Get Role
+        val role = roleRepository.findByName(roleName.name)
+        // Encode Password
+        user.password = passwordEncoder.encode(user.password)
+        // Set Role
+        user.roles.add(role.get())
+        return userRepository.save(user)
+    }
+
+    /**
+     * Load user by username for Login
+     */
+    override fun loadUserByUsername(username: String?): UserDetails {
+        TODO("Not yet implemented")
+    }
 }
