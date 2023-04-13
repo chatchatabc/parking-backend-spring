fc54c0ea94a9b9afca3977698e7fcda592972837
Raphile14 <rllddalangin@gmail.com>
2023-04-13 11:49:18 +0800

    feat: created corresponding service functions for Vehicle

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/impl/domain/service/VehicleServiceImpl.kt
@@ -0,0 +1,36 @@
+package com.chatchatabc.parking.impl.domain.service
+
+import com.chatchatabc.parking.domain.model.Vehicle
+import com.chatchatabc.parking.domain.repository.UserRepository
+import com.chatchatabc.parking.domain.repository.VehicleRepository
+import com.chatchatabc.parking.domain.service.VehicleService
+import org.springframework.stereotype.Service
+
+@Service
+class VehicleServiceImpl (
+    private val vehicleRepository: VehicleRepository,
+    private val userRepository: UserRepository
+) : VehicleService {
+    /**
+     * Register a vehicle to a user
+     */
+    override fun register(userId: String, vehicle: Vehicle): Vehicle {
+        val user = userRepository.findById(userId).get()
+        vehicle.user = user
+        return vehicleRepository.save(vehicle)
+    }
+
+    /**
+     * Update a vehicle's information
+     */
+    override fun update(userId: String, vehicleId: String, newVehicleInfo: Vehicle): Vehicle {
+        val user = userRepository.findById(userId).get()
+        val vehicle = vehicleRepository.findByIdAndUser(vehicleId, user).get()
+
+        if (newVehicleInfo.plateNumber != null) {
+            vehicle.plateNumber = newVehicleInfo.plateNumber
+        }
+
+        return vehicleRepository.save(vehicle)
+    }
+}
