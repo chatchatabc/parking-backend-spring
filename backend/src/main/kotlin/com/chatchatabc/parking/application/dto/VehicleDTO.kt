01460620f310aec9cacc9c88bc44830e8df47650
Raphile14 <rllddalangin@gmail.com>
2023-04-13 11:48:50 +0800

    feat: created VehicleDTO.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/dto/VehicleDTO.kt
@@ -0,0 +1,12 @@
+package com.chatchatabc.parking.application.dto
+
+import com.chatchatabc.parking.domain.model.Vehicle
+
+data class VehicleRegisterRequest(
+    val plateNumber: String
+)
+
+data class VehicleResponse(
+    val vehicle: Vehicle?,
+    val error: ErrorContent?
+)
