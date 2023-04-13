dbf5404a7a2c36b87b361bfd5f4f8268e988acc2
Raphile14 <rllddalangin@gmail.com>
2023-04-13 10:36:02 +0800

    feat: added findByUsername to UserRepository.kt

--- backend/src/main/kotlin/com/chatchatabc/parking/domain/repository/UserRepository.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/domain/repository/UserRepository.kt
@@ -3,7 +3,13 @@ package com.chatchatabc.parking.domain.repository
 import com.chatchatabc.parking.domain.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, String> {
+
+    /**
+     * Find a user by username
+     */
+    fun findByUsername(username: String): Optional<User>
 }
