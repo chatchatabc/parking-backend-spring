cc17ccf5ba85653a2456b7470d2dff9d2c239d55
Raphile14 <rllddalangin@gmail.com>
2023-04-13 10:36:30 +0800

    feat: implemented user login api for AuthController.kt

--- backend/src/main/kotlin/com/chatchatabc/parking/application/rest/AuthController.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/rest/AuthController.kt
@@ -1,14 +1,20 @@
 package com.chatchatabc.parking.application.rest
 
 import com.chatchatabc.parking.application.dto.ErrorContent
+import com.chatchatabc.parking.application.dto.UserLoginRequest
 import com.chatchatabc.parking.application.dto.UserRegisterRequest
 import com.chatchatabc.parking.application.dto.UserResponse
+import com.chatchatabc.parking.application.rest.service.JwtService
 import com.chatchatabc.parking.domain.model.RoleNames
 import com.chatchatabc.parking.domain.model.User
 import com.chatchatabc.parking.domain.repository.UserRepository
 import com.chatchatabc.parking.domain.service.UserService
 import org.modelmapper.ModelMapper
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,9 +24,40 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/auth")
 class AuthController(
     private val userRepository: UserRepository,
-    private val userService: UserService
+    private val userService: UserService,
+    private val authenticationManager: AuthenticationManager,
+    private val jwtService: JwtService
 ) {
     private val mapper = ModelMapper()
+
+    @PostMapping("/login")
+    fun login(
+        @RequestBody request: UserLoginRequest
+    ): ResponseEntity<UserResponse> {
+        return try {
+            val queriedUser = userRepository.findByUsername(request.username)
+            if (queriedUser.isEmpty) {
+                throw Exception("User not found")
+            }
+
+            // Authenticate user
+            authenticationManager.authenticate(
+                UsernamePasswordAuthenticationToken(
+                    request.username,
+                    request.password
+                )
+            )
+
+            val token: String = jwtService.generateToken(queriedUser.get())
+            val headers = HttpHeaders()
+            headers.set("X-Access-Token", token)
+            ResponseEntity.ok().headers(headers).body(UserResponse(queriedUser.get(), null))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(UserResponse(null, ErrorContent("Login Error", e.message ?: "Unknown Error")))
+        }
+    }
 
     /**
      * Register a new user
