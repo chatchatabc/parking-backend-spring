c40e6925c1db1e061ce0fc2e17427b238f189f84
Raphile14 <rllddalangin@gmail.com>
2023-04-13 10:22:42 +0800

    improvement: added init roles to DataInit.kt

--- backend/src/main/kotlin/com/chatchatabc/parking/infra/DataInit.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/infra/DataInit.kt
@@ -1,12 +1,15 @@
 package com.chatchatabc.parking.infra
 
+import com.chatchatabc.parking.domain.service.RoleService
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
-class DataInit : CommandLineRunner {
+class DataInit (
+    private val roleService: RoleService
+) : CommandLineRunner {
     private val log = LoggerFactory.getLogger(DataInit::class.java)
 
     /**
@@ -15,6 +18,7 @@ class DataInit : CommandLineRunner {
     @Transactional
     override fun run(vararg args: String?) {
         log.info("Initializing data...")
+        roleService.initRoles()
         log.info("Data initialized.")
     }
 }
