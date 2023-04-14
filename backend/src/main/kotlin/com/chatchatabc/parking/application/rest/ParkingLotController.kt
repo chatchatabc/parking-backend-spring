25b7d9a4891ad915e3b990bd9713934d3908baba
Raphile14 <rllddalangin@gmail.com>
2023-04-14 15:04:18 +0800

    feat: implemented get parking lot by id and distance using Haversine formula

--- backend/src/main/kotlin/com/chatchatabc/parking/application/rest/ParkingLotController.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/rest/ParkingLotController.kt
@@ -9,8 +9,6 @@ import com.chatchatabc.parking.domain.model.User
 import com.chatchatabc.parking.domain.repository.ParkingLotRepository
 import com.chatchatabc.parking.domain.service.ParkingLotService
 import org.modelmapper.ModelMapper
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
@@ -26,11 +24,45 @@ class ParkingLotController(
     /**
      * Get parking lots by pageable
      */
-    @GetMapping("/get")
+    @GetMapping("/get/{parkingLotId}")
     fun get(
-        pageable: Pageable
-    ): ResponseEntity<Page<ParkingLot>> {
-        return ResponseEntity.ok(parkingLotRepository.findAll(pageable))
+        @PathVariable parkingLotId: String
+    ): ResponseEntity<ParkingLotResponse> {
+        return try {
+            val parkingLot = parkingLotRepository.findById(parkingLotId)
+            if (parkingLot.isEmpty) {
+                throw Exception("Parking Lot Not Found")
+            }
+            ResponseEntity.ok(ParkingLotResponse(parkingLot.get(), null))
+        } catch (e: Exception) {
+            ResponseEntity.ok(
+                ParkingLotResponse(
+                    null,
+                    ErrorContent("Get Parking By ID Error", e.message ?: "Unknown Error")
+                )
+            )
+        }
+    }
+
+    /**
+     * Get parking lots by distance
+     */
+    @GetMapping("/get-by-location")
+    fun getByLocation(
+        @RequestParam("longitude") longitude: Double,
+        @RequestParam("latitude") latitude: Double,
+        @RequestParam("distance") distance: Double,
+    ): ResponseEntity<List<ParkingLot>> {
+        println("longitude: $longitude")
+        println("latitude: $latitude")
+        var inputDistance = distance
+        // Place a cap on the range, distance should not exceed 0.1km
+        if (distance >= 0.1) {
+            inputDistance = 0.1
+        }
+        return ResponseEntity.ok(
+            parkingLotRepository.findByDistance(longitude, latitude, inputDistance)
+        )
     }
 
     /**
--- backend/src/main/kotlin/com/chatchatabc/parking/domain/repository/ParkingLotRepository.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/domain/repository/ParkingLotRepository.kt
@@ -15,4 +15,10 @@ interface ParkingLotRepository : JpaRepository<ParkingLot, String> {
      */
     @Query("SELECT p FROM ParkingLot p WHERE p.id = ?1 AND p.owner = ?2")
     fun findByIdAndOwner(parkingLotId: String, owner: User): Optional<ParkingLot>
+
+    /**
+     * Find parking lots by distance using Haversine formula
+     */
+    @Query("SELECT p FROM ParkingLot p WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(p.latitude)) * cos(radians(p.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(p.latitude)))) <= :distance")
+    fun findByDistance(longitude: Double, latitude: Double, distance: Double): List<ParkingLot>
 }
