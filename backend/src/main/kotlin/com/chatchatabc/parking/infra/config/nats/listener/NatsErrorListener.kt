a941393a40fb5b482a79525b68b3b9bf9c2ded88
Raphile14 <rllddalangin@gmail.com>
2023-04-14 13:20:11 +0800

    feat: created and implemented NatsErrorListener.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/infra/config/nats/listener/NatsErrorListener.kt
@@ -0,0 +1,21 @@
+package com.chatchatabc.parking.infra.config.nats.listener
+
+import io.nats.client.Connection
+import io.nats.client.ErrorListener
+import org.slf4j.LoggerFactory
+
+class NatsErrorListener : ErrorListener {
+    private val log = LoggerFactory.getLogger(NatsErrorListener::class.java)
+
+    fun errorOccured(conn: Connection?, exp: Exception?) {
+        log.error("The server notificed the client with: $exp")
+    }
+
+    fun exceptionOccured(conn: Connection?, exp: Exception?) {
+        log.error("The connection handled an exception: ${exp!!.localizedMessage}")
+    }
+
+    fun slowConsumerDetected(conn: Connection?, consumer: String?) {
+        log.error("The server detected a slow consumer: $consumer")
+    }
+}
