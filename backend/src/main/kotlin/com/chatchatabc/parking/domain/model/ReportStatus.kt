2c0f9afcac98b8c767b9a01eda0847bcc2f90785
Raphile14 <rllddalangin@gmail.com>
2023-04-19 11:00:54 +0800

    feat: created ReportStatus.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/domain/model/ReportStatus.kt
@@ -0,0 +1,34 @@
+package com.chatchatabc.parking.domain.model
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
+import lombok.Data
+import org.hibernate.annotations.CreationTimestamp
+import java.util.*
+
+@Data
+@Entity
+@Table(name = "report_statuses")
+open class ReportStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    open lateinit var id: String
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "report_id")
+    open lateinit var report: Report
+
+    @ManyToOne
+    @JoinColumn(name = "performed_by")
+    open lateinit var performedBy: User
+
+    @Column
+    open var status: Int = 0
+
+    @Column
+    open var remarks: String? = null
+
+    @CreationTimestamp
+    open lateinit var createdAt: Date
+}
