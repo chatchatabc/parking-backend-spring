bb3b26a43210e6a2d7f8b4d0cd8017c32afb56e5
Raphile14 <rllddalangin@gmail.com>
2023-04-19 16:23:47 +0800

    feat: created AuthParkingController to centralize apis regarding parking authentication

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/rest/AuthParkingController.kt
@@ -0,0 +1,58 @@
+package com.chatchatabc.parking.application.rest
+
+import com.chatchatabc.parking.application.dto.*
+import com.chatchatabc.parking.application.rest.service.JwtService
+import com.chatchatabc.parking.domain.service.UserService
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth/parking")
+class AuthParkingController(
+    private val userService: UserService,
+    private val jwtService: JwtService,
+) {
+    /**
+     * Login with phone number
+     */
+    @PostMapping("/login")
+    fun loginWithPhone(
+        @RequestBody request: UserParkingPhoneLoginRequest
+    ): ResponseEntity<UserParkingPhoneLoginResponse> {
+        return try {
+            // Check if user is fully registered
+            val isRegistered = userService.checkIfUserIsFullyRegistered(request.phone)
+            userService.createOTPAndSendSMS(request.phone)
+            ResponseEntity.ok().body(UserParkingPhoneLoginResponse(isRegistered, null))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(UserParkingPhoneLoginResponse(null, ErrorContent("Login Error", e.message ?: "Unknown Error")))
+        }
+    }
+
+    /**
+     * Verify OTP
+     */
+    @PostMapping("/verify")
+    fun verifyOTP(
+        @RequestBody request: UserParkingVerifyRequest
+    ): ResponseEntity<UserResponse> {
+        return try {
+            val headers = HttpHeaders()
+            val user = userService.parkingVerifyOTP(request.phone, request.otp)
+            val token: String = jwtService.generateToken(user)
+            headers.set("X-Access-Token", token)
+            ResponseEntity.ok().headers(headers).body(UserResponse(user, null))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(UserResponse(null, ErrorContent("OTP Verify Error", e.message ?: "Unknown Error")))
+        }
+    }
+}
