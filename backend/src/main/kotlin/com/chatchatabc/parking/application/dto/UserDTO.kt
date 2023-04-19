30fbdced6e0df37022601603371ac01fbc4ce932
Raphile14 <rllddalangin@gmail.com>
2023-04-19 16:21:05 +0800

    feat: created new DTOs for UserDTO.kt

--- backend/src/main/kotlin/com/chatchatabc/parking/application/dto/UserDTO.kt
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/dto/UserDTO.kt
@@ -13,6 +13,20 @@ data class UserLoginRequest(
     val password: String
 )
 
+data class UserParkingPhoneLoginRequest(
+    val phone: String
+)
+
+data class UserParkingPhoneLoginResponse(
+    val isRegistered: Boolean?,
+    val error: ErrorContent?
+)
+
+data class UserParkingVerifyRequest(
+    val phone: String,
+    val otp: String
+)
+
 data class UserResponse(
     val user: User?,
     val error: ErrorContent?
