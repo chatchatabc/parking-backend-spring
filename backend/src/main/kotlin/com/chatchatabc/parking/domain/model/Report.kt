f182cae11ec0d4021866fa433efa6bf1ec47b517
Raphile14 <rllddalangin@gmail.com>
2023-04-19 11:00:07 +0800

    feat: created Report.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/domain/model/Report.kt
@@ -0,0 +1,38 @@
+package com.chatchatabc.parking.domain.model
+
+import jakarta.persistence.*
+import lombok.Data
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.util.*
+
+@Data
+@Entity
+@Table(name = "reports")
+open class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    open lateinit var id: String
+
+    @Column
+    open lateinit var name: String
+
+    @Column
+    open lateinit var description: String
+
+    @ManyToOne
+    @JoinColumn(name = "reported_by")
+    open lateinit var reportedBy: User
+
+    @Column
+    open var cancelledAt: Date? = null
+
+    @OneToMany(mappedBy = "report", cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
+    open var reportStatuses: MutableList<ReportStatus> = mutableListOf()
+
+    @CreationTimestamp
+    open lateinit var createdAt: Date
+
+    @UpdateTimestamp
+    open lateinit var updatedAt: Date
+}
