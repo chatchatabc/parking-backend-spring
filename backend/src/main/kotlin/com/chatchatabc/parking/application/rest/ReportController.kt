56e5047a05cb8d9c584c12d2e28067479ad5b8fc
Raphile14 <rllddalangin@gmail.com>
2023-04-20 10:23:49 +0800

    feat: created initial ReportController.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/rest/ReportController.kt
@@ -0,0 +1,18 @@
+package com.chatchatabc.parking.application.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/report")
+class ReportController {
+
+    // TODO: Create Report
+    fun createReport() {
+
+    }
+
+    // TODO: View Reports
+
+    // TODO: Create Report Statuses
+}
