f61040e72960a589bec0018fe08d09c262396461
Raphile14 <rllddalangin@gmail.com>
2023-04-20 14:10:07 +0800

    refactor: JwtServiceImpl.kt now uses interface from api module

--- backend/parking-service/src/main/kotlin/com/chatchatabc/parking/impl/application/rest/service/JwtServiceImpl.kt
+++ backend/parking-service/src/main/kotlin/com/chatchatabc/parking/impl/application/rest/service/JwtServiceImpl.kt
@@ -3,9 +3,10 @@ package com.chatchatabc.parking.impl.application.rest.service
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
-import com.chatchatabc.parking.application.rest.service.JwtService
-import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.api.application.dto.user.UserDTO
+import com.chatchatabc.api.application.rest.service.JwtService
 import com.chatchatabc.parking.domain.repository.UserRepository
+import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.*
@@ -22,11 +23,12 @@ class JwtServiceImpl(
 ) : JwtService {
     private val hmac512: Algorithm = Algorithm.HMAC512(secret)
     private val verifier: JWTVerifier = JWT.require(hmac512).build()
+    private val modelMapper = ModelMapper()
 
     /**
      * Generate Token
      */
-    override fun generateToken(user: User): String {
+    override fun generateToken(user: UserDTO): String? {
         return JWT.create()
             .withSubject(user.id)
             .withExpiresAt(Date(System.currentTimeMillis() + expiration.toLong()))
@@ -36,14 +38,14 @@ class JwtServiceImpl(
     /**
      * Validate Token and Get User
      */
-    override fun validateTokenAndGetUser(token: String): User? {
+    override fun validateTokenAndGetUser(token: String): UserDTO? {
         return try {
             val subject = verifier.verify(token).subject
             val user = userRepository.findById(subject)
             if (user.isEmpty) {
                 throw Exception("Invalid Token")
             }
-            user.get()
+            modelMapper.map(user.get(), UserDTO::class.java)
         } catch (e: Exception) {
             return null
         }
