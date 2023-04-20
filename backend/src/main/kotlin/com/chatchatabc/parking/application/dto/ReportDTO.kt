d8eba467c1c5a35fc8a6322a248dd237d611abd4
Raphile14 <rllddalangin@gmail.com>
2023-04-20 10:23:04 +0800

    feat: created ReportDTO.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/dto/ReportDTO.kt
@@ -0,0 +1,16 @@
+package com.chatchatabc.parking.application.dto
+
+import com.chatchatabc.parking.domain.model.Report
+
+data class ReportCreateRequest(
+    val name: String,
+    val description: String,
+    val plateNumber: String,
+    val latitude: Double,
+    val longitude: Double,
+)
+
+data class ReportResponse(
+    val report: Report?,
+    val errorContent: ErrorContent?
+)
