2d55a5d5370850c0fba6d390af3f8d2e44c039aa
Raphile14 <rllddalangin@gmail.com>
2023-04-20 10:52:58 +0800

    feat: created module for parking user

---
+++ backend/parking-user/src/main/kotlin/com/chatchatabc/parkinguser/ParkingUserApplication.kt
@@ -0,0 +1,11 @@
+package com.chatchatabc.parkinguser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParkingUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParkingUserApplication>(*args)
+}
---
+++ backend/parking-user/src/test/kotlin/com/chatchatabc/parkinguser/ParkingUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chatchatabc.parkinguser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkingUserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
