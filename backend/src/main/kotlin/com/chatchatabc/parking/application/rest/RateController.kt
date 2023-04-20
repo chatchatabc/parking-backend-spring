4c608ebfec4de8cc42716c02d5ed9ceac7f6eff4
Raphile14 <rllddalangin@gmail.com>
2023-04-20 10:25:15 +0800

    feat: created initial RateController.kt

---
+++ backend/src/main/kotlin/com/chatchatabc/parking/application/rest/RateController.kt
@@ -0,0 +1,14 @@
+package com.chatchatabc.parking.application.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/rate")
+class RateController {
+    // TODO: Create Rate
+
+    // TODO: Get Rate
+
+    // TODO: Update Rate
+}
