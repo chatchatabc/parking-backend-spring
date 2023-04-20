14ac835495d1700fe9fec5b71f363817d15296cb
Raphile14 <rllddalangin@gmail.com>
2023-04-20 16:12:43 +0800

    chore: enabled dubbo on main class file

--- backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/ParkingManagementApplication.kt
+++ backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/ParkingManagementApplication.kt
@@ -1,9 +1,11 @@
 package com.chatchatabc.parkingmanagement
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableDubbo
 class ParkingManagementApplication
 
 fun main(args: Array<String>) {
