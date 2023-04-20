0e14934a19165fd1080526a579a2a15d849da865
Raphile14 <rllddalangin@gmail.com>
2023-04-20 11:02:22 +0800

    feat: created module for parking admin

---
+++ backend/parking-admin/src/main/kotlin/com/chatchatabc/parkingadmin/ParkingAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.chatchatabc.parkingadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParkingAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<ParkingAdminApplication>(*args)
+}
