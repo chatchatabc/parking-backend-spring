3b1d7815e51c6c04f9c67f1282176ca2aae22b41
Raphile14 <rllddalangin@gmail.com>
2023-04-20 10:44:14 +0800

    feat: created module for parking management

---
+++ backend/parking-management/src/test/kotlin/com/chatchatabc/parkingmanagement/ParkingManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chatchatabc.parkingmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
