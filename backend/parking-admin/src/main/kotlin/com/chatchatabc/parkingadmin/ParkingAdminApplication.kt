650acbea2e67b36f66ec1c332ec12974bb934d3a
Raphile14 <rllddalangin@gmail.com>
2023-04-20 16:35:38 +0800

    feat: security config for parking-admin

--- backend/parking-admin/src/main/kotlin/com/chatchatabc/parkingadmin/ParkingAdminApplication.kt
+++ backend/parking-admin/src/main/kotlin/com/chatchatabc/parkingadmin/ParkingAdminApplication.kt
@@ -1,9 +1,11 @@
 package com.chatchatabc.parkingadmin
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableDubbo
 class ParkingAdminApplication
 
 fun main(args: Array<String>) {
