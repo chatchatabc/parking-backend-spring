9e5f2883b44caa1a41afb9bfe34d69d7f2fe2a03
Raphile14 <rllddalangin@gmail.com>
2023-04-24 10:51:38 +0800

    feat: implemented user update profile api

--- backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/application/rest/UserController.kt
+++ backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/application/rest/UserController.kt
@@ -2,15 +2,14 @@ package com.chatchatabc.parkingmanagement.application.rest
 
 import com.chatchatabc.api.application.dto.ErrorContent
 import com.chatchatabc.api.application.dto.user.UserDTO
+import com.chatchatabc.api.application.dto.user.UserProfileUpdateRequest
 import com.chatchatabc.api.application.dto.user.UserResponse
 import com.chatchatabc.api.domain.service.UserService
 import org.apache.dubbo.config.annotation.DubboReference
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/user")
@@ -32,33 +31,28 @@ class UserController(
         }
     }
 
-//    /**
-//     * Update user
-//     */
-//    @PutMapping("/update")
-//    fun updateUser(
-//        @RequestBody request: com.chatchatabc.service.application.dto.UserProfileUpdateRequest
-//    ): ResponseEntity<com.chatchatabc.service.application.dto.UserResponse> {
-//        return try {
-//            // Get principal from security context
-//            val principal = SecurityContextHolder.getContext().authentication.principal as User
-//            val user = userService.updateUser(
-//                principal.id,
-//                request.username,
-//                request.email,
-//                request.firstName,
-//                request.lastName
-//            )
-//            ResponseEntity.ok().body(com.chatchatabc.service.application.dto.UserResponse(user, null))
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                .body(
-//                    com.chatchatabc.service.application.dto.UserResponse(
-//                        null,
-//                        ErrorContent("User Profile Update Error", e.message ?: "Unknown Error")
-//                    )
-//                )
-//        }
-//    }
+    /**
+     * Update user
+     */
+    @PutMapping("/update")
+    fun updateUser(
+        @RequestBody request: UserProfileUpdateRequest
+    ): ResponseEntity<UserResponse> {
+        return try {
+            // Get principal from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as UserDTO
+            val user = userService.updateUser(
+                principal.id,
+                request.username,
+                request.email,
+                request.firstName,
+                request.lastName
+            )
+            ResponseEntity.ok().body(UserResponse(user, null))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(UserResponse(null, ErrorContent("User Profile Update Error", e.message ?: "Unknown Error")))
+        }
+    }
 }
