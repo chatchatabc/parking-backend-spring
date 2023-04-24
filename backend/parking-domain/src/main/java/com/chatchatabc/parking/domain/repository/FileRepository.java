9c4b80a9d3ed617781cb979e3b5837283b8857bf
Raphile14 <rllddalangin@gmail.com>
2023-04-24 15:58:49 +0800

    feat: created FileRepository.java

---
+++ backend/parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/FileRepository.java
@@ -0,0 +1,9 @@
+package com.chatchatabc.parking.domain.repository;
+
+import com.chatchatabc.parking.domain.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+}
