eda2f6b421b49cac763d3b5b473fec5e28626ff3
Raphile14 <rllddalangin@gmail.com>
2023-04-24 10:46:53 +0800

    feat: implemented user get profile api

--- backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/application/rest/UserController.kt
+++ backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/application/rest/UserController.kt
@@ -1,11 +1,37 @@
 package com.chatchatabc.parkingmanagement.application.rest
 
+import com.chatchatabc.api.application.dto.ErrorContent
+import com.chatchatabc.api.application.dto.user.UserDTO
+import com.chatchatabc.api.application.dto.user.UserResponse
+import com.chatchatabc.api.domain.service.UserService
+import org.apache.dubbo.config.annotation.DubboReference
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/user")
-class UserController {
+class UserController(
+    @DubboReference
+    private val userService: UserService
+) {
+    @GetMapping("/me")
+    fun getProfile(): ResponseEntity<UserResponse> {
+        return try {
+            // Get ID from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as UserDTO
+            val user = userService.getUser(principal.id)
+            ResponseEntity.ok().body(UserResponse(user, null))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(UserResponse(null, ErrorContent("User Profile Get Error", e.message ?: "Unknown Error")))
+        }
+    }
+
 //    /**
 //     * Update user
 //     */
