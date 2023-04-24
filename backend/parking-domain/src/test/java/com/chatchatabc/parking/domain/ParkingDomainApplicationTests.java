919f8f4f3b12b2261c9404bb738485d39d325a63
Raphile14 <rllddalangin@gmail.com>
2023-04-24 14:06:12 +0800

    feat: created module for parking domain

---
+++ backend/parking-domain/src/test/java/com/chatchatabc/parking/domain/ParkingDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.parking.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
