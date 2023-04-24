aee91bacc4b0d200b9bb30096a365666ea4e48de
Raphile14 <rllddalangin@gmail.com>
2023-04-24 16:33:43 +0800

    refactor: moved ParkingLotRepository to parking domain

---
+++ backend/parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ParkingLotRepository.java
@@ -0,0 +1,33 @@
+package com.chatchatabc.parking.domain.repository;
+
+import com.chatchatabc.parking.domain.model.ParkingLot;
+import com.chatchatabc.parking.domain.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, String> {
+    /**
+     * Find a parking lot by its id and owner
+     */
+    @Query("SELECT p FROM ParkingLot p WHERE p.id = ?1 AND p.owner = ?2")
+    Optional<ParkingLot> findByIdAndOwner(String id, User owner);
+
+    /**
+     * Find parking lots by distance using Haversine formula
+     */
+    @Query("SELECT p FROM ParkingLot p WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(p.latitude)) * cos(radians(p.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(p.latitude)))) <= :distance")
+    List<ParkingLot> findByDistance(double latitude, double longitude, double distance);
+
+    /**
+     * Find all parking lots by owner
+     */
+    @Query("SELECT p FROM ParkingLot p WHERE p.owner = ?1")
+    Page<ParkingLot> findAllByOwner(User owner, Pageable pageable);
+}
