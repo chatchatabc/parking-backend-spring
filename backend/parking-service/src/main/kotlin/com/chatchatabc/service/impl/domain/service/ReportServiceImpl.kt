f42e2a66f5ca66f6c63b6a3368c13e3635edfeac
Raphile14 <rllddalangin@gmail.com>
2023-04-24 17:53:50 +0800

    refactor: moved ReportServiceImpl to parking domain

--- backend/parking-service/src/main/kotlin/com/chatchatabc/service/impl/domain/service/ReportServiceImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.chatchatabc.service.impl.domain.service
-
-import com.chatchatabc.service.domain.service.ReportService
-import org.springframework.stereotype.Service
-
-@Service
-class ReportServiceImpl : ReportService {
-}
