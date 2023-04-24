fbee2d6d7618d8dcf50ed3fd47bc03d6cf173077
Raphile14 <rllddalangin@gmail.com>
2023-04-24 16:36:54 +0800

    refactor: moved VehicleRepository to parking domain

---
+++ backend/parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleRepository.java
@@ -0,0 +1,26 @@
+package com.chatchatabc.parking.domain.repository;
+
+import com.chatchatabc.parking.domain.model.User;
+import com.chatchatabc.parking.domain.model.Vehicle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, String> {
+    /**
+     * Find a vehicle by its id and user
+     */
+    @Query("SELECT v FROM Vehicle v WHERE v.id = ?1 AND v.owner = ?2")
+    Optional<Vehicle> findByIdAndOwner(String vehicleId, User owner);
+
+    /**
+     * Find all vehicles of a user
+     */
+    @Query("SELECT v FROM Vehicle v WHERE v.owner = ?1")
+    Page<Vehicle> findAllByOwner(User owner, Pageable pageable);
+}
