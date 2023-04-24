82f351e0891645032b97327ea0c830f30a4700ec
Raphile14 <rllddalangin@gmail.com>
2023-04-24 11:23:27 +0800

    feat: implemented register parking lot api

--- backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/application/rest/ParkingLotController.kt
+++ backend/parking-management/src/main/kotlin/com/chatchatabc/parkingmanagement/application/rest/ParkingLotController.kt
@@ -1,11 +1,23 @@
 package com.chatchatabc.parkingmanagement.application.rest
 
+import com.chatchatabc.api.application.dto.ErrorContent
+import com.chatchatabc.api.application.dto.parking_lot.ParkingLotCreateRequest
+import com.chatchatabc.api.application.dto.parking_lot.ParkingLotDTO
+import com.chatchatabc.api.application.dto.parking_lot.ParkingLotResponse
+import com.chatchatabc.api.application.dto.user.UserDTO
+import com.chatchatabc.api.domain.service.ParkingLotService
+import org.apache.dubbo.config.annotation.DubboReference
 import org.modelmapper.ModelMapper
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/parking-lot")
-class ParkingLotController {
+class ParkingLotController(
+    @DubboReference
+    private val parkingLotService: ParkingLotService
+) {
     private val modelMapper = ModelMapper()
 
     /**
@@ -70,36 +82,43 @@ class ParkingLotController {
 //        )
 //    }
 //
-//    /**
-//     * Register a parking lot
-//     */
-//    @PostMapping("/register")
-//    fun register(
-//        @RequestBody request: ParkingLotCreateRequest
-//    ): ResponseEntity<ParkingLotResponse> {
-//        return try {
-//            // Get principal from Security Context
-//            val principal = SecurityContextHolder.getContext().authentication.principal as User
-//            val parkingLot = modelMapper.map(request, ParkingLot::class.java)
-//            val createdParkingLot = parkingLotService.register(principal.id, parkingLot)
-//            val parkingLotDTO = modelMapper.map(createdParkingLot, ParkingLotDTO::class.java)
-//            return ResponseEntity.ok(
-//                ParkingLotResponse(
-//                    parkingLotDTO,
-//                    null
-//                )
-//            )
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            ResponseEntity.badRequest()
-//                .body(
-//                    ParkingLotResponse(
-//                        null,
-//                        ErrorContent("Register Parking Lot Error", e.message ?: "Unknown Error")
-//                    )
-//                )
-//        }
-//    }
+    /**
+     * Register a parking lot
+     */
+    @PostMapping("/register")
+    fun register(
+        @RequestBody req: ParkingLotCreateRequest
+    ): ResponseEntity<ParkingLotResponse> {
+        return try {
+            // Get principal from Security Context
+            val principal = SecurityContextHolder.getContext().authentication.principal as UserDTO
+            val createdParkingLot = parkingLotService.registerParkingLot(
+                principal.id,
+                req.name,
+                req.latitude,
+                req.longitude,
+                req.address,
+                req.description,
+                req.capacity,
+            )
+            val parkingLotDTO = modelMapper.map(createdParkingLot, ParkingLotDTO::class.java)
+            return ResponseEntity.ok(
+                ParkingLotResponse(
+                    parkingLotDTO,
+                    null
+                )
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.badRequest()
+                .body(
+                    ParkingLotResponse(
+                        null,
+                        ErrorContent("Register Parking Lot Error", e.message ?: "Unknown Error")
+                    )
+                )
+        }
+    }
 //
 //    /**
 //     * Update a parking lot
