c2395afe5956bc75bd2463db8d260d41458ddd84
Raphile14 <rllddalangin@gmail.com>
2023-04-24 16:29:02 +0800

    refactor: moved ReportRepository to parking domain

--- backend/parking-service/src/main/kotlin/com/chatchatabc/service/domain/repository/ReportRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.chatchatabc.service.domain.repository
-
-import com.chatchatabc.service.domain.model.Report
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ReportRepository : JpaRepository<Report, String> {
-}
