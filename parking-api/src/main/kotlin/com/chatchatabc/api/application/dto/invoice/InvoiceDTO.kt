0a84fdb80c4b10a8e252cf3f713b42dffd83c589
Raphile14 <rllddalangin@gmail.com>
2023-04-25 14:25:48 +0800

    chore: made the invoice variable be optional in InvoiceDTO.kt

--- parking-api/src/main/kotlin/com/chatchatabc/api/application/dto/invoice/InvoiceDTO.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/api/application/dto/invoice/InvoiceDTO.kt
@@ -4,6 +4,6 @@ import com.chatchatabc.api.application.dto.ErrorContent
 import com.chatchatabc.parking.domain.model.Invoice
 
 data class InvoiceResponse(
-    val invoice: Invoice,
+    val invoice: Invoice?,
     val error: ErrorContent?
 )
