6ead7dad916a0ef25ba24203cf4e27723815504e
Raphile14 <rllddalangin@gmail.com>
2023-04-25 17:10:54 +0800

    feat: added findAllByUser to VehicleRepository

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleRepository.java
@@ -23,4 +23,14 @@ public interface VehicleRepository extends JpaRepository<Vehicle, String> {
      */
     @Query("SELECT v FROM Vehicle v WHERE v.owner = ?1")
     Page<Vehicle> findAllByOwner(User owner, Pageable pageable);
+
+    /**
+     * Find all vehicles of a user by user id
+     *
+     * @param userId   the user id
+     * @param pageable the pageable
+     * @return the page
+     */
+    @Query("SELECT v FROM Vehicle v JOIN v.users u WHERE u.id = :userId")
+    Page<Vehicle> findAllByUser(String userId, Pageable pageable);
 }
