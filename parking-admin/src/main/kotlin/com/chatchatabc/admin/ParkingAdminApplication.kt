67b19980916fde86f7f1a0efd2f227073aa0b357
Raphile14 <rllddalangin@gmail.com>
2023-04-25 11:31:49 +0800

    fix: added component scans to read shared library

--- parking-admin/src/main/kotlin/com/chatchatabc/admin/ParkingAdminApplication.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/admin/ParkingAdminApplication.kt
@@ -1,9 +1,18 @@
 package com.chatchatabc.admin
 
+import com.chatchatabc.parking.DomainConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@ComponentScan("com.chatchatabc.parking")
+@EnableJpaRepositories(basePackages = arrayOf("com.chatchatabc.parking.domain.repository"))
+@EntityScan(basePackages = arrayOf("com.chatchatabc.parking.domain.model"))
+@Import(DomainConfig::class)
 class ParkingAdminApplication
 
 fun main(args: Array<String>) {
--- parking-api/src/main/kotlin/com/chatchatabc/api/ParkingApiApplication.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/api/ParkingApiApplication.kt
@@ -2,10 +2,16 @@ package com.chatchatabc.api
 
 import com.chatchatabc.parking.DomainConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@ComponentScan("com.chatchatabc.parking")
+@EnableJpaRepositories(basePackages = arrayOf("com.chatchatabc.parking.domain.repository"))
+@EntityScan(basePackages = arrayOf("com.chatchatabc.parking.domain.model"))
 @Import(DomainConfig::class)
 class ParkingManagementApplication
 
