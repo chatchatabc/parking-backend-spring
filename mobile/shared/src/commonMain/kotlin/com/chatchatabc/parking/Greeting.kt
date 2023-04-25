70d96e5e80ffa603935b0c270629ee2de9c3ec66
Raphile14 <rllddalangin@gmail.com>
2023-04-25 08:59:20 +0800

    refactor: removed kmm mobile module

--- mobile/shared/src/commonMain/kotlin/com/chatchatabc/parking/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.chatchatabc.parking
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- mobile/shared/src/commonMain/kotlin/com/chatchatabc/parking/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.chatchatabc.parking
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
