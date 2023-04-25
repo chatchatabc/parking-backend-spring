3b59d6e0579cfee4368b1e7804044110ca95e1f2
Raphile14 <rllddalangin@gmail.com>
2023-04-25 16:39:52 +0800

    feat: implemented get vehicle by id

--- parking-api/src/main/kotlin/com/chatchatabc/api/application/rest/VehicleController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/api/application/rest/VehicleController.kt
@@ -8,10 +8,7 @@ import com.chatchatabc.parking.domain.service.VehicleService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/vehicle")
@@ -21,7 +18,30 @@ class VehicleController(
 ) {
     // TODO: Get my vehicles
 
-    // TODO: Get vehicle by id
+    /**
+     * Get a vehicle by id
+     */
+    @GetMapping("/get/{vehicleId}")
+    fun getVehicleById(
+        @PathVariable vehicleId: String
+    ): ResponseEntity<ApiResponse> {
+        return  try {
+            // Get user from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as User
+            val vehicle = vehicleRepository.findById(vehicleId)
+            if (vehicle.isEmpty) {
+                throw Exception("Vehicle not found")
+            }
+            // User should have access to this vehicle. If user is not inside vehicle users array
+            if (vehicle.get().users.find { it.id == principal.id } == null) {
+                throw Exception("User does not have access to this vehicle")
+            }
+
+            ResponseEntity.ok(ApiResponse(vehicle, HttpStatus.OK.hashCode(), "Vehicle retrieved successfully", false))
+        } catch (e: Exception) {
+            ResponseEntity.ok(ApiResponse(null, HttpStatus.BAD_REQUEST.hashCode(), e.message ?: "Unknown Error", true))
+        }
+    }
 
     /**
      * Register a vehicle
