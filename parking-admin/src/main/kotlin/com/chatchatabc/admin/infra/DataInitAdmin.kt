b578a30c59b8d79deb5e481a35ed4dc34d6c5615
Raphile14 <rllddalangin@gmail.com>
2023-04-26 10:33:47 +0800

    feat: created DataInitAdmin.kt

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/admin/infra/DataInitAdmin.kt
@@ -0,0 +1,19 @@
+package com.chatchatabc.admin.infra
+
+import com.chatchatabc.admin.domain.service.AdminService
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class DataInitAdmin(
+        private val adminService: AdminService
+) : CommandLineRunner {
+    private val log = LoggerFactory.getLogger(DataInitAdmin::class.java)
+
+    override fun run(vararg args: String?) {
+        log.info("Initializing data for Admin...")
+        adminService.initAdmin()
+        log.info("Admin data initialized.")
+    }
+}
