bc667da02aeaac49a0e5a4d9fd7d1ada4c1a76f1
Raphile14 <rllddalangin@gmail.com>
2023-04-26 18:09:13 +0800

    feat: created initial DashboardController.kt

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/admin/application/rest/DashboardController.kt
@@ -0,0 +1,12 @@
+package com.chatchatabc.admin.application.rest
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/dashboard")
+class DashboardController {
+    @GetMapping("/test")
+    fun test(): String {
+        return "Hello World!"
+    }
+}
