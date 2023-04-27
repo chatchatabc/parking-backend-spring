aa7a0749b9d06b1c54c54f7588de28e86d6115e8
Raphile14 <rllddalangin@gmail.com>
2023-04-27 17:20:19 +0800

    feat(admin): implemented initial get user login data

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/UserController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/UserController.kt
@@ -3,7 +3,9 @@ package com.chatchatabc.parking.admin.application.rest
 import com.chatchatabc.parking.admin.application.dto.ApiResponse
 import com.chatchatabc.parking.domain.enums.ResponseNames
 import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.parking.domain.model.log.UserLoginLog
 import com.chatchatabc.parking.domain.repository.UserRepository
+import com.chatchatabc.parking.domain.repository.log.UserLoginLogRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
@@ -15,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/user")
 class UserController(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val userLoginLogRepository: UserLoginLogRepository
 ) {
 
     /**
@@ -30,6 +33,36 @@ class UserController(
             ResponseEntity.ok(
                 ApiResponse(
                     users,
+                    HttpStatus.OK.value(),
+                    ResponseNames.SUCCESS.name,
+                    false
+                )
+            )
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(
+                ApiResponse(
+                    null,
+                    HttpStatus.BAD_REQUEST.value(),
+                    ResponseNames.ERROR.name,
+                    true
+                )
+            )
+        }
+    }
+
+    /**
+     * Get User Login Logs w/ pageable
+     */
+    // TODO: Add date range?
+    @GetMapping("/get-login-logs")
+    fun getUserLoginLogs(
+        pageable: Pageable
+    ): ResponseEntity<ApiResponse<Page<UserLoginLog>>> {
+        return try {
+            val logs = userLoginLogRepository.findAll(pageable)
+            ResponseEntity.ok(
+                ApiResponse(
+                    logs,
                     HttpStatus.OK.value(),
                     ResponseNames.SUCCESS.name,
                     false
