e76f439c868b1770a945163903156c337dd377b8
Raphile14 <rllddalangin@gmail.com>
2023-04-27 15:42:43 +0800

    feat: created ResponseNames.java to make error codes consistent for KMM and Admin Website

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/enums/ResponseNames.java
@@ -0,0 +1,14 @@
+package com.chatchatabc.parking.domain.enums;
+
+public enum ResponseNames {
+    SUCCESS,
+
+    /**
+     * User Related
+     */
+    USER_LOGIN_SUCCESS,
+    USER_BAD_CREDENTIALS,
+
+    ERROR,
+    UNKNOWN_ERROR,
+}
