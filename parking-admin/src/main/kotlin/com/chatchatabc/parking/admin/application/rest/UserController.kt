da000f9e96fff5b1e74da41d63a9229e586c47a4
Raphile14 <rllddalangin@gmail.com>
2023-04-27 16:39:40 +0800

    feat(admin): implemented get list of users api

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/UserController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/UserController.kt
@@ -1,10 +1,49 @@
 package com.chatchatabc.parking.admin.application.rest
 
+import com.chatchatabc.parking.admin.application.dto.ApiResponse
+import com.chatchatabc.parking.domain.enums.ResponseNames
+import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.parking.domain.repository.UserRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/user")
 class UserController(
+    private val userRepository: UserRepository
+) {
 
-)
+    /**
+     * Get all users by pageable
+     */
+    @GetMapping("/get")
+    fun getUsers(
+        pageable: Pageable
+    ): ResponseEntity<ApiResponse<Page<User>>> {
+        return try {
+            val users = userRepository.findAll(pageable)
+            ResponseEntity.ok(
+                ApiResponse(
+                    users,
+                    HttpStatus.OK.value(),
+                    ResponseNames.SUCCESS.name,
+                    false
+                )
+            )
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(
+                ApiResponse(
+                    null,
+                    HttpStatus.BAD_REQUEST.value(),
+                    ResponseNames.ERROR.name,
+                    true
+                )
+            )
+        }
+    }
+}
