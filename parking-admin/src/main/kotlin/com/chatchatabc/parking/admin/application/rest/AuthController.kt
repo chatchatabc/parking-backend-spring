a8ef976a12b95ab3392509ae7ae8de9d35e6ea77
Raphile14 <rllddalangin@gmail.com>
2023-04-27 17:19:54 +0800

    feat(admin): implemented initial logging of user logins

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/AuthController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/AuthController.kt
@@ -4,7 +4,9 @@ import com.chatchatabc.parking.admin.application.dto.ApiResponse
 import com.chatchatabc.parking.admin.application.dto.user.UserLoginRequest
 import com.chatchatabc.parking.domain.enums.ResponseNames
 import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.parking.domain.model.log.UserLoginLog
 import com.chatchatabc.parking.domain.repository.UserRepository
+import com.chatchatabc.parking.domain.repository.log.UserLoginLogRepository
 import com.chatchatabc.parking.web.common.application.rest.service.JwtService
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpHeaders
@@ -19,7 +21,8 @@ import org.springframework.web.bind.annotation.*
 class AuthController(
     private val authenticationManager: AuthenticationManager,
     private val userRepository: UserRepository,
-    private val jwtService: JwtService
+    private val jwtService: JwtService,
+    private val userLoginLogRepository: UserLoginLogRepository
 ) {
     /**
      * Login user and authenticate user
@@ -29,9 +32,8 @@ class AuthController(
         @RequestBody req: UserLoginRequest,
         request: HttpServletRequest
     ): ResponseEntity<ApiResponse<User>> {
+        val user = userRepository.findByUsername(req.username)
         return try {
-            val user = userRepository.findByUsername(req.username).get()
-
             // Authenticate user
             authenticationManager.authenticate(
                 UsernamePasswordAuthenticationToken(
@@ -41,11 +43,35 @@ class AuthController(
             )
             // Generate JWT Token
             val headers = HttpHeaders()
-            val token: String = jwtService.generateToken(user.id)
+            val token: String = jwtService.generateToken(user.get().id)
             headers.set("X-Access-Token", token)
+            // Generate Successful Login Log
+            // TODO: Move as an event
+            userLoginLogRepository.save(
+                UserLoginLog().apply {
+                    this.user = user.get()
+                    this.ipAddress = request.remoteAddr
+                    this.email = user.get().email
+                    this.phone = user.get().phone
+                    this.type = 1
+                    this.success = true
+                }
+            )
             ResponseEntity.ok().headers(headers)
-                .body(ApiResponse(user, HttpStatus.OK.value(), ResponseNames.USER_LOGIN_SUCCESS.name, false))
+                .body(ApiResponse(user.get(), HttpStatus.OK.value(), ResponseNames.USER_LOGIN_SUCCESS.name, false))
         } catch (e: Exception) {
+            // Generate Failed Login Log
+            // TODO: Move as an event
+            userLoginLogRepository.save(
+                UserLoginLog().apply {
+                    this.user = user.get()
+                    this.ipAddress = request.remoteAddr
+                    this.email = user.get().email
+                    this.phone = user.get().phone
+                    this.type = 1
+                    this.success = false
+                }
+            )
             ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(
                     ApiResponse(
