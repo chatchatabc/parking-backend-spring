669a2beef5da9531104141451e72b809f36f4472
Raphile14 <rllddalangin@gmail.com>
2023-04-27 14:28:41 +0800

    refactor: modified locations of main classes and removed unnecessary config files

--- parking-web-common/src/main/java/com/chatchatabc/parking/web/common/WebCommonConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.chatchatabc.parking.web.common;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class WebCommonConfiguration {
-}
