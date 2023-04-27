d73d3a2e4f0214782ce372364309639be11d1084
Raphile14 <rllddalangin@gmail.com>
2023-04-27 16:02:14 +0800

    feat: added more fields to ResponseNames.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/enums/ResponseNames.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/enums/ResponseNames.java
@@ -1,14 +1,32 @@
 package com.chatchatabc.parking.domain.enums;
 
 public enum ResponseNames {
+    /**
+     * Misc Positive
+     */
     SUCCESS,
+    SUCCESS_CREATE,
+    SUCCESS_UPDATE,
 
     /**
      * User Related
      */
     USER_LOGIN_SUCCESS,
     USER_BAD_CREDENTIALS,
+    USER_VERIFY_OTP_SUCCESS,
 
+    /**
+     * Invoice Related
+     */
+    INVOICE_END_SUCCESS,
+    INVOICE_PAY_SUCCESS,
+
+    /**
+     * Misc Negative
+     */
     ERROR,
+    ERROR_CREATE,
+    ERROR_UPDATE,
+    ERROR_NOT_FOUND,
     UNKNOWN_ERROR,
 }
