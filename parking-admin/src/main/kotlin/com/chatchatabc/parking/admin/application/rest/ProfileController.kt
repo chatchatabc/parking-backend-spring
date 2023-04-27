e33759e6f23377969fd28eb783d0e8ef8ba18fc1
Raphile14 <rllddalangin@gmail.com>
2023-04-27 16:37:05 +0800

    feat: created ProfileController.kt and transferred /me and /logout there

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/ProfileController.kt
@@ -0,0 +1,69 @@
+package com.chatchatabc.parking.admin.application.rest
+
+import com.chatchatabc.parking.admin.application.dto.ApiResponse
+import com.chatchatabc.parking.domain.enums.ResponseNames
+import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.parking.domain.repository.UserRepository
+import io.swagger.v3.oas.annotations.Operation
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/profile")
+class ProfileController(
+    private val userRepository: UserRepository
+) {
+    /**
+     * Get User Profile
+     */
+    @Operation(
+        summary = "Get the profile of the logged in user",
+        description = "User to get the profile of the logged in user."
+    )
+    @GetMapping("/me")
+    fun getProfile(): ResponseEntity<ApiResponse<User>> {
+        return try {
+            // Get ID from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as User
+            val user = userRepository.findById(principal.id).get()
+            ResponseEntity.ok().body(
+                ApiResponse(user, HttpStatus.OK.value(), ResponseNames.SUCCESS.name, false)
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(
+                    ApiResponse(
+                        null,
+                        HttpStatus.BAD_REQUEST.value(),
+                        ResponseNames.ERROR.name,
+                        true
+                    )
+                )
+        }
+    }
+
+    /**
+     * Logout user
+     */
+    @PostMapping("/logout")
+    fun logoutUser(
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ): ResponseEntity<ApiResponse<User>> {
+        return try {
+            // TODO: Implement logout
+            ResponseEntity.ok(ApiResponse(null, HttpStatus.OK.value(), ResponseNames.SUCCESS.name, false))
+        } catch (e: Exception) {
+            ResponseEntity.badRequest()
+                .body(ApiResponse(null, HttpStatus.BAD_REQUEST.value(), ResponseNames.ERROR.name, true))
+        }
+    }
+}
