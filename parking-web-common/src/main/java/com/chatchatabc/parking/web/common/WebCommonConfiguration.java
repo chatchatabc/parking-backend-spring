f031d38ea44f82f42d698d80a29f76305de84a0d
Raphile14 <rllddalangin@gmail.com>
2023-04-27 10:54:31 +0800

    feat: created parking-web-common module

---
+++ parking-web-common/src/main/java/com/chatchatabc/parking/web/common/WebCommonConfiguration.java
@@ -0,0 +1,9 @@
+package com.chatchatabc.parking.web.common;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class WebCommonConfiguration {
+}
