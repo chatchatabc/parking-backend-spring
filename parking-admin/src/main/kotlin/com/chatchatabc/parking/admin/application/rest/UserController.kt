c37a1c6556a90e80ba402096132ad9f33704f8be
Raphile14 <rllddalangin@gmail.com>
2023-04-27 11:57:38 +0800

    feat: implemented get user profile api

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/UserController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/UserController.kt
@@ -2,13 +2,14 @@ package com.chatchatabc.parking.admin.application.rest
 
 import com.chatchatabc.parking.admin.application.dto.ApiResponse
 import com.chatchatabc.parking.domain.model.User
+import com.chatchatabc.parking.domain.repository.UserRepository
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import jakarta.servlet.http.HttpSession
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,23 +17,43 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/user")
 class UserController(
-        private val httpSession: HttpSession
+    private val userRepository: UserRepository
 ) {
+    /**
+     * Get User Profile
+     */
+    @Operation(
+        summary = "Get the profile of the logged in user",
+        description = "User to get the profile of the logged in user."
+    )
+    @GetMapping("/me")
+    fun getProfile(): ResponseEntity<ApiResponse<User>> {
+        return try {
+            // Get ID from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as User
+            val user = userRepository.findById(principal.id).get()
+            ResponseEntity.ok().body(
+                ApiResponse(user, HttpStatus.OK.value(), "Get profile successful", false)
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(
+                    ApiResponse(null, HttpStatus.BAD_REQUEST.value(), e.message ?: "Unknown Error", true)
+                )
+        }
+    }
 
     /**
      * Logout user
      */
     @PostMapping("/logout")
     fun logoutUser(
-            request: HttpServletRequest,
-            response: HttpServletResponse
+        request: HttpServletRequest,
+        response: HttpServletResponse
     ): ResponseEntity<ApiResponse<User>> {
         return try {
-            val authentication: Authentication? = SecurityContextHolder.getContext().authentication
-            if (authentication == null) {
-                throw Exception("User not logged in")
-            }
-            httpSession.invalidate()
+            // TODO: Implement logout
             ResponseEntity.ok(ApiResponse(null, HttpStatus.OK.value(), "Logout successful", false))
         } catch (e: Exception) {
             ResponseEntity.badRequest().body(ApiResponse(null, HttpStatus.BAD_REQUEST.value(), e.message, true))
