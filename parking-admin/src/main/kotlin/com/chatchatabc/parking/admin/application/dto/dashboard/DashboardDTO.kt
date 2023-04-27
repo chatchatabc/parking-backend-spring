8fbcc3e84d6c686399f80f55f6849f1e3adc2dbf
Raphile14 <rllddalangin@gmail.com>
2023-04-27 16:27:31 +0800

    feat: created DashboardDTO.kt

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/dto/dashboard/DashboardDTO.kt
@@ -0,0 +1,8 @@
+package com.chatchatabc.parking.admin.application.dto.dashboard
+
+data class DashboardStatistics(
+    val totalUsers: Long?,
+    val totalVehicles: Long?,
+    val totalParkingLots: Long?,
+    val totalActiveInvoices: Long?
+)
