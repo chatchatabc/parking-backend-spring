c06d54bba103082fca3eb7de8ef2f9568bb44062
Raphile14 <rllddalangin@gmail.com>
2023-04-28 17:21:32 +0800

    improvement(admin): implemented get invoices api

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/InvoiceController.kt
@@ -0,0 +1,26 @@
+package com.chatchatabc.parking.admin.application.rest
+
+import com.chatchatabc.parking.domain.model.Invoice
+import com.chatchatabc.parking.domain.repository.InvoiceRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class InvoiceController(
+    private val invoiceRepository: InvoiceRepository
+) {
+    /**
+     * Get invoices
+     */
+    @QueryMapping
+    fun getInvoices(
+        @Argument page: Int,
+        @Argument size: Int
+    ): Page<Invoice> {
+        val pr = PageRequest.of(page, size)
+        return invoiceRepository.findAll(pr)
+    }
+}
