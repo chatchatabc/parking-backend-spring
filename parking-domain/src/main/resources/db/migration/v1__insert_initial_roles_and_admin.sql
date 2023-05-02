8fbb87043dfc545f4ecc43b38aa440666930a444
Raphile14 <rllddalangin@gmail.com>
2023-05-02 10:43:56 +0800

    test: added initial phone field to insert initial roles and admin

--- parking-domain/src/main/resources/db/migration/v1__insert_initial_roles_and_admin.sql
+++ parking-domain/src/main/resources/db/migration/v1__insert_initial_roles_and_admin.sql
@@ -16,10 +16,11 @@ SELECT '9b9fd9be-bea0-44f5-98e0-18ea30d06a62', 'ROLE_USER'
 WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'ROLE_USER');
 
 -- Insert initial admin user
-INSERT INTO users (id, email, username, password, flag)
+INSERT INTO users (id, email, username, phone, password, flag)
 SELECT 'ec4af6e9-ec57-434d-990d-ae83d9459a31',
        'admin@gmail.com',
        'admin',
+       '1234567890',
        '$2a$10$HfewouomThstiUJu.zfYPOsLJahJHCVnqS7GbEz0KFBQjiZUcsINK',
        0
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
