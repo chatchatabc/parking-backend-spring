af3078adc32da2d29029a30dcf1e2e99c94f98b7
Raphile14 <rllddalangin@gmail.com>
2023-05-02 16:42:15 +0800

    feat: implemented get reports graphql api

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/ReportResolver.kt
@@ -0,0 +1,37 @@
+package com.chatchatabc.parking.admin.application.graphql
+
+import com.chatchatabc.parking.admin.application.dto.PageInfo
+import com.chatchatabc.parking.admin.application.dto.PagedResponse
+import com.chatchatabc.parking.domain.model.Report
+import com.chatchatabc.parking.domain.repository.ReportRepository
+import org.springframework.data.domain.PageRequest
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class ReportResolver(
+    private val reportRepository: ReportRepository
+) {
+    /**
+     * Get reports
+     */
+    @QueryMapping
+    fun getReports(
+        @Argument page: Int,
+        @Argument size: Int
+    ): PagedResponse<Report> {
+        val pr = PageRequest.of(page, size)
+        val reports = reportRepository.findAll(pr)
+        return PagedResponse(
+            reports.content,
+            PageInfo(
+                reports.size,
+                reports.totalElements,
+                reports.isFirst,
+                reports.isLast,
+                reports.isEmpty
+            )
+        )
+    }
+}
