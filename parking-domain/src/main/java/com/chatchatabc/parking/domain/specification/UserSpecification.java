380c3fd69b099291561a36fe9d984662d0f3f8ce
Raphile14 <rllddalangin@gmail.com>
2023-05-02 17:34:55 +0800

    feat: created UserSpecification.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/specification/UserSpecification.java
@@ -0,0 +1,19 @@
+package com.chatchatabc.parking.domain.specification;
+
+import com.chatchatabc.parking.domain.model.User;
+import org.springframework.data.jpa.domain.Specification;
+
+public class UserSpecification {
+    public static Specification<User> withKeyword(String keyword) {
+        return (root, query, builder) -> {
+            String pattern = "%" + keyword.toLowerCase() + "%";
+            return builder.or(
+                    builder.like(builder.lower(root.get("username")), pattern),
+                    builder.like(builder.lower(root.get("email")), pattern),
+                    builder.like(builder.lower(root.get("firstName")), pattern),
+                    builder.like(builder.lower(root.get("lastName")), pattern),
+                    builder.like(builder.lower(root.get("phone")), pattern)
+            );
+        };
+    }
+}
