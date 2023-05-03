e8551d19f849eb528087f70d612246afb6e4bbed
Raphile14 <rllddalangin@gmail.com>
2023-05-03 15:49:25 +0800

    improvement(domain): added regex for user entity fields

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/User.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/User.java
@@ -3,6 +3,7 @@ package com.chatchatabc.parking.domain.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -40,21 +41,26 @@ public class User extends FlagEntity implements UserDetails {
     private String notificationId = UUID.randomUUID().toString();
 
     @Column(unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$", message = "Invalid email format.")
     private String email;
 
     @Column(unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9_]{3,15}$", message = "Invalid username format.")
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Column(unique = true, nullable = false)
+    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Invalid phone number format.")
     private String phone;
 
     @Column
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "First name can only contain letters and spaces.")
     private String firstName;
 
     @Column
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Last name can only contain letters and spaces.")
     private String lastName;
 
     @Column
