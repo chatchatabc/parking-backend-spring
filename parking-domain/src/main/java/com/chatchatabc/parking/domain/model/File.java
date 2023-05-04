309602f67ecca384cf9361f673640e739cd6183b
Raphile14 <rllddalangin@gmail.com>
2023-05-04 15:27:18 +0800

    refactor: removed filename field to File.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/File.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/File.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -39,7 +38,4 @@ public class File {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
 }
