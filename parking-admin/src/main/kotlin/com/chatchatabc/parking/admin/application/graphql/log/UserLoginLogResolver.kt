04d8a57b54bd4e8b64984c221b179d641df8d639
Raphile14 <rllddalangin@gmail.com>
2023-05-04 09:08:54 +0800

    feat(admin): implemented get login logs by user

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/log/UserLoginLogResolver.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/log/UserLoginLogResolver.kt
@@ -3,6 +3,7 @@ package com.chatchatabc.parking.admin.application.graphql.log
 import com.chatchatabc.parking.admin.application.dto.PageInfo
 import com.chatchatabc.parking.admin.application.dto.PagedResponse
 import com.chatchatabc.parking.domain.model.log.UserLoginLog
+import com.chatchatabc.parking.domain.repository.UserRepository
 import com.chatchatabc.parking.domain.repository.log.UserLoginLogRepository
 import org.springframework.data.domain.PageRequest
 import org.springframework.graphql.data.method.annotation.Argument
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Controller
 
 @Controller
 class UserLoginLogResolver(
-    private val userLoginLogRepository: UserLoginLogRepository
+    private val userLoginLogRepository: UserLoginLogRepository,
+    private val userRepository: UserRepository
 ) {
     /**
      * Get user login logs
@@ -23,6 +25,30 @@ class UserLoginLogResolver(
     ): PagedResponse<UserLoginLog> {
         val pr = PageRequest.of(page, size)
         val logs = userLoginLogRepository.findAll(pr)
+        return PagedResponse(
+            logs.content,
+            PageInfo(
+                logs.size,
+                logs.totalElements,
+                logs.isFirst,
+                logs.isLast,
+                logs.isEmpty
+            )
+        )
+    }
+
+    /**
+     * Get user login logs per user
+     */
+    @QueryMapping
+    fun getUserLoginLogsByUser(
+        @Argument page: Int,
+        @Argument size: Int,
+        @Argument id: String
+    ): PagedResponse<UserLoginLog> {
+        val pr = PageRequest.of(page, size)
+        val user = userRepository.findByUserId(id).get()
+        val logs = userLoginLogRepository.findByUser(user, pr)
         return PagedResponse(
             logs.content,
             PageInfo(
