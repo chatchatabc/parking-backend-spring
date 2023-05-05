b5ce04a7c27e2809cae0c33810434d27548508b2
Raphile14 <rllddalangin@gmail.com>
2023-05-05 17:14:43 +0800

    feat(admin): implemented get parking lots by owner

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/ParkingLotResolver.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/ParkingLotResolver.kt
@@ -4,6 +4,7 @@ import com.chatchatabc.parking.admin.application.dto.PageInfo
 import com.chatchatabc.parking.admin.application.dto.PagedResponse
 import com.chatchatabc.parking.domain.model.ParkingLot
 import com.chatchatabc.parking.domain.repository.ParkingLotRepository
+import com.chatchatabc.parking.domain.repository.UserRepository
 import org.springframework.data.domain.PageRequest
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -12,7 +13,8 @@ import java.util.*
 
 @Controller
 class ParkingLotResolver(
-    private val parkingLotRepository: ParkingLotRepository
+    private val parkingLotRepository: ParkingLotRepository,
+    private val userRepository: UserRepository
 ) {
     /**
      * Get parking lots
@@ -44,5 +46,26 @@ class ParkingLotResolver(
         @Argument id: String
     ): Optional<ParkingLot> {
         return parkingLotRepository.findById(id)
+    }
+
+    @QueryMapping
+    fun getParkingLotsByOwner(
+        @Argument page: Int,
+        @Argument size: Int,
+        @Argument ownerId: String
+    ): PagedResponse<ParkingLot> {
+        val pr = PageRequest.of(page, size)
+        val user = userRepository.findByUserId(ownerId).get()
+        val parkingLots = parkingLotRepository.findAllByOwner(user, pr)
+        return PagedResponse(
+            parkingLots.content,
+            PageInfo(
+                parkingLots.size,
+                parkingLots.totalElements,
+                parkingLots.isFirst,
+                parkingLots.isLast,
+                parkingLots.isEmpty
+            )
+        )
     }
 }
