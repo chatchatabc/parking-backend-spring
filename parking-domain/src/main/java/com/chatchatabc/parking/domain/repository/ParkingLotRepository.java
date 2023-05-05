4d52bc8e76c6b35085009a48e7737286d676f246
Raphile14 <rllddalangin@gmail.com>
2023-05-05 17:45:17 +0800

    chore(domain): modified ParkingLotRepository.java to extend to JpaSpecificationExecutor

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ParkingLotRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ParkingLotRepository.java
@@ -5,6 +5,7 @@ import com.chatchatabc.parking.domain.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ParkingLotRepository extends JpaRepository<ParkingLot, String> {
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, String>, JpaSpecificationExecutor<ParkingLot> {
     /**
      * Find a parking lot by its id and owner
      */
