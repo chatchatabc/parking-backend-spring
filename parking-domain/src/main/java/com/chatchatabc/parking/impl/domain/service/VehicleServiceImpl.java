1836924ea6bf37c19d6db0a32c65aa107d560756
Raphile14 <rllddalangin@gmail.com>
2023-05-09 14:19:24 +0800

    refactor(domain): modified more services to use member instead of user

--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/VehicleServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/VehicleServiceImpl.java
@@ -1,8 +1,8 @@
 package com.chatchatabc.parking.impl.domain.service;
 
-import com.chatchatabc.parking.domain.model.User;
+import com.chatchatabc.parking.domain.model.Member;
 import com.chatchatabc.parking.domain.model.Vehicle;
-import com.chatchatabc.parking.domain.repository.UserRepository;
+import com.chatchatabc.parking.domain.repository.MemberRepository;
 import com.chatchatabc.parking.domain.repository.VehicleRepository;
 import com.chatchatabc.parking.domain.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,22 +15,22 @@ public class VehicleServiceImpl implements VehicleService {
     @Autowired
     private VehicleRepository vehicleRepository;
     @Autowired
-    private UserRepository userRepository;
+    private MemberRepository memberRepository;
 
     /**
      * Register vehicle
      *
-     * @param userId      the user id
+     * @param memberId    the member id
      * @param name        the name of the vehicle
      * @param plateNumber the plate number of the vehicle
      * @param type        the type of the vehicle
      * @return the vehicle
      */
     @Override
-    public Vehicle registerVehicle(String userId, String name, String plateNumber, int type) throws Exception {
-        Optional<User> owner = userRepository.findByUserId(userId);
+    public Vehicle registerVehicle(String memberId, String name, String plateNumber, int type) throws Exception {
+        Optional<Member> owner = memberRepository.findByMemberId(memberId);
         if (owner.isEmpty()) {
-            throw new Exception("User not found");
+            throw new Exception("Member not found");
         }
         Vehicle vehicle = new Vehicle();
         vehicle.setName(name);
@@ -38,16 +38,16 @@ public class VehicleServiceImpl implements VehicleService {
         vehicle.setType(type);
         vehicle.setOwner(owner.get());
         Vehicle savedVehicle = vehicleRepository.save(vehicle);
-        // Add user to user vehicles table
+        // Add member to member vehicles table
         owner.get().getVehicles().add(savedVehicle);
-        userRepository.save(owner.get());
+        memberRepository.save(owner.get());
         return savedVehicle;
     }
 
     /**
      * Update vehicle
      *
-     * @param userId      the user id
+     * @param memberId    the member id
      * @param vehicleId   the vehicle id
      * @param name        the name of the vehicle
      * @param plateNumber the plate number of the vehicle
@@ -55,17 +55,17 @@ public class VehicleServiceImpl implements VehicleService {
      * @return the vehicle
      */
     @Override
-    public Vehicle updateVehicle(String userId, String vehicleId, String name, String plateNumber, Integer type) throws Exception {
-        Optional<User> user = userRepository.findByUserId(userId);
-        if (user.isEmpty()) {
-            throw new Exception("User not found");
+    public Vehicle updateVehicle(String memberId, String vehicleId, String name, String plateNumber, Integer type) throws Exception {
+        Optional<Member> member = memberRepository.findByMemberId(memberId);
+        if (member.isEmpty()) {
+            throw new Exception("Member not found");
         }
         Optional<Vehicle> vehicle = vehicleRepository.findById(vehicleId);
         if (vehicle.isEmpty()) {
             throw new Exception("Vehicle not found");
         }
-        // Check if user has access to update vehicle
-        if (!vehicle.get().getUsers().contains(user.get())) {
+        // Check if member has access to update vehicle
+        if (!vehicle.get().getMembers().contains(member.get())) {
             throw new Exception("Vehicle not found");
         }
 
@@ -84,87 +84,87 @@ public class VehicleServiceImpl implements VehicleService {
     }
 
     /**
-     * Add a user to a vehicle
+     * Add a member to a vehicle
      *
-     * @param userId      the user id
-     * @param vehicleId   the vehicle id
-     * @param userToAddId the user to add id
+     * @param memberId      the member id
+     * @param vehicleId     the vehicle id
+     * @param memberToAddId the member to add id
      * @return the vehicle
      * @throws Exception the exception
      */
     @Override
-    public Vehicle addUserToVehicle(String userId, String vehicleId, String userToAddId) throws Exception {
-        Optional<User> user = userRepository.findByUserId(userId);
-        if (user.isEmpty()) {
-            throw new Exception("User not found");
+    public Vehicle addMemberToVehicle(String memberId, String vehicleId, String memberToAddId) throws Exception {
+        Optional<Member> member = memberRepository.findByMemberId(memberId);
+        if (member.isEmpty()) {
+            throw new Exception("Member not found");
         }
 
         Optional<Vehicle> vehicle = vehicleRepository.findById(vehicleId);
         if (vehicle.isEmpty()) {
             throw new Exception("Vehicle not found");
         }
-        // Check if user has access to update vehicle
-        if (!vehicle.get().getUsers().contains(user.get())) {
+        // Check if member has access to update vehicle
+        if (!vehicle.get().getMembers().contains(member.get())) {
             throw new Exception("Vehicle not found");
         }
 
-        Optional<User> userToAdd = userRepository.findByUserId(userToAddId);
-        if (userToAdd.isEmpty()) {
-            throw new Exception("User to add not found");
+        Optional<Member> memberToAdd = memberRepository.findByMemberId(memberToAddId);
+        if (memberToAdd.isEmpty()) {
+            throw new Exception("Member to add not found");
         }
-        // Check if user to add is already in vehicle
-        if (vehicle.get().getUsers().contains(userToAdd.get())) {
-            throw new Exception("User already in vehicle");
+        // Check if member to add is already in vehicle
+        if (vehicle.get().getMembers().contains(memberToAdd.get())) {
+            throw new Exception("Member already in vehicle");
         }
 
-        // Add vehicle to user
-        userToAdd.get().getVehicles().add(vehicle.get());
-        userRepository.save(userToAdd.get());
+        // Add vehicle to member
+        memberToAdd.get().getVehicles().add(vehicle.get());
+        memberRepository.save(memberToAdd.get());
 
         return vehicle.get();
     }
 
     /**
-     * Remove a user from a vehicle
+     * Remove a member from a vehicle
      *
-     * @param userId         the user id
-     * @param vehicleId      the vehicle id
-     * @param userToRemoveId the user to remove id
+     * @param memberId         the member id
+     * @param vehicleId        the vehicle id
+     * @param memberToRemoveId the member to remove id
      * @return the vehicle
      * @throws Exception the exception
      */
     @Override
-    public Vehicle removeUserFromVehicle(String userId, String vehicleId, String userToRemoveId) throws Exception {
-        Optional<User> user = userRepository.findByUserId(userId);
-        if (user.isEmpty()) {
-            throw new Exception("User not found");
+    public Vehicle removeMemberFromVehicle(String memberId, String vehicleId, String memberToRemoveId) throws Exception {
+        Optional<Member> member = memberRepository.findByMemberId(memberId);
+        if (member.isEmpty()) {
+            throw new Exception("Member not found");
         }
 
         Optional<Vehicle> vehicle = vehicleRepository.findById(vehicleId);
         if (vehicle.isEmpty()) {
             throw new Exception("Vehicle not found");
         }
-        // Check if user has access to update vehicle
-        if (!vehicle.get().getUsers().contains(user.get())) {
+        // Check if member has access to update vehicle
+        if (!vehicle.get().getMembers().contains(member.get())) {
             throw new Exception("Vehicle not found");
         }
-        // Check if user is the owner, do not remove
-        if (vehicle.get().getOwner().getId().equals(userToRemoveId)) {
+        // Check if member is the owner, do not remove
+        if (vehicle.get().getOwner().getId().equals(memberToRemoveId)) {
             throw new Exception("Cannot remove owner");
         }
 
-        Optional<User> userToRemove = userRepository.findByUserId(userToRemoveId);
-        if (userToRemove.isEmpty()) {
-            throw new Exception("User to add not found");
+        Optional<Member> memberToRemove = memberRepository.findByMemberId(memberToRemoveId);
+        if (memberToRemove.isEmpty()) {
+            throw new Exception("Member to add not found");
         }
-        // Check if user to remove is not in vehicle
-        if (!vehicle.get().getUsers().contains(userToRemove.get())) {
-            throw new Exception("User not in vehicle");
+        // Check if member to remove is not in vehicle
+        if (!vehicle.get().getMembers().contains(memberToRemove.get())) {
+            throw new Exception("Member not in vehicle");
         }
 
-        // Remove vehicle from user
-        userToRemove.get().getVehicles().remove(vehicle.get());
-        userRepository.save(userToRemove.get());
+        // Remove vehicle from member
+        memberToRemove.get().getVehicles().remove(vehicle.get());
+        memberRepository.save(memberToRemove.get());
 
         return vehicle.get();
     }
