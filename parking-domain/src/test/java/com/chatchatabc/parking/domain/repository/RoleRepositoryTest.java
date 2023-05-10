204b64749ce34c7c4daf3180594f307c1439ce28
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-10 17:28:26 +0800

    feat: create RoleRepositoryTest.java

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/RoleRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.chatchatabc.parking.domain.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleRepositoryTest {
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findRolesIn() {
+    }
+}
