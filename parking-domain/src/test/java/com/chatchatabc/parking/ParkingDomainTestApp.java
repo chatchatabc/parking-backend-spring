dcadd406aa42511e83820ea387ba6af9f1c0ddce
Libing Chen <libing.chen@gmail.com>
2023-05-10 18:55:15 +0800

    chore: add @SpringBootApplication

--- parking-domain/src/test/java/com/chatchatabc/parking/ParkingDomainTestApp.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/ParkingDomainTestApp.java
@@ -1,7 +1,9 @@
 package com.chatchatabc.parking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ParkingDomainTestApp {
     public static void main(String[] args) {
         SpringApplication
