0e4a75d1ee2df5e7e2e4a398f4de1ed2565e8f69
Libing Chen <libing.chen@gmail.com>
2023-05-10 19:06:37 +0800

    test: add @Dataset

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/RoleRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/RoleRepositoryTest.java
@@ -1,63 +1,34 @@
 package com.chatchatabc.parking.domain.repository;
 
+import com.chatchatabc.parking.TestContainersBaseTest;
 import com.chatchatabc.parking.domain.model.Role;
+import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(classes = {RoleRepository.class} )
-@ActiveProfiles("test")
-public class RoleRepositoryTest {
+@DataSet("db/datasets/role.xml")
+public class RoleRepositoryTest extends TestContainersBaseTest {
 
     @Autowired
     private RoleRepository roleRepository;
 
     @Test
     public void testFindByName() {
-        // Create a test role and save it to the database
-        Role role = new Role();
-        role.setName("ROLE_TEST");
-        roleRepository.save(role);
-
-        // Invoke the method being tested
-        Optional<Role> result = roleRepository.findByName("ROLE_TEST");
-
-        // Assert that the role is found
-        assertTrue(result.isPresent());
-        assertEquals("ROLE_TEST", result.get().getName());
+        assertThat(roleRepository.findByName("ROLE_ADMIN")).isPresent();
     }
 
     @Test
     public void testFindRolesIn() {
-        // Create test roles and save them to the database
-        Role role1 = new Role();
-        role1.setName("ROLE_TEST1");
-        roleRepository.save(role1);
-
-        Role role2 = new Role();
-        role2.setName("ROLE_TEST2");
-        roleRepository.save(role2);
-
-        Role role3 = new Role();
-        role3.setName("ROLE_TEST3");
-        roleRepository.save(role3);
-
-        // Create a list of role names to search for
-        List<String> roleNames = Arrays.asList("ROLE_TEST1", "ROLE_TEST2");
-
         // Invoke the method being tested
-        List<Role> result = roleRepository.findRolesIn(roleNames);
-
-        // Assert the result
-        assertEquals(2, result.size());
-        assertTrue(result.stream().anyMatch(role -> role.getName().equals("ROLE_TEST1")));
-        assertTrue(result.stream().anyMatch(role -> role.getName().equals("ROLE_TEST2")));
+        List<Role> result = roleRepository.findRolesIn(Arrays.asList("ROLE_ADMIN", "ROLE_MEMBER"));
+        assertThat(result).hasSize(2);
+        assertTrue(result.stream().anyMatch(role -> role.getName().equals("ROLE_ADMIN")));
+        assertTrue(result.stream().anyMatch(role -> role.getName().equals("ROLE_MEMBER")));
     }
 }
