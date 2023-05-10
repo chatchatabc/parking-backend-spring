89dd89d3a7a76956a66d5b640e5a52b8e22da6e6
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-10 18:11:59 +0800

    test: initial test for RoleRepositoryTest.java

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/RoleRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/RoleRepositoryTest.java
@@ -1,16 +1,63 @@
 package com.chatchatabc.parking.domain.repository;
 
+import com.chatchatabc.parking.domain.model.Role;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RoleRepositoryTest {
+@SpringBootTest
+@ActiveProfiles("test")
+public class RoleRepositoryTest {
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Test
-    void findByName() {
+    public void testFindByName() {
+        // Create a test role and save it to the database
+        Role role = new Role();
+        role.setName("ROLE_TEST");
+        roleRepository.save(role);
+
+        // Invoke the method being tested
+        Optional<Role> result = roleRepository.findByName("ROLE_TEST");
+
+        // Assert that the role is found
+        assertTrue(result.isPresent());
+        assertEquals("ROLE_TEST", result.get().getName());
     }
 
     @Test
-    void findRolesIn() {
+    public void testFindRolesIn() {
+        // Create test roles and save them to the database
+        Role role1 = new Role();
+        role1.setName("ROLE_TEST1");
+        roleRepository.save(role1);
+
+        Role role2 = new Role();
+        role2.setName("ROLE_TEST2");
+        roleRepository.save(role2);
+
+        Role role3 = new Role();
+        role3.setName("ROLE_TEST3");
+        roleRepository.save(role3);
+
+        // Create a list of role names to search for
+        List<String> roleNames = Arrays.asList("ROLE_TEST1", "ROLE_TEST2");
+
+        // Invoke the method being tested
+        List<Role> result = roleRepository.findRolesIn(roleNames);
+
+        // Assert the result
+        assertEquals(2, result.size());
+        assertTrue(result.stream().anyMatch(role -> role.getName().equals("ROLE_TEST1")));
+        assertTrue(result.stream().anyMatch(role -> role.getName().equals("ROLE_TEST2")));
     }
 }
