db4545119db94cbd660f69b49f5cd75b1be8d764
Libing Chen <libing.chen@gmail.com>
2023-05-10 18:21:09 +0800

    test: add redisContainer and natsContainer

--- parking-domain/src/test/java/com/chatchatabc/parking/TestContainersConfiguration.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/TestContainersConfiguration.java
@@ -3,6 +3,8 @@ package com.chatchatabc.parking;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SuppressWarnings("resource")
@@ -18,6 +20,21 @@ public class TestContainersConfiguration {
     @ServiceConnection
     public PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>("postgres:15.2");
+    }
+
+    @Bean
+    @ServiceConnection
+    public GenericContainer<?> redisContainer() {
+        return new GenericContainer<>("redis:7.0.10")
+                .withExposedPorts(6379);
+    }
+
+    @Bean
+    @ServiceConnection
+    public GenericContainer<?> natsContainer(DynamicPropertyRegistry properties) {
+        GenericContainer<?> natsContainer = new GenericContainer<>("nats:2.9.16").withExposedPorts(4222);
+        properties.add("spring.nats.uri", () -> "nats://" + natsContainer.getHost() + ":" + natsContainer.getMappedPort(4222));
+        return natsContainer;
     }
 
 }
