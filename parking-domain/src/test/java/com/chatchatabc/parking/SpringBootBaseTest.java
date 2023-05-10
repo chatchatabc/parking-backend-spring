0fa092787612d1368f0c05ffc014fadcac5a82ba
Libing Chen <libing.chen@gmail.com>
2023-05-10 18:16:08 +0800

    test: add ParkingDomainTestApp

--- parking-domain/src/test/java/com/chatchatabc/parking/SpringBootBaseTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/SpringBootBaseTest.java
@@ -3,7 +3,7 @@ package com.chatchatabc.parking;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = ParkingDomainTestApp.class)
 @ActiveProfiles("test")
 public abstract class SpringBootBaseTest {
 }
