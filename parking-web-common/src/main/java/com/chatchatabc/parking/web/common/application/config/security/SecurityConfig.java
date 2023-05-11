05ac8b207f33d7ffd8acccd831e64c31f3fc79cf
Raphile14 <rllddalangin@gmail.com>
2023-05-11 15:26:32 +0800

    improvement(web-common): fixed deprecation error with cors and csrf

--- parking-web-common/src/main/java/com/chatchatabc/parking/web/common/application/config/security/SecurityConfig.java
+++ parking-web-common/src/main/java/com/chatchatabc/parking/web/common/application/config/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,7 +31,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors().and().csrf().disable()
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     // Allow all requests to the authentication endpoint
                     auth.requestMatchers("/api/auth/**").permitAll();
