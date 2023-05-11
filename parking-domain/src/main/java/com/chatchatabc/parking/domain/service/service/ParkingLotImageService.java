4a72784d3dd23d9c41bb3857494e29342061c823
Raphile14 <rllddalangin@gmail.com>
2023-05-11 14:56:28 +0800

    refactor(domain): adjusted ParkingLotImageService.java based on updated schema

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/service/ParkingLotImageService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/service/ParkingLotImageService.java
@@ -14,10 +14,13 @@ public interface ParkingLotImageService {
      * @param parkingLot  the parking lot to which the image belongs
      * @param namespace   the namespace of the file
      * @param inputStream the file to upload
+     * @param filename    the filename
+     * @param filesize    the filesize
+     * @param mimetype    the mimetype
      * @return the uploaded file data
      * @throws Exception if an error occurs
      */
-    ParkingLotImage uploadImage(Member uploadedBy, ParkingLot parkingLot, String namespace, InputStream inputStream) throws Exception;
+    ParkingLotImage uploadImage(Member uploadedBy, ParkingLot parkingLot, String namespace, InputStream inputStream, String filename, Long filesize, String mimetype) throws Exception;
 
     /**
      * Mark an image as deleted
