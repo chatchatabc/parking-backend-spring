dfba6875e4c91acefc4db02d92efaae145f8e1a8
Libing Chen <libing.chen@gmail.com>
2023-05-11 09:20:46 +0800

    test: OSSMockClient

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/infra/oss/OSSMockClient.java
@@ -0,0 +1,31 @@
+package com.chatchatabc.parking.impl.infra.oss;
+
+import com.aliyun.oss.ClientConfiguration;
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.common.utils.IOUtils;
+import com.aliyun.oss.model.PutObjectResult;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class OSSMockClient extends OSSClient {
+    private Map<String, byte[]> fileStore = new HashMap<>();
+
+    public OSSMockClient() {
+        super("", null, new ClientConfiguration());
+    }
+
+    @Override
+    public PutObjectResult putObject(String bucketName, String key, InputStream input) throws OSSException, ClientException {
+        try {
+            fileStore.put(key, IOUtils.readStreamAsByteArray(input));
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        return new PutObjectResult();
+    }
+}
