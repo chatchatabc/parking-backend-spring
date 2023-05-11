896a0054250dcfa7797754d0c18eceff524e6d82
Raphile14 <rllddalangin@gmail.com>
2023-05-11 10:42:12 +0800

    refactor(admin): moved all dtos into one file DTO.kt

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/dto/ApiResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.chatchatabc.parking.admin.application.dto
-
-data class ApiResponse<T>(
-    val data: T?,
-    val code: Int = 0,
-    val message: String?,
-    val error: Boolean = false
-)
--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/dto/PageInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.chatchatabc.parking.admin.application.dto
-
-data class PageInfo(
-    val size: Int,
-    val totalElements: Long,
-    val first: Boolean,
-    val last: Boolean,
-    val empty: Boolean
-)
