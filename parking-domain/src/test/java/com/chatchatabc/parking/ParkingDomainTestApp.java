70a0c4277f100e3cc9606f714d615876902e8d72
Libing Chen <libing.chen@gmail.com>
2023-05-11 14:31:05 +0800

    test: adjust to spring boot app for unit test

--- parking-domain/src/test/java/com/chatchatabc/parking/ParkingDomainTestApp.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/ParkingDomainTestApp.java
@@ -2,13 +2,12 @@ package com.chatchatabc.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({TestContainersConfiguration.class, InfraConfiguration.class})
 public class ParkingDomainTestApp {
     public static void main(String[] args) {
-        SpringApplication
-                .from(ParkingDomainTestApp::main)
-                .with(TestContainersConfiguration.class)
-                .run(args);
+        SpringApplication.run(ParkingDomainTestApp.class, args);
     }
 }
