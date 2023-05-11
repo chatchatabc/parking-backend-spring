88212fd8890e42c70dfd2ebe76d29117b39e8d1e
Libing Chen <libing.chen@gmail.com>
2023-05-11 14:30:34 +0800

    test: introduce @TestConfiguration

--- parking-domain/src/test/java/com/chatchatabc/parking/InfraConfiguration.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/InfraConfiguration.java
@@ -2,11 +2,11 @@ package com.chatchatabc.parking;
 
 import com.aliyun.oss.OSS;
 import com.chatchatabc.parking.impl.infra.oss.OSSMockClient;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+@TestConfiguration
 public class InfraConfiguration {
     @Bean
     public OSS ossMockClient() {
