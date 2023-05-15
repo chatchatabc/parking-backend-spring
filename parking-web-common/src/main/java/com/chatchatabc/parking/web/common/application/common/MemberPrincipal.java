fd95a08dfc2388b4ad00d924438b170851a9f7c1
Raphile14 <rllddalangin@gmail.com>
2023-05-15 15:42:18 +0800

    feat(web-common): created MemberPrincipal.java

---
+++ parking-web-common/src/main/java/com/chatchatabc/parking/web/common/application/common/MemberPrincipal.java
@@ -0,0 +1,22 @@
+package com.chatchatabc.parking.web.common.application.common;
+
+import java.security.Principal;
+
+public interface MemberPrincipal extends Principal {
+
+    String getMemberUuid();
+
+    static MemberPrincipal of (String memberUuid, String username) {
+        return new MemberPrincipal() {
+            @Override
+            public String getMemberUuid() {
+                return memberUuid;
+            }
+
+            @Override
+            public String getName() {
+                return username;
+            }
+        };
+    }
+}
