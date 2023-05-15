828f0cc5d8d827092273fa56ad5770f4cc5a9249
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-15 16:05:34 +0800

    test: added unit tests for MemberRepositoryTest.java

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
@@ -1,33 +1,55 @@
 package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
+import com.chatchatabc.parking.domain.model.Member;
 import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@DataSet({"db/datasets/member.xml", "db/datasets/role.xml"})
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataSet("db/datasets/member.xml")
 class MemberRepositoryTest extends TestContainersBaseTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
     @Test
-    void findByUsername() {
+    void testFindByUsername() {
+        String username = "admin";
+        Optional<Member> member = memberRepository.findByUsername(username);
+        assertThat(member).isPresent();
+        assertThat(member.get().getUsername()).isEqualTo(username);
     }
 
     @Test
-    void findByPhone() {
+    void testFindByPhone() {
+        String phone = "1234567890";
+        Optional<Member> member = memberRepository.findByPhone(phone);
+        assertThat(member).isPresent();
+        assertThat(member.get().getPhone()).isEqualTo(phone);
     }
 
     @Test
-    void countMembersByRole() {
+    void testCountMembersByRole() {
+        String roleName = "ROLE_ADMIN";
+        Long count = memberRepository.countMembersByRole(roleName);
+        assertThat(count).isEqualTo(1L);
     }
 
     @Test
-    void findByMemberUuid() {
+    void testFindByMemberUuid() {
+        String memberUuid = "ec4af6e9-ec57-434d-990d-ae83d9459a31";
+        Optional<Member> member = memberRepository.findByMemberUuid(memberUuid);
+        assertThat(member).isPresent();
+        assertThat(member.get().getMemberUuid()).isEqualTo(memberUuid);
     }
 
     @Test
-    void countVerified() {
+    void testCountVerified() {
+        Long count = memberRepository.countVerified();
+        assertThat(count).isEqualTo(4L);
     }
 }
