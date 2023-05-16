39309e1c3b0c8a6ce3256e3d9b77017082ea5960
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-16 16:59:20 +0800

    feat: create VehicleRepositoryTest.java

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.chatchatabc.parking.domain.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleRepositoryTest {
+
+    @Test
+    void findByVehicleUuidAndOwner() {
+    }
+
+    @Test
+    void findByVehicleUuid() {
+    }
+
+    @Test
+    void findAllByOwner() {
+    }
+
+    @Test
+    void findAllByMember() {
+    }
+}
