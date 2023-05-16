d7190a2dfb398e925dc6d2ce37ea3a55270721a2
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-16 14:59:17 +0800

    feat: add unit test for counting unverified members

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
@@ -81,4 +81,18 @@ class MemberRepositoryTest extends TestContainersBaseTest {
         Long count = memberRepository.countVerified();
         assertThat(count).isEqualTo(4L);
     }
+
+    @Test
+    @DataSet("db/datasets/member_without_verification.xml")
+    void testCountVerified_NoMembersVerified() {
+        Optional<Member> member = memberRepository.findById(1L);
+        member.ifPresent(m -> {
+            m.setEmailVerifiedAt(null);
+            m.setPhoneVerifiedAt(null);
+            memberRepository.save(m);
+        });
+
+        Long count = memberRepository.countVerified();
+        assertThat(count).isEqualTo(0L);
+    }
 }
