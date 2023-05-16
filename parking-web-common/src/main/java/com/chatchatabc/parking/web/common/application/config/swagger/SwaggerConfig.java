0248db679189d656fea5dd13fc6a01f2adb3f54c
Raphile14 <rllddalangin@gmail.com>
2023-05-16 13:12:11 +0800

    improvement: modified swagger to use details from application.properties

--- parking-web-common/src/main/java/com/chatchatabc/parking/web/common/application/config/swagger/SwaggerConfig.java
+++ parking-web-common/src/main/java/com/chatchatabc/parking/web/common/application/config/swagger/SwaggerConfig.java
@@ -3,11 +3,18 @@ package com.chatchatabc.parking.web.common.application.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${server.swagger.title}")
+    private String title;
+    @Value("${server.swagger.description}")
+    private String description;
+    @Value("${server.swagger.version}")
+    private String version;
 
     /**
      * Swagger configuration
@@ -19,10 +26,9 @@ public class SwaggerConfig {
         return new OpenAPI().components(new Components())
                 .info(
                         new Info()
-                                // TODO: Put details on application properties
-                                .title("Parking Swagger API")
-                                .description("Parking Swagger API")
-                                .version("1.0.0")
+                                .title(title)
+                                .description(description)
+                                .version(version)
                 );
     }
 }
