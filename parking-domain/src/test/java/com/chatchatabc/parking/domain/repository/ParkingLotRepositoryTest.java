c0ee194f720ca1006babaf08db835d3cf50457bd
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-17 11:54:40 +0800

    feat: create ParkingLotRepositoryTest.java

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/ParkingLotRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.chatchatabc.parking.domain.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingLotRepositoryTest {
+
+    @Test
+    void findByParkingLotUuid() {
+    }
+
+    @Test
+    void findByOwner() {
+    }
+
+    @Test
+    void findByDistance() {
+    }
+
+    @Test
+    void findAllByOwner() {
+    }
+
+    @Test
+    void findAllByOwnerAndStatus() {
+    }
+
+    @Test
+    void countVerified() {
+    }
+}
