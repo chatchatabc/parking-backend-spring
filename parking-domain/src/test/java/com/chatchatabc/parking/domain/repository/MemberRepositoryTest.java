d48f292f3a404292b71056d91f1ace30a8d427fe
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-17 17:38:18 +0800

    remove: delete testCountVerified_NoMembersVerified() test method

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
@@ -80,18 +80,4 @@ class MemberRepositoryTest extends TestContainersBaseTest {
         Long verifiedMembersCount = memberRepository.countVerified();
         assertThat(verifiedMembersCount).isEqualTo(3);
     }
-
-    //    TODO: Use member.xml
-    @Test
-    void testCountVerified_NoMembersVerified() {
-        Optional<Member> member = memberRepository.findById(1L);
-        member.ifPresent(m -> {
-            m.setEmailVerifiedAt(null);
-            m.setPhoneVerifiedAt(null);
-            memberRepository.save(m);
-        });
-
-        Long count = memberRepository.countVerified();
-        assertThat(count).isEqualTo(0L);
-    }
 }
--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
@@ -1,13 +1,37 @@
 package com.chatchatabc.parking.domain.repository;
 
+import com.chatchatabc.parking.TestContainersBaseTest;
+import com.chatchatabc.parking.domain.model.Vehicle;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
-class VehicleRepositoryTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class VehicleRepositoryTest extends TestContainersBaseTest {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
     @Test
-    void findByVehicleUuidAndOwner() {
+    public void testFindByVehicleUuidAndOwner_ShouldReturnMatchingVehicleWhenExists() {
+        // Given
+        String vehicleUuid = "2da0ddab-9e9d-45cb-a2a5-f6bff1765ea9";
+        Long ownerId = 5L;
+
+        // When
+        Optional<Vehicle> vehicleOptional = vehicleRepository.findByVehicleUuidAndOwner(vehicleUuid, ownerId);
+
+        // Then
+        assertTrue(vehicleOptional.isPresent());
+        assertEquals("2da0ddab-9e9d-45cb-a2a5-f6bff1765ea9", vehicleOptional.get().getVehicleUuid());
+        assertEquals("Lightning McQueen", vehicleOptional.get().getName());
+        assertEquals("ASD1234", vehicleOptional.get().getPlateNumber());
+        assertEquals(0, vehicleOptional.get().getType());
+        assertEquals(ownerId, vehicleOptional.get().getOwner());
     }
 
     @Test
