785e5c71a11630e65a2b7e5b666a2db22ff19914
Raphile14 <rllddalangin@gmail.com>
2023-05-17 16:29:07 +0800

    test: added parking lot dataset to TestContainersBaseTest.java

--- parking-domain/src/test/java/com/chatchatabc/parking/TestContainersBaseTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/TestContainersBaseTest.java
@@ -11,6 +11,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE, columnSensing = true, schema = "public")
-@DataSet({"db/datasets/role.xml", "db/datasets/member.xml", "db/datasets/vehicle.xml"})
+@DataSet({"db/datasets/role.xml", "db/datasets/member.xml", "db/datasets/vehicle.xml", "db/datasets/parking_lot.xml"})
 public abstract class TestContainersBaseTest extends SpringBootBaseTest {
 }
