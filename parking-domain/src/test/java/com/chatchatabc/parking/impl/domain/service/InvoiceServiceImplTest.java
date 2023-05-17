76ec6cacfb1698209765cf208d481ee347d35c0e
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-17 17:49:31 +0800

    feat: create InvoiceServiceImplTest.java

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/InvoiceServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvoiceServiceImplTest {
+
+    @Test
+    void createInvoice() {
+    }
+
+    @Test
+    void endInvoice() {
+    }
+
+    @Test
+    void payInvoice() {
+    }
+}
