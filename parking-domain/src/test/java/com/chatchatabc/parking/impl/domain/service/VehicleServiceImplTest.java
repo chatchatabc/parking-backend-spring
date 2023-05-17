877c3f062888e99f7a25d7cc05a727a019e026ed
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-17 17:54:43 +0800

    create: VehicleServiceImplTest.java

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/VehicleServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleServiceImplTest {
+
+    @Test
+    void registerVehicle() {
+    }
+
+    @Test
+    void updateVehicle() {
+    }
+
+    @Test
+    void addMemberToVehicle() {
+    }
+
+    @Test
+    void removeMemberFromVehicle() {
+    }
+}
