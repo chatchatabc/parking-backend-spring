80196b01163c0c71e65a7110f1688f968351c7da
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-17 17:54:07 +0800

    chore: remove comments

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
@@ -18,14 +18,11 @@ public class VehicleRepositoryTest extends TestContainersBaseTest {
 
     @Test
     public void testFindByVehicleUuidAndOwner_ShouldReturnMatchingVehicleWhenExists() {
-        // Given
         String vehicleUuid = "2da0ddab-9e9d-45cb-a2a5-f6bff1765ea9";
         Long ownerId = 5L;
 
-        // When
         Optional<Vehicle> vehicleOptional = vehicleRepository.findByVehicleUuidAndOwner(vehicleUuid, ownerId);
 
-        // Then
         assertTrue(vehicleOptional.isPresent());
         assertEquals("2da0ddab-9e9d-45cb-a2a5-f6bff1765ea9", vehicleOptional.get().getVehicleUuid());
         assertEquals("Lightning McQueen", vehicleOptional.get().getName());
