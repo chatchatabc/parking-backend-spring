e4be82548351f6e7d0c6a3e08ff35c92a6e1b1ac
Raphile14 <rllddalangin@gmail.com>
2023-05-18 10:52:21 +0800

    test(domain): implemented testFindAllByVehicle_InvoicesAreFound

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/InvoiceRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/InvoiceRepositoryTest.java
@@ -2,11 +2,20 @@ package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class InvoiceRepositoryTest extends TestContainersBaseTest {
+    @Autowired
+    private InvoiceRepository invoiceRepository;
 
     @Test
-    void findAllByVehicle() {
+    void testFindAllByVehicle_InvoicesAreFound() {
+        Long vehicleId = 1L;
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        assertThat(invoiceRepository.findAllByVehicle(vehicleId, pageRequest).getNumberOfElements()).isGreaterThan(0);
     }
 
     @Test
