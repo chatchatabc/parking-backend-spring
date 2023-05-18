2c5b2bb9b51f38f91c0ffe238443fee2735bcf7f
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-18 13:24:21 +0800

    chore: remove unused import statement for List

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
