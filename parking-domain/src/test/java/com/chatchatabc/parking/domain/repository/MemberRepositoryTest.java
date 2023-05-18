33461bebc2b62c9a55dfc6c8861c81945ed3002d
Raphile14 <rllddalangin@gmail.com>
2023-05-18 10:23:28 +0800

    chore(domain): removed unnecessary @dataset

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/MemberRepositoryTest.java
@@ -2,7 +2,6 @@ package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
 import com.chatchatabc.parking.domain.model.Member;
-import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,7 +9,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataSet("db/datasets/member.xml")
 class MemberRepositoryTest extends TestContainersBaseTest {
 
     @Autowired
