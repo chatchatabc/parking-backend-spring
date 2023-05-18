33a3b2e70b2a11c8069b49d3c5c37f1d0c18a777
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-05-18 13:23:46 +0800

    feat: add import statement for Pageable

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/VehicleRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
