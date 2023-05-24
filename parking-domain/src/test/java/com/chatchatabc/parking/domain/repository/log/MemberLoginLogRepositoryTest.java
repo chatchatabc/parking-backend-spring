060fd05f1ef634bf0d768c7ecef152d9a9daf2fb
Raphile14 <rllddalangin@gmail.com>
2023-05-24 09:36:14 +0800

    test: implemented testFindByMember_ShouldReturnGreaterThan0

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/log/MemberLoginLogRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/log/MemberLoginLogRepositoryTest.java
@@ -2,10 +2,19 @@ package com.chatchatabc.parking.domain.repository.log;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberLoginLogRepositoryTest extends TestContainersBaseTest {
+    @Autowired
+    private MemberLoginLogRepository memberLoginLogRepository;
 
     @Test
-    void findByMember() {
+    void testFindByMember_ShouldReturnGreaterThan0() {
+        Long memberId = 6L;
+        PageRequest pr = PageRequest.of(0, 10);
+        assertThat(memberLoginLogRepository.findByMember(memberId, pr).getTotalElements()).isGreaterThan(0L);
     }
 }
