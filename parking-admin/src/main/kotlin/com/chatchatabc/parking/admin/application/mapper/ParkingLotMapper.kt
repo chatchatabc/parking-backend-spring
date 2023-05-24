748ea1c4a8f080d354847bf5608e1d4a2777f4db
Raphile14 <rllddalangin@gmail.com>
2023-05-24 14:34:38 +0800

    feat(admin): created ParkingLotMapper.kt

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/mapper/ParkingLotMapper.kt
@@ -0,0 +1,31 @@
+package com.chatchatabc.parking.admin.application.mapper
+
+import com.chatchatabc.parking.admin.application.rest.ParkingLotController
+import com.chatchatabc.parking.domain.model.ParkingLot
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.MappingTarget
+import org.mapstruct.Mappings
+
+@Mapper(componentModel = "spring")
+interface ParkingLotMapper {
+
+    /**
+     * Parking Lot Mapper
+     */
+    @Mappings(
+        Mapping(target = "name", source = "request.name"),
+        Mapping(target = "latitude", source = "request.latitude"),
+        Mapping(target = "longitude", source = "request.longitude"),
+        Mapping(target = "address", source = "request.address"),
+        Mapping(target = "description", source = "request.description"),
+        Mapping(target = "capacity", source = "request.capacity"),
+        Mapping(target = "businessHoursStart", source = "request.businessHoursStart"),
+        Mapping(target = "businessHoursEnd", source = "request.businessHoursEnd"),
+        Mapping(target = "openDaysFlag", source = "request.openDaysFlag"),
+    )
+    fun createParkingLotFromCreateRequest(
+        request: ParkingLotController.ParkingLotCreateRequest,
+        @MappingTarget parkingLot: ParkingLot
+    )
+}
