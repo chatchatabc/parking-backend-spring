140b968e87b47e36e62cac39217afb9d33dd3b2c
Libing Chen <libing.chen@gmail.com>
2023-05-24 16:28:06 +0800

    feat: introduce OssObjectMock and add getObject()

--- parking-domain/src/test/java/com/chatchatabc/parking/impl/infra/oss/OSSMockClient.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/infra/oss/OSSMockClient.java
@@ -5,15 +5,22 @@ import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.OSSException;
 import com.aliyun.oss.common.utils.IOUtils;
+import com.aliyun.oss.model.OSSObject;
 import com.aliyun.oss.model.PutObjectResult;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
 public class OSSMockClient extends OSSClient {
-    private Map<String, byte[]> fileStore = new HashMap<>();
+    private final Map<String, OssObjectMock> fileStore = new HashMap<>();
+
+    record OssObjectMock(String bucketName, String key, byte[] content) {
+    }
+
 
     public OSSMockClient() {
         super("http://localhost", null, new ClientConfiguration());
@@ -22,10 +29,29 @@ public class OSSMockClient extends OSSClient {
     @Override
     public PutObjectResult putObject(String bucketName, String key, InputStream input) throws OSSException, ClientException {
         try {
-            fileStore.put(key, IOUtils.readStreamAsByteArray(input));
+            fileStore.put(key, new OssObjectMock(bucketName, key, IOUtils.readStreamAsByteArray(input)));
         } catch (Exception e) {
             throw new ClientException(e);
         }
         return new PutObjectResult();
+    }
+
+    @Override
+    public OSSObject getObject(String bucketName, String key) throws OSSException, ClientException {
+        if (Objects.equals(key, "health.txt")) {
+            final OSSObject ossObject = new OSSObject();
+            ossObject.setBucketName(bucketName);
+            ossObject.setKey(key);
+            return ossObject;
+        }
+        if (fileStore.containsKey(key)) {
+            var item = fileStore.get(key);
+            OSSObject ossObject = new OSSObject();
+            ossObject.setKey(key);
+            ossObject.setBucketName(item.bucketName());
+            ossObject.setObjectContent(new ByteArrayInputStream(item.content()));
+            return ossObject;
+        }
+        return null;
     }
 }
