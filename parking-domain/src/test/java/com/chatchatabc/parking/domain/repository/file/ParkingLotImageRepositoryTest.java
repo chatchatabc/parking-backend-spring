b650c70c4390908b252e7a76cca85c74a264ac4e
Raphile14 <rllddalangin@gmail.com>
2023-05-25 14:03:33 +0800

    test: implemented testFindAllByParkingLotAndStatus_ShouldReturnGreaterThan0 and testFindAllByParkingLotAndStatus_ShouldReturn0

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/file/ParkingLotImageRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/file/ParkingLotImageRepositoryTest.java
@@ -2,14 +2,30 @@ package com.chatchatabc.parking.domain.repository.file;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ParkingLotImageRepositoryTest extends TestContainersBaseTest {
+    @Autowired
+    private ParkingLotImageRepository parkingLotImageRepository;
 
     @Test
-    void findAllByParkingLotAndStatus() {
+    void testFindAllByParkingLotAndStatus_ShouldReturnGreaterThan0() {
+        Long parkingLotId = 2L;
+        PageRequest pr = PageRequest.of(0, 10);
+        assertThat(parkingLotImageRepository.findAllByParkingLotAndStatus(parkingLotId, 0, pr).getTotalElements()).isGreaterThan(0);
     }
 
     @Test
-    void findByIdAndStatus() {
+    void testFindAllByParkingLotAndStatus_ShouldReturn0() {
+        Long parkingLotId = 1L;
+        PageRequest pr = PageRequest.of(0, 10);
+        assertThat(parkingLotImageRepository.findAllByParkingLotAndStatus(parkingLotId, 0, pr).getTotalElements()).isEqualTo(0);
+    }
+
+    @Test
+    void testFindByIdAndStatus() {
     }
 }
