8734a6d5e33f794c046a7f7f88f5fb8961056e54
Raphile14 <rllddalangin@gmail.com>
2023-05-25 10:40:55 +0800

    test: created ParkingLotImageServiceImplTest.java

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/file/ParkingLotImageServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.chatchatabc.parking.impl.domain.service.file;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingLotImageServiceImplTest {
+
+    @Test
+    void uploadImage() {
+    }
+
+    @Test
+    void deleteImage() {
+    }
+
+    @Test
+    void restoreImage() {
+    }
+}
