f6932e468c2e399c7ee0e07024850b5ff6a291cd
Raphile14 <rllddalangin@gmail.com>
2023-05-25 11:36:54 +0800

    refactor: temporarily commented out specification by owner due to entities not being joined automatically anymore

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/specification/ParkingLotSpecification.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/specification/ParkingLotSpecification.java
@@ -1,8 +1,6 @@
 package com.chatchatabc.parking.domain.specification;
 
-import com.chatchatabc.parking.domain.model.Member;
 import com.chatchatabc.parking.domain.model.ParkingLot;
-import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 
 public class ParkingLotSpecification {
@@ -11,18 +9,35 @@ public class ParkingLotSpecification {
         return (root, query, builder) -> {
             String pattern = "%" + keyword.toLowerCase() + "%";
 
+            // Find a way to join the owner member entity without entity mapping
             // Join the owner member entity
-            Join<ParkingLot, Member> owner = root.join("owner");
+            // Join<ParkingLot, Member> owner = root.join("owner");
 
             return builder.or(
                     builder.like(builder.lower(root.get("name")), pattern),
-                    builder.like(builder.lower(root.get("address")), pattern),
-                    builder.like(builder.lower(owner.get("username")), pattern),
-                    builder.like(builder.lower(owner.get("email")), pattern),
-                    builder.like(builder.lower(owner.get("firstName")), pattern),
-                    builder.like(builder.lower(owner.get("lastName")), pattern),
-                    builder.like(builder.lower(owner.get("phone")), pattern)
+                    builder.like(builder.lower(root.get("address")), pattern)
+//                    builder.like(builder.lower(owner.get("username")), pattern),
+//                    builder.like(builder.lower(owner.get("email")), pattern),
+//                    builder.like(builder.lower(owner.get("firstName")), pattern),
+//                    builder.like(builder.lower(owner.get("lastName")), pattern),
+//                    builder.like(builder.lower(owner.get("phone")), pattern)
             );
+
+            // Subquery to join the owner member entity
+//            Subquery<Member> subquery = query.subquery(Member.class);
+//            Root<Member> subqueryRoot = subquery.from(Member.class);
+//            Join<Member, ParkingLot> subqueryJoin = subqueryRoot.join("parkingLots");
+//            subquery.select(subqueryJoin.getParent());
+//
+//            return builder.or(
+//                    builder.like(builder.lower(root.get("name")), pattern),
+//                    builder.like(builder.lower(root.get("address")), pattern),
+//                    builder.like(builder.lower(subqueryRoot.get("username")), pattern),
+//                    builder.like(builder.lower(subqueryRoot.get("email")), pattern),
+//                    builder.like(builder.lower(subqueryRoot.get("firstName")), pattern),
+//                    builder.like(builder.lower(subqueryRoot.get("lastName")), pattern),
+//                    builder.like(builder.lower(subqueryRoot.get("phone")), pattern)
+//            );
         };
     }
 }
