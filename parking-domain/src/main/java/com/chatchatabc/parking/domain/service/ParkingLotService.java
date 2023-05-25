46cd6517d6112f3d0ff5624ec631dc3b96d9666e
Raphile14 <rllddalangin@gmail.com>
2023-05-25 10:43:47 +0800

    refactor(domain): removed update function and merged it with just one save service function

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/ParkingLotService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/ParkingLotService.java
@@ -1,10 +1,6 @@
 package com.chatchatabc.parking.domain.service;
 
 import com.chatchatabc.parking.domain.model.ParkingLot;
-import com.chatchatabc.parking.domain.model.file.ParkingLotImage;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface ParkingLotService {
 
@@ -14,37 +10,6 @@ public interface ParkingLotService {
      * @param parkingLot the parking lot
      */
     void saveParkingLot(ParkingLot parkingLot);
-
-    /**
-     * Update parking lot
-     *
-     * @param memberId           the member id
-     * @param parkingLotUuid     the parking lot uuid
-     * @param name               the name of the parking lot
-     * @param latitude           the latitude of the parking lot
-     * @param longitude          the longitude of the parking lot
-     * @param address            the address of the parking lot
-     * @param description        the description of the parking lot
-     * @param capacity           the capacity of the parking lot
-     * @param businessHoursStart the business hours start of the parking lot
-     * @param businessHoursEnd   the business hours end of the parking lot
-     * @param openDaysFlag       the open days flag of the parking lot
-     * @return the parking lot
-     */
-    ParkingLot updateParkingLot(
-            String memberId,
-            String parkingLotUuid,
-            String name,
-            Double latitude,
-            Double longitude,
-            String address,
-            String description,
-            Integer capacity,
-            LocalDateTime businessHoursStart,
-            LocalDateTime businessHoursEnd,
-            Integer openDaysFlag,
-            List<ParkingLotImage> images
-    ) throws Exception;
 
     /**
      * Verify parking lot
--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/ParkingLotServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/ParkingLotServiceImpl.java
@@ -2,7 +2,6 @@ package com.chatchatabc.parking.impl.domain.service;
 
 import com.chatchatabc.parking.domain.model.Member;
 import com.chatchatabc.parking.domain.model.ParkingLot;
-import com.chatchatabc.parking.domain.model.file.ParkingLotImage;
 import com.chatchatabc.parking.domain.repository.InvoiceRepository;
 import com.chatchatabc.parking.domain.repository.MemberRepository;
 import com.chatchatabc.parking.domain.repository.ParkingLotRepository;
@@ -11,8 +10,6 @@ import com.chatchatabc.parking.domain.service.ParkingLotService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,81 +33,8 @@ public class ParkingLotServiceImpl implements ParkingLotService {
      */
     @Override
     public void saveParkingLot(ParkingLot parkingLot) {
-        parkingLotRepository.save(parkingLot);
-    }
-
-    /**
-     * Update parking lot
-     *
-     * @param memberUuid     the member id
-     * @param parkingLotUuid the parking lot uuid
-     * @param name           the name of the parking lot
-     * @param latitude       the latitude of the parking lot
-     * @param longitude      the longitude of the parking lot
-     * @param address        the address of the parking lot
-     * @param description    the description of the parking lot
-     * @param capacity       the capacity of the parking lot
-     * @return the parking lot
-     */
-    @Override
-    public ParkingLot updateParkingLot(String memberUuid, String parkingLotUuid, String name, Double latitude, Double longitude, String address, String description, Integer capacity, LocalDateTime businessHoursStart, LocalDateTime businessHoursEnd, Integer openDaysFlag, List<ParkingLotImage> images) throws Exception {
-        Optional<Member> member = memberRepository.findByMemberUuid(memberUuid);
-        if (member.isEmpty()) {
-            throw new Exception("Member not found");
-        }
-        Optional<ParkingLot> parkingLot = parkingLotRepository.findByParkingLotUuid(parkingLotUuid);
-        if (parkingLot.isEmpty()) {
-            throw new Exception("Parking lot not found");
-        }
-
-        // Apply Updates
-        if (name != null) {
-            parkingLot.get().setName(name);
-        }
-        if (latitude != null) {
-            parkingLot.get().setLatitude(latitude);
-        }
-        if (longitude != null) {
-            parkingLot.get().setLongitude(longitude);
-        }
-        if (address != null) {
-            parkingLot.get().setAddress(address);
-        }
-        if (description != null) {
-            parkingLot.get().setDescription(description);
-        }
-        if (capacity != null) {
-            parkingLot.get().setCapacity(capacity);
-            // Get active invoices and update available slots
-            Long activeInvoices = invoiceRepository.countActiveInvoicesByParkingLotId(parkingLot.get().getId());
-            parkingLot.get().setAvailableSlots(capacity - activeInvoices.intValue());
-        }
-        if (businessHoursStart != null) {
-            parkingLot.get().setBusinessHoursStart(businessHoursStart);
-        }
-        if (businessHoursEnd != null) {
-            parkingLot.get().setBusinessHoursEnd(businessHoursEnd);
-        }
-        if (openDaysFlag != null) {
-            parkingLot.get().setOpenDaysFlag(openDaysFlag);
-        }
-
-        // Update Image Order
-        if (images != null) {
-            List<ParkingLotImage> updatedImages = new ArrayList<>();
-            for (ParkingLotImage image : images) {
-                Optional<ParkingLotImage> parkingLotImage = parkingLotImageRepository.findById(image.getId());
-                if (parkingLotImage.isEmpty()) {
-                    throw new Exception("Image not found");
-                }
-                parkingLotImage.get().setFileOrder(image.getFileOrder());
-                updatedImages.add(parkingLotImage.get());
-            }
-            parkingLotImageRepository.saveAll(updatedImages);
-        }
-
         // TODO: NATS publish parking lot update
-        return parkingLotRepository.save(parkingLot.get());
+        parkingLotRepository.save(parkingLot);
     }
 
     /**
