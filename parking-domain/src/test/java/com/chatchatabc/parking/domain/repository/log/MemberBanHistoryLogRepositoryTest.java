3effd62f95d0063bc37625788cd90d757d16d85e
Raphile14 <rllddalangin@gmail.com>
2023-05-25 15:39:38 +0800

    test: implemented testFindLatestBanLog_ShouldReturnBanLog and testFindLatestBanLog_ShouldReturnEmpty

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/log/MemberBanHistoryLogRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/log/MemberBanHistoryLogRepositoryTest.java
@@ -2,10 +2,23 @@ package com.chatchatabc.parking.domain.repository.log;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class MemberBanHistoryLogRepositoryTest extends TestContainersBaseTest {
+    @Autowired
+    private MemberBanHistoryLogRepository memberBanHistoryLogRepository;
 
     @Test
-    void findLatestBanLog() {
+    void testFindLatestBanLog_ShouldReturnBanLog() {
+        Long memberId = 5L;
+        assertThat(memberBanHistoryLogRepository.findLatestBanLog(memberId)).isPresent();
+    }
+
+    @Test
+    void testFindLatestBanLog_ShouldReturnEmpty() {
+        Long memberId = 1L;
+        assertThat(memberBanHistoryLogRepository.findLatestBanLog(memberId)).isEmpty();
     }
 }
