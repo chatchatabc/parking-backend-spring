8ae5ba0cc80a4038a71ebbf130194664ed138659
Raphile14 <rllddalangin@gmail.com>
2023-05-29 15:24:51 +0800

    refactor: renamed MemberRepository.java to UserRepository.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/UserRepository.java
@@ -0,0 +1,55 @@
+package com.chatchatabc.parking.domain.repository;
+
+import com.chatchatabc.parking.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    /**
+     * Find user by username
+     *
+     * @param username the username
+     * @return the user
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * Find user by phone
+     *
+     * @param phone the phone
+     * @return the user
+     */
+    Optional<User> findByPhone(String phone);
+
+    /**
+     * Count users by role name
+     *
+     * @param roleName the role name
+     * @return the count
+     */
+    @Query("SELECT COUNT(u.id) FROM User u JOIN u.roles r WHERE r.name = :roleName")
+    Long countUsersByRole(String roleName);
+
+
+    /**
+     * Find user by user uuid
+     *
+     * @param userUuid the user id
+     * @return the user
+     */
+    Optional<User> findByUserUuid(String userUuid);
+
+    /**
+     * Count verified users
+     *
+     * @return the count
+     */
+    @Query("SELECT COUNT(u.id) FROM User u WHERE u.emailVerifiedAt IS NOT NULL OR u.phoneVerifiedAt IS NOT NULL")
+    Long countVerified();
+}
