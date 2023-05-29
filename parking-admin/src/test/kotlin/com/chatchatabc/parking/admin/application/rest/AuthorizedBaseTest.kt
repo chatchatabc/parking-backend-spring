9c06a4ea6dc71b8e6845049eb03bf61005b5909c
Raphile14 <rllddalangin@gmail.com>
2023-05-29 15:46:52 +0800

    feat(admin): added WebAppconfiguration and ContextConfiguration to initialize controller unit test with spring security

--- parking-admin/src/test/kotlin/com/chatchatabc/parking/admin/application/rest/AuthorizedBaseTest.kt
+++ parking-admin/src/test/kotlin/com/chatchatabc/parking/admin/application/rest/AuthorizedBaseTest.kt
@@ -2,21 +2,55 @@ package com.chatchatabc.parking.admin.application.rest
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.chatchatabc.parking.web.common.application.config.security.SecurityConfig
+import com.chatchatabc.parking.web.common.application.config.security.filter.JwtRequestFilter
 import com.chatchatabc.parking.web.common.application.rest.service.JwtService
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 import java.util.*
+import javax.sql.DataSource
 
+@WebAppConfiguration
+@ContextConfiguration(classes = [SecurityConfig::class])
 @WithMockUser(username = "dfc3cd78-9c89-4da2-8749-253afed080af", password = "123456", roles = ["ADMIN"])
 open class AuthorizedBaseTest {
+    @Autowired
+    lateinit var mvc: MockMvc
+
+    @Autowired
+    lateinit var context: WebApplicationContext
+
     @MockBean
     lateinit var jwtService: JwtService
 
+    @MockBean
+    lateinit var jwtRequestFilter: JwtRequestFilter
+
+    @MockBean
+    lateinit var dataSource: DataSource
+
     private val token: String by lazy { generateToken() }
+
+    @BeforeEach
+    fun setup() {
+        mvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
+            .build()
+    }
 
     fun get(urlTemplate: String, vararg uriVariables: Any?): MockHttpServletRequestBuilder {
         return MockMvcRequestBuilders.get(urlTemplate, *uriVariables).header("Authorization", "Bearer $token")
