068ef86dbf5087ee741b936e74c97c170664e26e
Raphile14 <rllddalangin@gmail.com>
2023-05-29 15:23:00 +0800

    chore: optimized imports

--- parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/file/ParkingLotImageServiceImplTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/file/ParkingLotImageServiceImplTest.java
@@ -2,8 +2,6 @@ package com.chatchatabc.parking.impl.domain.service.file;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ParkingLotImageServiceImplTest {
 
     @Test
