f47e77487183609378013ef325ea4d116fea949a
Raphile14 <rllddalangin@gmail.com>
2023-05-30 13:31:37 +0800

    feat(api): implemented save report service

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/ReportService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/ReportService.java
@@ -1,4 +1,13 @@
 package com.chatchatabc.parking.domain.service;
 
+import com.chatchatabc.parking.domain.model.Report;
+
 public interface ReportService {
+
+    /**
+     * Save report
+     *
+     * @param report the saved report
+     */
+    void saveReport(Report report);
 }
--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/ReportServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/ReportServiceImpl.java
@@ -1,8 +1,25 @@
 package com.chatchatabc.parking.impl.domain.service;
 
+import com.chatchatabc.parking.domain.model.Report;
+import com.chatchatabc.parking.domain.repository.ReportRepository;
 import com.chatchatabc.parking.domain.service.ReportService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportServiceImpl implements ReportService {
+    private final ReportRepository reportRepository;
+
+    public ReportServiceImpl(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    /**
+     * Save report
+     *
+     * @param report the saved report
+     */
+    @Override
+    public void saveReport(Report report) {
+        reportRepository.save(report);
+    }
 }
