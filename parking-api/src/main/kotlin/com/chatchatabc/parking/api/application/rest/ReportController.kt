0f252082d259a63326e37cbcb89f48e9368d58b8
Raphile14 <rllddalangin@gmail.com>
2023-05-30 13:32:01 +0800

    feat(api): created ReportController.kt and implemented get all and save apis

---
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/ReportController.kt
@@ -0,0 +1,64 @@
+package com.chatchatabc.parking.api.application.rest
+
+import com.chatchatabc.parking.api.application.dto.ApiResponse
+import com.chatchatabc.parking.api.application.dto.ErrorElement
+import com.chatchatabc.parking.api.application.mapper.ReportMapper
+import com.chatchatabc.parking.domain.enums.ResponseNames
+import com.chatchatabc.parking.domain.model.Report
+import com.chatchatabc.parking.domain.repository.ReportRepository
+import com.chatchatabc.parking.domain.service.ReportService
+import org.mapstruct.factory.Mappers
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/report")
+class ReportController(
+    private val reportRepository: ReportRepository,
+    private val reportService: ReportService
+) {
+    private val reportMapper = Mappers.getMapper(ReportMapper::class.java)
+
+    /**
+     * Get Reports
+     */
+    @GetMapping("/get-reports")
+    fun getReports(
+        pageable: Pageable
+    ): ResponseEntity<ApiResponse<Page<Report>>> {
+        return try {
+            // Get user from security context
+            val reports = reportRepository.findAll(pageable)
+            ResponseEntity.ok(ApiResponse(reports, null))
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
+        }
+    }
+
+    data class ReportCreateRequest(
+        val name: String,
+        val description: String,
+        val plateNumber: String,
+        val latitude: Double,
+        val longitude: Double
+    )
+
+    /**
+     * Create Report
+     */
+    @PostMapping("/create-report")
+    fun createReport(
+        @RequestBody req: ReportCreateRequest
+    ): ResponseEntity<ApiResponse<Nothing>> {
+        return try {
+            val report = Report()
+            reportMapper.createReportFromRequest(req, report)
+            reportService.saveReport(report)
+            ResponseEntity.ok(ApiResponse(null, null))
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
+        }
+    }
+}
