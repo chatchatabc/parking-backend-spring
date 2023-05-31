5cd9dcc921ae545b6a127da83dde1946254a1b81
Raphile14 <rllddalangin@gmail.com>
2023-05-31 09:18:21 +0800

    test: implemented testFindAllByReportedBy_ShouldReturnGreaterThan0

--- parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/ReportRepositoryTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/domain/repository/ReportRepositoryTest.java
@@ -1,7 +1,20 @@
 package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.TestContainersBaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReportRepositoryTest extends TestContainersBaseTest {
+    @Autowired
+    private ReportRepository reportRepository;
 
+    @Test
+    void testFindAllByReportedBy_ShouldReturnGreaterThan0() {
+        Long userId = 1L;
+        PageRequest pr = PageRequest.of(0, 10);
+        assertThat(reportRepository.findAllByReportedBy(userId, pr).getTotalElements()).isGreaterThan(0L);
+    }
 }
