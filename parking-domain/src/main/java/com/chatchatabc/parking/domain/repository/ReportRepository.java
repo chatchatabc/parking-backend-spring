d96b1c46d3a586a604e208d3f6db319a4eb8844d
Raphile14 <rllddalangin@gmail.com>
2023-05-31 09:17:19 +0800

    feat(domain): implemented findAllByReportedBy

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ReportRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ReportRepository.java
@@ -1,9 +1,20 @@
 package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.domain.model.Report;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    /**
+     * Find all reports by user id
+     *
+     * @param userId   user id
+     * @param pageable pagination
+     * @return page of reports
+     */
+    Page<Report> findAllByReportedBy(Long userId, Pageable pageable);
 }
