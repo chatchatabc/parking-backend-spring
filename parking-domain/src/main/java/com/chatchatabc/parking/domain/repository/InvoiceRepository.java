7696e5440368278210c00aed056477face4d1100
Raphile14 <rllddalangin@gmail.com>
2023-06-02 09:58:53 +0800

    feat(domain): implemented findLatestActiveInvoice

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/InvoiceRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/InvoiceRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, String> {
 
@@ -48,4 +50,14 @@ public interface InvoiceRepository extends JpaRepository<Invoice, String> {
      */
     @Query("SELECT COUNT(i) FROM Invoice i WHERE i.parkingLot = ?1 AND i.vehicle = ?2 AND i.endAt IS NULL")
     Long countActiveInvoicesByParkingLotAndVehicle(Long parkingLot, Long vehicle);
+
+    /**
+     * Find latest active invoice
+     *
+     * @param parkingLotId the parking lot id
+     * @param vehicleId    the vehicle id
+     * @return the latest active invoice
+     */
+    @Query("SELECT i FROM Invoice i WHERE i.parkingLot = ?1 AND i.vehicle = ?2 AND i.endAt IS NULL ORDER BY i.createdAt DESC LIMIT 1")
+    Optional<Invoice> findLatestActiveInvoice(Long parkingLotId, Long vehicleId);
 }
