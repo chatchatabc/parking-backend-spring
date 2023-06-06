e13e49d212e38d4bf80c71a68dab3eaa11140753
Raphile14 <rllddalangin@gmail.com>
2023-06-06 17:07:31 +0800

    feat(admin): implemented findAllByUser

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/log/UserBanHistoryLogRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/log/UserBanHistoryLogRepository.java
@@ -1,6 +1,8 @@
 package com.chatchatabc.parking.domain.repository.log;
 
 import com.chatchatabc.parking.domain.model.log.UserBanHistoryLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,4 +20,13 @@ public interface UserBanHistoryLogRepository extends JpaRepository<UserBanHistor
      */
     @Query("SELECT m FROM UserBanHistoryLog m WHERE m.user = ?1 AND m.status > -1 ORDER BY m.createdAt DESC LIMIT 1")
     Optional<UserBanHistoryLog> findLatestBanLog(Long userId);
+
+    /**
+     * Find all ban logs for a user
+     *
+     * @param userId   the id of the user
+     * @param pageable the pageable
+     * @return the page of ban logs
+     */
+    Page<UserBanHistoryLog> findAllByUser(Long userId, Pageable pageable);
 }
