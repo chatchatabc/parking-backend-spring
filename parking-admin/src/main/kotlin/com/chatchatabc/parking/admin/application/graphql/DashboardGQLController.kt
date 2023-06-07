307f667653f48096b079c8d57fdcb5ebd0941014
Raphile14 <rllddalangin@gmail.com>
2023-06-07 11:59:10 +0800

    feat: created DashboardGQLController.kt and implemented getDashboardPieGraph
    Fixes #3

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/DashboardGQLController.kt
@@ -0,0 +1,54 @@
+package com.chatchatabc.parking.admin.application.graphql
+
+import com.chatchatabc.parking.domain.repository.ParkingLotRepository
+import com.chatchatabc.parking.domain.repository.UserRepository
+import com.chatchatabc.parking.domain.repository.VehicleRepository
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class DashboardGQLController(
+    private val userRepository: UserRepository,
+    private val parkingLotRepository: ParkingLotRepository,
+    private val vehicleRepository: VehicleRepository
+) {
+
+    /**
+     * Dashboard Pie Graph DTO
+     */
+    data class DashboardPieGraphDTO(
+        // Parking Lots
+        var parkingLotVerifiedCount: Long = 0,
+        var parkingLotUnverifiedCount: Long = 0,
+
+        // Users
+        var userVerifiedCount: Long = 0,
+        var userUnverifiedCount: Long = 0,
+
+        // Vehicles
+        var vehicleVerifiedCount: Long = 0,
+        var vehicleUnverifiedCount: Long = 0,
+    )
+
+    /**
+     * Get Dashboard Pie Graph DTO
+     */
+    @QueryMapping
+    fun getDashboardPieGraph(): DashboardPieGraphDTO {
+        val data = DashboardPieGraphDTO().apply {
+            // Parking
+            parkingLotVerifiedCount = parkingLotRepository.countVerified()
+            parkingLotUnverifiedCount = parkingLotRepository.count() - parkingLotVerifiedCount
+
+            // Users
+            userVerifiedCount = userRepository.countVerified()
+            userUnverifiedCount = userRepository.count() - userVerifiedCount
+
+            // Vehicles
+            // TODO: implement vehicle verification system
+            vehicleVerifiedCount = vehicleRepository.count()
+            vehicleUnverifiedCount = 0
+        }
+        return data
+    }
+}
