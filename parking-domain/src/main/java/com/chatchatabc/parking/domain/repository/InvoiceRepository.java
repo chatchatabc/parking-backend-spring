d63f7db3a29ccf1c829fe52022aa8f8d716ef676
Raphile14 <rllddalangin@gmail.com>
2023-06-08 15:41:49 +0800

    chore: have InvoiceRepository.java extend to JpaSpecificationExecutor

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/InvoiceRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/InvoiceRepository.java
@@ -4,6 +4,7 @@ import com.chatchatabc.parking.domain.model.Invoice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+public interface InvoiceRepository extends JpaRepository<Invoice, Long>, JpaSpecificationExecutor<Invoice> {
 
     /**
      * Find invoice by uuid
