ed44827f92cb6212a2320ab7fb6eac05ea029f9d
Raphile14 <rllddalangin@gmail.com>
2023-06-08 15:32:50 +0800

    feat: created VehicleSpecification.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/specification/VehicleSpecification.java
@@ -0,0 +1,16 @@
+package com.chatchatabc.parking.domain.specification;
+
+import com.chatchatabc.parking.domain.model.Vehicle;
+import org.springframework.data.jpa.domain.Specification;
+
+public class VehicleSpecification extends GenericSpecification<Vehicle> {
+    public static Specification<Vehicle> withKeyword(String keyword) {
+        return (root, query, builder) -> {
+            String pattern = "%" + keyword.toLowerCase() + "%";
+            return builder.or(
+                    builder.like(builder.lower(root.get("plateNumber")), pattern),
+                    builder.like(builder.lower(root.get("name")), pattern)
+            );
+        };
+    }
+}
