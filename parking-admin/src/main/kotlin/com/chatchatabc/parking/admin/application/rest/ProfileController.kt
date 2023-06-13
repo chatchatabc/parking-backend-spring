a71edde6228bdd2f44e8b3889e5fb6ffad77ed23
Raphile14 <rllddalangin@gmail.com>
2023-06-13 11:45:11 +0800

    refactor(admin): updated ProfileController.kt to follow Kotlin Idiomatic Coding

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/ProfileController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/ProfileController.kt
@@ -1,16 +1,13 @@
 package com.chatchatabc.parking.admin.application.rest
 
-import com.chatchatabc.parking.admin.application.dto.ApiResponse
-import com.chatchatabc.parking.admin.application.dto.ErrorElement
-import com.chatchatabc.parking.domain.enums.ResponseNames
-import com.chatchatabc.parking.domain.model.User
 import com.chatchatabc.parking.domain.repository.UserRepository
 import com.chatchatabc.parking.domain.service.log.UserLogoutLogService
+import com.chatchatabc.parking.user
+import com.chatchatabc.parking.web.common.toErrorResponse
+import com.chatchatabc.parking.web.common.toResponse
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -34,15 +31,7 @@ class ProfileController(
     fun getProfile(
         request: HttpServletRequest,
         principal: Principal
-    ): ResponseEntity<ApiResponse<User>> {
-        return try {
-            val user = userRepository.findByUserUuid(principal.name).get()
-            ResponseEntity.ok().body(ApiResponse(user, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
-        }
-    }
+    ) = runCatching { principal.name.user.toResponse() }.getOrElse { it.toErrorResponse() }
 
     /**
      * Logout user
@@ -52,14 +41,7 @@ class ProfileController(
         request: HttpServletRequest,
         response: HttpServletResponse,
         principal: Principal
-    ): ResponseEntity<ApiResponse<User>> {
-        return try {
-            val user = userRepository.findByUserUuid(principal.name).get()
-            userLogoutLogService.createLog(user.id, 1, request.remoteAddr)
-            ResponseEntity.ok(ApiResponse(null, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.badRequest()
-                .body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
-        }
-    }
+    ) = runCatching {
+        userLogoutLogService.createLog(principal.name.user.id, 1, request.remoteAddr).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 }
