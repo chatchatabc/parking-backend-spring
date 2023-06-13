92153e6bc62e45396dbaa3a93e5f4a3cbefc559c
Raphile14 <rllddalangin@gmail.com>
2023-06-13 11:15:48 +0800

    refactor(api): updated ReportController.kt to follow Kotlin Idiomatic Coding

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/ReportController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/ReportController.kt
@@ -1,15 +1,14 @@
 package com.chatchatabc.parking.api.application.rest
 
 import com.chatchatabc.parking.api.application.mapper.ReportMapper
-import com.chatchatabc.parking.domain.enums.ResponseNames
 import com.chatchatabc.parking.domain.model.Report
 import com.chatchatabc.parking.domain.repository.ReportRepository
-import com.chatchatabc.parking.domain.repository.UserRepository
 import com.chatchatabc.parking.domain.service.ReportService
+import com.chatchatabc.parking.user
 import com.chatchatabc.parking.web.common.ApiResponse
-import com.chatchatabc.parking.web.common.ErrorElement
+import com.chatchatabc.parking.web.common.toErrorResponse
+import com.chatchatabc.parking.web.common.toResponse
 import org.mapstruct.factory.Mappers
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.*
 class ReportController(
     private val reportRepository: ReportRepository,
     private val reportService: ReportService,
-    private val userRepository: UserRepository
 ) {
     private val reportMapper = Mappers.getMapper(ReportMapper::class.java)
 
@@ -29,15 +27,10 @@ class ReportController(
     @GetMapping("/reports")
     fun getReports(
         pageable: Pageable
-    ): ResponseEntity<ApiResponse<Page<Report>>> {
-        return try {
-            // Get user from security context
-            val reports = reportRepository.findAll(pageable)
-            ResponseEntity.ok(ApiResponse(reports, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.badRequest().body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
-        }
-    }
+    ) = runCatching {
+        // TODO: Get user from security context
+        reportRepository.findAll(pageable).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 
     /**
      * Get reports by Reported By
@@ -46,14 +39,11 @@ class ReportController(
     fun getReportsBy(
         @PathVariable userUuid: String,
         pageable: Pageable
-    ): ResponseEntity<ApiResponse<Page<Report>>> {
-        return try {
-            val user = userRepository.findByUserUuid(userUuid).orElseThrow()
-            val reports = reportRepository.findAllByReportedBy(user.id, pageable)
-            ResponseEntity.ok(ApiResponse(reports, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.badRequest().body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
-        }
+    ) = runCatching {
+        val reports = reportRepository.findAllByReportedBy(userUuid.user.id, pageable)
+        ResponseEntity.ok(ApiResponse(reports, listOf()))
+    }.getOrElse {
+        it.toErrorResponse()
     }
 
     /**
@@ -73,16 +63,11 @@ class ReportController(
     @PostMapping("/create-report")
     fun createReport(
         @RequestBody req: ReportCreateRequest
-    ): ResponseEntity<ApiResponse<Nothing>> {
-        return try {
-            val report = Report()
-            reportMapper.createReportFromRequest(req, report)
-            reportService.saveReport(report)
-            ResponseEntity.ok(ApiResponse(null, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.badRequest().body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR.name, null))))
-        }
-    }
+    ) = runCatching {
+        val report = Report()
+        reportMapper.createReportFromRequest(req, report)
+        reportService.saveReport(report).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 
     /**
      * Update Report Data Class
@@ -102,15 +87,9 @@ class ReportController(
     fun updateReport(
         @PathVariable reportId: Long,
         @RequestBody req: ReportUpdateRequest
-    ): ResponseEntity<ApiResponse<Nothing>> {
-        return try {
-            val report = reportRepository.findById(reportId).orElseThrow()
-            reportMapper.updateReportFromRequest(req, report)
-            reportRepository.save(report)
-            ResponseEntity.ok(ApiResponse(null, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.badRequest()
-                .body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR_UPDATE.name, null))))
-        }
-    }
+    ) = runCatching {
+        val report = reportRepository.findById(reportId).orElseThrow()
+        reportMapper.updateReportFromRequest(req, report)
+        reportRepository.save(report).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 }
