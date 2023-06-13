fec398fb9e004e024a7c9ebc6f96d85f74e3f4c3
Raphile14 <rllddalangin@gmail.com>
2023-06-13 14:29:00 +0800

    refactor(admin): updated UserLoginLogGQLController.kt to follow Kotlin Idiomatic Coding

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/log/UserLoginLogGQLController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/log/UserLoginLogGQLController.kt
@@ -1,10 +1,8 @@
 package com.chatchatabc.parking.admin.application.graphql.log
 
-import com.chatchatabc.parking.admin.application.dto.PageInfo
-import com.chatchatabc.parking.admin.application.dto.PagedResponse
-import com.chatchatabc.parking.domain.model.log.UserLoginLog
-import com.chatchatabc.parking.domain.repository.UserRepository
 import com.chatchatabc.parking.domain.repository.log.UserLoginLogRepository
+import com.chatchatabc.parking.user
+import com.chatchatabc.parking.web.common.toPagedResponse
 import org.springframework.data.domain.PageRequest
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -13,7 +11,6 @@ import org.springframework.stereotype.Controller
 @Controller
 class UserLoginLogGQLController(
     private val userLoginLogRepository: UserLoginLogRepository,
-    private val userRepository: UserRepository
 ) {
     /**
      * Get user login logs
@@ -22,90 +19,21 @@ class UserLoginLogGQLController(
     fun getUserLoginLogs(
         @Argument page: Int,
         @Argument size: Int
-    ): PagedResponse<UserLoginLog> {
+    ) = run {
         val pr = PageRequest.of(page, size)
-        val logs = userLoginLogRepository.findAll(pr)
-        return PagedResponse(
-            logs.content,
-            PageInfo(
-                logs.size,
-                logs.totalElements,
-                logs.isFirst,
-                logs.isLast,
-                logs.isEmpty
-            )
-        )
+        userLoginLogRepository.findAll(pr).toPagedResponse()
     }
 
     /**
-     * Get user login logs per user
+     * Get user login logs by identifier
      */
     @QueryMapping
-    fun getUserLoginLogsByUser(
+    fun getUserLoginLogs(
         @Argument page: Int,
         @Argument size: Int,
-        @Argument uuid: String
-    ): PagedResponse<UserLoginLog> {
+        @Argument id: String
+    ) = run {
         val pr = PageRequest.of(page, size)
-        val user = userRepository.findByUserUuid(uuid).get()
-        val logs = userLoginLogRepository.findByUser(user.id, pr)
-        return PagedResponse(
-            logs.content,
-            PageInfo(
-                logs.size,
-                logs.totalElements,
-                logs.isFirst,
-                logs.isLast,
-                logs.isEmpty
-            )
-        )
-    }
-
-    /**
-     * Get user login logs per username
-     */
-    @QueryMapping
-    fun getUserLoginLogsByUsername(
-        @Argument page: Int,
-        @Argument size: Int,
-        @Argument username: String
-    ): PagedResponse<UserLoginLog> {
-        val pr = PageRequest.of(page, size)
-        val user = userRepository.findByUsername(username).get()
-        val logs = userLoginLogRepository.findByUser(user.id, pr)
-        return PagedResponse(
-            logs.content,
-            PageInfo(
-                logs.size,
-                logs.totalElements,
-                logs.isFirst,
-                logs.isLast,
-                logs.isEmpty
-            )
-        )
-    }
-
-    /**
-     * Get user login logs per phone
-     */
-    @QueryMapping
-    fun getUserLoginLogsByPhone(
-        @Argument page: Int,
-        @Argument size: Int,
-        @Argument phone: String
-    ): PagedResponse<UserLoginLog> {
-        val pr = PageRequest.of(page, size)
-        val user = userRepository.findByPhone(phone).get()
-        val logs = userLoginLogRepository.findByUser(user.id, pr)
-        return PagedResponse(
-            logs.content,
-            PageInfo(
-                logs.size,
-                logs.totalElements,
-                logs.isFirst,
-                logs.isLast,
-                logs.isEmpty
-            )
-        )
+        userLoginLogRepository.findByUser(id.user.id, pr).toPagedResponse()
     }
 }
