56ebffeca8509a3298037ad0e74ce3ebeab1aa73
Raphile14 <rllddalangin@gmail.com>
2023-06-13 10:53:38 +0800

    refactor(domain): modified output data type of method extensions

--- parking-domain/src/main/kotlin/com/chatchatabc/parking/Util.kt
+++ parking-domain/src/main/kotlin/com/chatchatabc/parking/Util.kt
@@ -1,17 +1,43 @@
 package com.chatchatabc.parking
 
 import com.chatchatabc.parking.domain.SpringContextUtils
+import com.chatchatabc.parking.domain.model.ParkingLot
 import com.chatchatabc.parking.domain.model.User
 import java.util.*
 
-val String.user: Optional<User>
+val String.user: User
     get() {
+        // TODO: Add custom errors
         if (this.contains("@")) {
-            return SpringContextUtils.getUserRepository().findByEmail(this)
+            return SpringContextUtils.getUserRepository().findByEmail(this).orElseThrow()
         } else if (this.contains("-")) {
-            return SpringContextUtils.getUserRepository().findByUserUuid(this)
+            return SpringContextUtils.getUserRepository().findByUserUuid(this).orElseThrow()
         } else if (this.matches(Regex("^\\+?[0-9]\\d{1,14}\$"))) {
-            return SpringContextUtils.getUserRepository().findByPhone(this)
+            return SpringContextUtils.getUserRepository().findByPhone(this).orElseThrow()
         }
-        return SpringContextUtils.getUserRepository().findByUsername(this)
+        return SpringContextUtils.getUserRepository().findByUsername(this).orElseThrow()
+    }
+
+val String.parkingLot: ParkingLot
+    get() {
+        // TODO: Add custom errors
+        return SpringContextUtils.getParkingLotRepository().findByParkingLotUuid(this).orElseThrow()
+    }
+
+val Long.parkingLot: ParkingLot
+    get() {
+        // TODO: Add custom errors
+        return SpringContextUtils.getParkingLotRepository().findById(this).orElseThrow()
+    }
+
+val String.parkingLotByOwner: ParkingLot
+    get() {
+        // TODO: Add custom errors
+        return SpringContextUtils.getParkingLotRepository().findByOwnerUuid(this).orElseThrow()
+    }
+
+val Long.parkingLotByOwner: ParkingLot
+    get() {
+        // TODO: Add custom errors
+        return SpringContextUtils.getParkingLotRepository().findByOwner(this).orElseThrow()
     }
