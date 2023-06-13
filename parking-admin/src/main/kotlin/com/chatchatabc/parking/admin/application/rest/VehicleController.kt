efeb6129fa8dcecfcfc9f75b95d20650c193c205
Raphile14 <rllddalangin@gmail.com>
2023-06-13 12:00:31 +0800

    refactor(admin): updated VehicleController.kt to follow Kotlin Idiomatic Coding

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleController.kt
@@ -1,14 +1,12 @@
 package com.chatchatabc.parking.admin.application.rest
 
-import com.chatchatabc.parking.admin.application.dto.ApiResponse
-import com.chatchatabc.parking.admin.application.dto.ErrorElement
 import com.chatchatabc.parking.admin.application.mapper.VehicleMapper
-import com.chatchatabc.parking.domain.enums.ResponseNames
-import com.chatchatabc.parking.domain.model.Vehicle
 import com.chatchatabc.parking.domain.repository.VehicleRepository
 import com.chatchatabc.parking.domain.service.VehicleService
+import com.chatchatabc.parking.vehicle
+import com.chatchatabc.parking.web.common.toErrorResponse
+import com.chatchatabc.parking.web.common.toResponse
 import org.mapstruct.factory.Mappers
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 
@@ -36,16 +34,10 @@ class VehicleController(
     fun registerVehicle(
         @PathVariable userUuid: String,
         @RequestBody req: VehicleRegisterRequest
+    ) = runCatching {
+        vehicleService.registerVehicle(userUuid, req.name, req.plateNumber, req.type).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 
-    ): ResponseEntity<ApiResponse<Vehicle>> {
-        return try {
-            val vehicle = vehicleService.registerVehicle(userUuid, req.name, req.plateNumber, req.type)
-            ResponseEntity.ok(ApiResponse(vehicle, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.badRequest()
-                .body(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR_CREATE.name, null))))
-        }
-    }
 
     /**
      * Update vehicle request
@@ -64,14 +56,9 @@ class VehicleController(
         @PathVariable vehicleUuid: String,
         @RequestBody req: VehicleUpdateRequest,
         principal: Principal
-    ): ResponseEntity<ApiResponse<Nothing>> {
-        return try {
-            val vehicle = vehicleRepository.findByVehicleUuid(vehicleUuid).get()
-            vehicleMapper.updateVehicleFromUpdateRequest(req, vehicle)
-            vehicleService.updateVehicle(vehicle)
-            ResponseEntity.ok(ApiResponse(null, listOf()))
-        } catch (e: Exception) {
-            ResponseEntity.ok(ApiResponse(null, listOf(ErrorElement(ResponseNames.ERROR_UPDATE.name, null))))
-        }
-    }
+    ) = runCatching {
+        val vehicle = vehicleUuid.vehicle
+        vehicleMapper.updateVehicleFromUpdateRequest(req, vehicle)
+        vehicleService.updateVehicle(vehicle).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 }
