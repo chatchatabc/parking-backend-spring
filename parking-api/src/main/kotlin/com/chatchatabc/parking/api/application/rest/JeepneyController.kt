0ebd2a5b7802cef60c51cd74596ac9df46c2dc88
Raphile14 <rllddalangin@gmail.com>
2023-06-14 17:12:35 +0800

    feat(api): implemented get all jeepneys api

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/JeepneyController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/JeepneyController.kt
@@ -1,8 +1,10 @@
 package com.chatchatabc.parking.api.application.rest
 
 import com.chatchatabc.parking.domain.jeepney
+import com.chatchatabc.parking.domain.repository.JeepneyRepository
 import com.chatchatabc.parking.web.common.application.toErrorResponse
 import com.chatchatabc.parking.web.common.application.toResponse
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,8 +12,24 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/jeepney")
-class JeepneyController {
+class JeepneyController(
+    private val jeepneyRepository: JeepneyRepository
+) {
+
+    /**
+     * Get Jeepney by Identifier
+     */
     @GetMapping("/{id}")
     fun getJeepney(@PathVariable id: String) =
         runCatching { id.jeepney.toResponse() }.getOrElse { it.toErrorResponse() }
+
+    /**
+     * Get all Jeepneys
+     */
+    @GetMapping("/all")
+    fun getAllJeepneys(
+        pageable: Pageable
+    ) = runCatching {
+        jeepneyRepository.findAll(pageable).toResponse()
+    }.getOrElse { it.toErrorResponse() }
 }
