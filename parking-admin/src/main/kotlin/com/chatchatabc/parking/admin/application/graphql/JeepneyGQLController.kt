f49a64e8c7f20ad2987a091571890b99794299ee
Raphile14 <rllddalangin@gmail.com>
2023-06-14 13:19:30 +0800

    feat: implemented getJeepneys GQL API

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/JeepneyGQLController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/JeepneyGQLController.kt
@@ -1,7 +1,36 @@
 package com.chatchatabc.parking.admin.application.graphql
 
+import com.chatchatabc.parking.domain.repository.JeepneyRepository
+import com.chatchatabc.parking.domain.specification.JeepneySpecification
+import com.chatchatabc.parking.web.common.application.toPagedResponse
+import org.springframework.data.domain.PageRequest
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class JeepneyGQLController {
+class JeepneyGQLController(
+    private val jeepneyRepository: JeepneyRepository
+) {
+    /**
+     * Get Jeepneys
+     */
+    @QueryMapping
+    fun getJeepneys(
+        @Argument page: Int,
+        @Argument size: Int,
+        @Argument keyword: String?,
+        @Argument sortField: String? = null,
+        @Argument sortBy: Int? = null,
+    ) = run {
+        val pr = PageRequest.of(page, size)
+        var spec = JeepneySpecification.withKeyword(keyword ?: "")
+
+        // Sort
+        if (sortField != null && sortBy != null) {
+            spec = spec.and(JeepneySpecification.sortBy(sortField, sortBy))
+        }
+
+        jeepneyRepository.findAll(spec, pr).toPagedResponse()
+    }
 }
