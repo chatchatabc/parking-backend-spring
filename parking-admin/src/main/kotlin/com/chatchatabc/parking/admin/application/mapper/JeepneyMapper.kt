8d36000393298bcec5f065e5c4dedb2832fe719e
Raphile14 <rllddalangin@gmail.com>
2023-06-14 11:48:47 +0800

    feat: implemented updateJeepneyFromUpdateRequest

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/mapper/JeepneyMapper.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/mapper/JeepneyMapper.kt
@@ -26,4 +26,21 @@ interface JeepneyMapper {
         request: JeepneyController.JeepneyCreateRequest,
         @MappingTarget jeepney: Jeepney
     )
+
+    /**
+     * Jeepney Update Mapper
+     */
+    @Mappings(
+        Mapping(target = "name", source = "request.name"),
+        Mapping(target = "plateNumber", source = "request.plateNumber"),
+        Mapping(target = "capacity", source = "request.capacity"),
+        Mapping(target = "latitude", source = "request.latitude"),
+        Mapping(target = "longitude", source = "request.longitude"),
+        Mapping(target = "status", source = "request.status"),
+        Mapping(target = "flag", source = "request.flag"),
+    )
+    fun updateJeepneyFromUpdateRequest(
+        request: JeepneyController.JeepneyUpdateRequest,
+        @MappingTarget jeepney: Jeepney
+    )
 }
