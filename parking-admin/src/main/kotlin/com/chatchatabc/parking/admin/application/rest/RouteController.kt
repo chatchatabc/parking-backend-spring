66eeee795f1553452b6d24b6af830784402b1370
Raphile14 <rllddalangin@gmail.com>
2023-06-14 11:08:16 +0800

    feat(admin): created RouteController.kt

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/RouteController.kt
@@ -0,0 +1,9 @@
+package com.chatchatabc.parking.admin.application.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/route")
+class RouteController {
+}
