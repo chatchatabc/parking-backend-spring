35ead3579dec021f194f41b690f6aeb24103d16a
Raphile14 <rllddalangin@gmail.com>
2023-06-14 11:27:46 +0800

    feat(admin): created JeepneyMapper.kt and createJeepneyFromCreateRequest

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/mapper/JeepneyMapper.kt
@@ -0,0 +1,29 @@
+package com.chatchatabc.parking.admin.application.mapper
+
+import com.chatchatabc.parking.admin.application.rest.JeepneyController
+import com.chatchatabc.parking.domain.model.Jeepney
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.MappingTarget
+import org.mapstruct.Mappings
+
+@Mapper(componentModel = "spring")
+interface JeepneyMapper {
+
+    /**
+     * Jeepney Create Mapper
+     */
+    @Mappings(
+        Mapping(target = "name", source = "request.name"),
+        Mapping(target = "plateNumber", source = "request.plateNumber"),
+        Mapping(target = "capacity", source = "request.capacity"),
+        Mapping(target = "latitude", source = "request.latitude"),
+        Mapping(target = "longitude", source = "request.longitude"),
+        Mapping(target = "status", source = "request.status"),
+        Mapping(target = "flag", source = "request.flag"),
+    )
+    fun createJeepneyFromCreateRequest(
+        request: JeepneyController.JeepneyCreateRequest,
+        @MappingTarget jeepney: Jeepney
+    )
+}
