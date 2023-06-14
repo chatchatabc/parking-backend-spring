d986323c832ad7252a96344d813184a06a808aa1
Raphile14 <rllddalangin@gmail.com>
2023-06-14 10:41:44 +0800

    feat: created RouteServiceImpl.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/RouteServiceImpl.java
@@ -0,0 +1,8 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import com.chatchatabc.parking.domain.service.RouteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RouteServiceImpl implements RouteService {
+}
