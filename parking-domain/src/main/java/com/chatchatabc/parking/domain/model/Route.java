55bfd5ad9e6d3fa46d893d97637d875febc7d6f7
Raphile14 <rllddalangin@gmail.com>
2023-06-14 10:32:38 +0800

    feat: created Route.java model

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/Route.java
@@ -0,0 +1,48 @@
+package com.chatchatabc.parking.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "route")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Route extends FlagEntity {
+    public static class RouteStatus {
+        public static final int INACTIVE = -1;
+        public static final int DRAFT = 0;
+        public static final int ACTIVE = 1;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String routeUuid = UUID.randomUUID().toString();
+
+    @Column
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column
+    private Integer status = RouteStatus.DRAFT;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
