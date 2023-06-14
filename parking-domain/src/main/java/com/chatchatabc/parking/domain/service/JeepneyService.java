2bc8917b0d3e8be2b22e1ca888af7ed3a714b630
Raphile14 <rllddalangin@gmail.com>
2023-06-14 11:26:57 +0800

    feat(domain): implemented saveJeepney

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/JeepneyService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/JeepneyService.java
@@ -1,4 +1,12 @@
 package com.chatchatabc.parking.domain.service;
 
+import com.chatchatabc.parking.domain.model.Jeepney;
+
 public interface JeepneyService {
+    /**
+     * Save jeepney
+     *
+     * @param jeepney jeepney to save
+     */
+    void saveJeepney(Jeepney jeepney);
 }
--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/JeepneyServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/JeepneyServiceImpl.java
@@ -1,8 +1,25 @@
 package com.chatchatabc.parking.impl.domain.service;
 
+import com.chatchatabc.parking.domain.model.Jeepney;
+import com.chatchatabc.parking.domain.repository.JeepneyRepository;
 import com.chatchatabc.parking.domain.service.JeepneyService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JeepneyServiceImpl implements JeepneyService {
+    private final JeepneyRepository jeepneyRepository;
+
+    public JeepneyServiceImpl(JeepneyRepository jeepneyRepository) {
+        this.jeepneyRepository = jeepneyRepository;
+    }
+
+    /**
+     * Save jeepney
+     *
+     * @param jeepney jeepney to save
+     */
+    @Override
+    public void saveJeepney(Jeepney jeepney) {
+        jeepneyRepository.save(jeepney);
+    }
 }
