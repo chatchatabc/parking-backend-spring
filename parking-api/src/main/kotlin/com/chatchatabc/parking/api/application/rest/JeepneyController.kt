a51e0f02237f0ebe56aadb58b5eecc09c219b479
Raphile14 <rllddalangin@gmail.com>
2023-06-14 17:10:00 +0800

    feat(api): implemented get jeepney by identifier

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/JeepneyController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/JeepneyController.kt
@@ -1,9 +1,17 @@
 package com.chatchatabc.parking.api.application.rest
 
+import com.chatchatabc.parking.domain.jeepney
+import com.chatchatabc.parking.web.common.application.toErrorResponse
+import com.chatchatabc.parking.web.common.application.toResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/jeepney")
 class JeepneyController {
+    @GetMapping("/{id}")
+    fun getJeepney(@PathVariable id: String) =
+        runCatching { id.jeepney.toResponse() }.getOrElse { it.toErrorResponse() }
 }
