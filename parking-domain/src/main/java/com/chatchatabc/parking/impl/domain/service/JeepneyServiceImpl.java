8ca4f8758691672171dbc7ad5b83efd29cb38d6d
Raphile14 <rllddalangin@gmail.com>
2023-06-14 10:41:40 +0800

    feat: created JeepneyServiceImpl.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/JeepneyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import com.chatchatabc.parking.domain.service.JeepneyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JeepneyServiceImpl implements JeepneyService {
+}
