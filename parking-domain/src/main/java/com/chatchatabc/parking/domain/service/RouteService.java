471c81ab5c6073bdb6dc9760ac81f1d19dc04582
Raphile14 <rllddalangin@gmail.com>
2023-06-15 17:57:35 +0800

    feat(domain): implemented saveRoute

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/RouteService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/RouteService.java
@@ -1,4 +1,13 @@
 package com.chatchatabc.parking.domain.service;
 
+import com.chatchatabc.parking.domain.model.Route;
+
 public interface RouteService {
+
+    /**
+     * Save route
+     *
+     * @param route route
+     */
+    void saveRoute(Route route);
 }
--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/RouteServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/RouteServiceImpl.java
@@ -1,8 +1,25 @@
 package com.chatchatabc.parking.impl.domain.service;
 
+import com.chatchatabc.parking.domain.model.Route;
+import com.chatchatabc.parking.domain.repository.RouteRepository;
 import com.chatchatabc.parking.domain.service.RouteService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RouteServiceImpl implements RouteService {
+    private final RouteRepository routeRepository;
+
+    public RouteServiceImpl(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
+    }
+
+    /**
+     * Save route
+     *
+     * @param route route
+     */
+    @Override
+    public void saveRoute(Route route) {
+        routeRepository.save(route);
+    }
 }
