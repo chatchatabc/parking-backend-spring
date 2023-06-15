0095184d81f8c628b375dcecdeccd45c75eec02a
Raphile14 <rllddalangin@gmail.com>
2023-06-15 13:55:37 +0800

    feat(api): implemented getAllJeepneysByRoute and updateJeepneyLocation

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/JeepneyController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/JeepneyController.kt
@@ -2,18 +2,21 @@ package com.chatchatabc.parking.api.application.rest
 
 import com.chatchatabc.parking.domain.jeepney
 import com.chatchatabc.parking.domain.repository.JeepneyRepository
+import com.chatchatabc.parking.web.common.application.NatsMessage
+import com.chatchatabc.parking.web.common.application.enums.NatsPayloadTypes
+import com.chatchatabc.parking.web.common.application.nats.NatsPayload
 import com.chatchatabc.parking.web.common.application.toErrorResponse
+import com.chatchatabc.parking.web.common.application.toJson
 import com.chatchatabc.parking.web.common.application.toResponse
+import io.nats.client.Connection
 import org.springframework.data.domain.Pageable
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/jeepney")
 class JeepneyController(
-    private val jeepneyRepository: JeepneyRepository
+    private val jeepneyRepository: JeepneyRepository,
+    private val natsConnection: Connection
 ) {
 
     /**
@@ -32,4 +35,53 @@ class JeepneyController(
     ) = runCatching {
         jeepneyRepository.findAll(pageable).toResponse()
     }.getOrElse { it.toErrorResponse() }
+
+    /**
+     * Get All Jeepneys by Routes
+     */
+    @GetMapping("/route/{id}")
+    fun getAllJeepneysByRoute(
+        @PathVariable id: String,
+        pageable: Pageable
+    ) = runCatching {
+        // TODO: Convert to method extension
+        jeepneyRepository.findAllByRouteUuid(id, pageable).toResponse()
+    }.getOrElse { it.toErrorResponse() }
+
+    data class JeepneyUpdateLocationRequest(
+        val latitude: Double,
+        val longitude: Double
+    )
+
+    /**
+     * Update Jeepney Location
+     */
+    @PutMapping("/update-location/{id}")
+    fun updateJeepneyLocation(
+        @PathVariable id: String,
+        request: JeepneyUpdateLocationRequest
+    ) = runCatching {
+        val jeepney = id.jeepney.apply {
+            latitude = request.latitude
+            longitude = request.longitude
+        }
+        // TODO: Change to service
+        jeepneyRepository.save(jeepney)
+
+        // NATS message structure
+        val natsMessage =
+            NatsMessage(
+                NatsPayloadTypes.JEEPNEY_LOCATION_UPDATE,
+                NatsPayload.JeepneyPayload(
+                    jeepney.jeepneyUuid,
+                    jeepney.latitude,
+                    jeepney.longitude
+                )
+            ).toJson().toByteArray()
+
+        // Publish to all Clients Listening
+        natsConnection.publish("route-${jeepney.routeUuid}", natsMessage)
+        jeepney.toResponse()
+    }.getOrElse { it.toErrorResponse() }
+
 }
