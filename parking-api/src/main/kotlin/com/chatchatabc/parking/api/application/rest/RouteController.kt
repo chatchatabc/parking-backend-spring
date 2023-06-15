46ff5d2db34e32ad94ca1cd32cf69be9625a9f82
Raphile14 <rllddalangin@gmail.com>
2023-06-15 17:18:34 +0800

    feat(api): implemented getLastUpdatedAtOfRoute

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/RouteController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/RouteController.kt
@@ -1,12 +1,16 @@
 package com.chatchatabc.parking.api.application.rest
 
 import com.chatchatabc.parking.domain.repository.RouteRepository
+import com.chatchatabc.parking.domain.route
 import com.chatchatabc.parking.web.common.application.toErrorResponse
+import com.chatchatabc.parking.web.common.application.toNullResponse
 import com.chatchatabc.parking.web.common.application.toResponse
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/route")
@@ -20,4 +24,19 @@ class RouteController(
     @GetMapping("/")
     fun getAllRoutes(pageable: Pageable) =
         runCatching { routeRepository.findAll(pageable).toResponse() }.getOrElse { it.toErrorResponse() }
+
+    /**
+     * Check last updated at of a route
+     */
+    @GetMapping("/last-updated-at/{id}/{timestamp}")
+    fun getLastUpdatedAtOfRoute(@PathVariable id: String, @PathVariable timestamp: LocalDateTime) =
+        runCatching {
+            val route = id.route
+            if (route.updatedAt.isAfter(timestamp)) {
+                route.toResponse()
+            } else {
+                // Return empty response
+                route.toNullResponse()
+            }
+        }.getOrElse { it.toErrorResponse() }
 }
