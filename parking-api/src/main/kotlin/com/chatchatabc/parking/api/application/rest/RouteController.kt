8f447038953583bed76423bc7a7816c365bd8876
Raphile14 <rllddalangin@gmail.com>
2023-06-15 13:55:59 +0800

    feat(api): implemented getAllRoutes

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/RouteController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/RouteController.kt
@@ -1,9 +1,23 @@
 package com.chatchatabc.parking.api.application.rest
 
+import com.chatchatabc.parking.domain.repository.RouteRepository
+import com.chatchatabc.parking.web.common.application.toErrorResponse
+import com.chatchatabc.parking.web.common.application.toResponse
+import org.springframework.data.domain.Pageable
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/route")
-class RouteController {
+class RouteController(
+    private val routeRepository: RouteRepository
+) {
+
+    /**
+     * Get All Routes
+     */
+    @GetMapping("/")
+    fun getAllRoutes(pageable: Pageable) =
+        runCatching { routeRepository.findAll(pageable).toResponse() }.getOrElse { it.toErrorResponse() }
 }
