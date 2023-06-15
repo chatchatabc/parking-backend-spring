075c1f076bd21df2beea58b1608d24fd7f372ba0
Raphile14 <rllddalangin@gmail.com>
2023-06-15 17:57:48 +0800

    test(domain): created RouteServiceImplTest.java and implemented testSaveRoute_ShouldBeSuccessful

---
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/RouteServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import com.chatchatabc.parking.TestContainersBaseTest;
+import com.chatchatabc.parking.domain.model.Route;
+import com.chatchatabc.parking.domain.repository.RouteRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class RouteServiceImplTest extends TestContainersBaseTest {
+    @Autowired
+    private RouteServiceImpl routeService;
+    @Autowired
+    private RouteRepository routeRepository;
+
+    @Test
+    void testSaveRoute_ShouldBeSuccessful() {
+        Long count = routeRepository.count();
+
+        Route route = new Route();
+        route.setName("route-name");
+        route.setDescription("description");
+        route.setStatus(Route.RouteStatus.DRAFT);
+        route.setPoints("points");
+
+        routeService.saveRoute(route);
+        assertThat(routeRepository.count()).isGreaterThan(count);
+    }
+}
