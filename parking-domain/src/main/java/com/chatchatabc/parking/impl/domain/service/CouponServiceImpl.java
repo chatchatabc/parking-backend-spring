fed8a89b5cee12a242ddd1b2c97403cfeaf9cbf7
Raphile14 <rllddalangin@gmail.com>
2023-06-19 14:39:39 +0800

    feat: created initial CouponService.java and CouponServiceImpl.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/CouponServiceImpl.java
@@ -0,0 +1,8 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import com.chatchatabc.parking.domain.service.CouponService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CouponServiceImpl implements CouponService {
+}
