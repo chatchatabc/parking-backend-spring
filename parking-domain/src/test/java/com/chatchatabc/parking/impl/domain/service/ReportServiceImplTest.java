2d3d86e99caf6ff4fbdcf2a19204c3a32725079b
Raphile14 <rllddalangin@gmail.com>
2023-06-19 11:35:50 +0800

    test(domain): moved testSaveReport_ShouldSaveSuccessfully from ReportServiceTest

--- parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/ReportServiceImplTest.java
+++ parking-domain/src/test/java/com/chatchatabc/parking/impl/domain/service/ReportServiceImplTest.java
@@ -1,5 +1,30 @@
 package com.chatchatabc.parking.impl.domain.service;
 
-class ReportServiceImplTest {
+import com.chatchatabc.parking.TestContainersBaseTest;
+import com.chatchatabc.parking.domain.model.Report;
+import com.chatchatabc.parking.domain.repository.ReportRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ReportServiceImplTest extends TestContainersBaseTest {
+    @Autowired
+    private ReportServiceImpl reportService;
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Test
+    void testSaveReport_ShouldSaveSuccessfully() {
+        Long currentCount = reportRepository.count();
+        Report report = new Report();
+        report.setName("Test Report");
+        report.setDescription("Test Description");
+        report.setPlateNumber("Test Plate Number");
+        report.setLatitude(1.0);
+        report.setLongitude(1.0);
+        report.setReportedBy(1L);
+        reportService.saveReport(report);
+        assertThat(reportRepository.count()).isGreaterThan(currentCount);
+    }
 }
