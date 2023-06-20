2800bfa2f293bdf5dfeb379918573f708912fc50
Raphile14 <rllddalangin@gmail.com>
2023-06-20 15:22:04 +0800

    feat(domain): created VehicleTypeService.java and VehicleTypeServiceImpl.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/VehicleTypeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import com.chatchatabc.parking.domain.service.VehicleTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleTypeServiceImpl implements VehicleTypeService {
+}
