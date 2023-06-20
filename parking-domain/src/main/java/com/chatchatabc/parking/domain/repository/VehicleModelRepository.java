c03bf4dab219a6ac476c9a59ae184f6f76183d3b
Raphile14 <rllddalangin@gmail.com>
2023-06-20 16:50:27 +0800

    feat(domain): implemented findByModelUuid and findAllByBrandUuid

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleModelRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleModelRepository.java
@@ -1,7 +1,30 @@
 package com.chatchatabc.parking.domain.repository;
 
+import com.chatchatabc.parking.domain.model.VehicleModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface VehicleModelRepository {
+public interface VehicleModelRepository extends JpaRepository<VehicleModel, Long> {
+
+    /**
+     * Find Vehicle Model by Model UUID
+     *
+     * @param modelUuid Model UUID
+     * @return Vehicle Model
+     */
+    Optional<VehicleModel> findByModelUuid(String modelUuid);
+
+    /**
+     * Find Vehicle Model by Brand UUID
+     *
+     * @param brandUuid Brand UUID
+     * @param pageable  Pageable
+     * @return Page of Vehicle Model
+     */
+    Page<VehicleModel> findAllByBrandUuid(String brandUuid, Pageable pageable);
 }
