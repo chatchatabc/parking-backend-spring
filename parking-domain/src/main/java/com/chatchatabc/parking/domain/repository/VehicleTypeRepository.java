c6844573bf27b0df092aa6ef142df08789c18edf
Raphile14 <rllddalangin@gmail.com>
2023-06-20 16:05:19 +0800

    chore(domain): extended JpaSpecificationExecutor to VehicleTypeRepository.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleTypeRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleTypeRepository.java
@@ -2,12 +2,13 @@ package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.domain.model.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface VehicleTypeRepository extends JpaRepository<VehicleType, Long> {
+public interface VehicleTypeRepository extends JpaRepository<VehicleType, Long>, JpaSpecificationExecutor<VehicleType> {
 
     /**
      * Find Vehicle Type by Type UUID
