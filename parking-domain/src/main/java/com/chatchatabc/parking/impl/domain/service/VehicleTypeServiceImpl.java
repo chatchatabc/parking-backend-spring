5c4fce2f5762c1c564feb29e4869226ec2e5d262
Raphile14 <rllddalangin@gmail.com>
2023-06-20 15:28:42 +0800

    feat(domain): implemented saveVehicleType

--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/VehicleTypeServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/VehicleTypeServiceImpl.java
@@ -1,8 +1,25 @@
 package com.chatchatabc.parking.impl.domain.service;
 
+import com.chatchatabc.parking.domain.model.VehicleType;
+import com.chatchatabc.parking.domain.repository.VehicleTypeRepository;
 import com.chatchatabc.parking.domain.service.VehicleTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleTypeServiceImpl implements VehicleTypeService {
+    private final VehicleTypeRepository vehicleTypeRepository;
+
+    public VehicleTypeServiceImpl(VehicleTypeRepository vehicleTypeRepository) {
+        this.vehicleTypeRepository = vehicleTypeRepository;
+    }
+
+    /**
+     * Save Vehicle Type
+     *
+     * @param vehicleType Vehicle Type
+     */
+    @Override
+    public void saveVehicleType(VehicleType vehicleType) {
+        vehicleTypeRepository.save(vehicleType);
+    }
 }
