dfc686017d400916745896e73e2848d533b62368
Raphile14 <rllddalangin@gmail.com>
2023-06-20 13:02:18 +0800

    feat(admin): created VehicleBrandController.kt and implemented createVehicleBrand API

---
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleBrandController.kt
@@ -0,0 +1,46 @@
+package com.chatchatabc.parking.admin.application.rest
+
+import com.chatchatabc.parking.admin.application.mapper.VehicleBrandMapper
+import com.chatchatabc.parking.domain.model.VehicleBrand
+import com.chatchatabc.parking.domain.service.VehicleBrandService
+import com.chatchatabc.parking.domain.user
+import com.chatchatabc.parking.web.common.application.toErrorResponse
+import org.mapstruct.factory.Mappers
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
+
+@RestController
+@RequestMapping("/api/vehicle-brand")
+class VehicleBrandController(
+    private val vehicleBrandService: VehicleBrandService
+) {
+    private val vehicleBrandMapper = Mappers.getMapper(VehicleBrandMapper::class.java)
+
+    /**
+     * Vehicle Brand Create Request Data Class
+     */
+    data class VehicleBrandCreateRequest(
+        val name: String,
+        val status: Int = 0
+    )
+
+    /**
+     * Admin create Vehicle Brand
+     */
+    @PostMapping("/create")
+    fun createVehicleBrand(
+        @RequestBody req: VehicleBrandCreateRequest,
+        principal: Principal
+    ) = runCatching {
+        val createdVehicleBrand = VehicleBrand().apply {
+            this.createdBy = principal.name.user.id
+        }
+        vehicleBrandMapper.createVehicleBrandFromCreateRequest(req, createdVehicleBrand)
+        vehicleBrandService.saveVehicleBrand(createdVehicleBrand)
+    }.getOrElse { it.toErrorResponse() }
+
+    // TODO: Update Brand
+}
