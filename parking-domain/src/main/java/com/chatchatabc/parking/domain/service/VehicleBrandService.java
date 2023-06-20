96c4839e78d525a7e87eb4777768f4fc91dd1f88
Raphile14 <rllddalangin@gmail.com>
2023-06-20 12:01:48 +0800

    feat(domain): implemented saveVehicleBrand function

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/VehicleBrandService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/VehicleBrandService.java
@@ -1,4 +1,13 @@
 package com.chatchatabc.parking.domain.service;
 
+import com.chatchatabc.parking.domain.model.VehicleBrand;
+
 public interface VehicleBrandService {
+
+    /**
+     * Save Vehicle Brand
+     *
+     * @param vehicleBrand Vehicle Brand
+     */
+    void saveVehicleBrand(VehicleBrand vehicleBrand);
 }
