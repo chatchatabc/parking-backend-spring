5933cb2b827a523febd6ce8b20b135546ac9ecc9
Raphile14 <rllddalangin@gmail.com>
2023-06-20 13:21:50 +0800

    feat(domain): implemented findByBrandUuid

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleBrandRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleBrandRepository.java
@@ -4,6 +4,16 @@ import com.chatchatabc.parking.domain.model.VehicleBrand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VehicleBrandRepository extends JpaRepository<VehicleBrand, Long> {
+
+    /**
+     * Find Vehicle Brand by Brand UUID
+     *
+     * @param brandUuid Brand UUID
+     * @return Vehicle Brand
+     */
+    Optional<VehicleBrand> findByBrandUuid(String brandUuid);
 }
