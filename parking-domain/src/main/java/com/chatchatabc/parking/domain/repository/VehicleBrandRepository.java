e3f2fe3bb27975d46fec143dfedcbbb242bb04f3
Raphile14 <rllddalangin@gmail.com>
2023-06-20 14:14:01 +0800

    chore(admin): extended JpaSpecificationExecutor to VehicleBrandRepository.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleBrandRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/VehicleBrandRepository.java
@@ -2,12 +2,13 @@ package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.domain.model.VehicleBrand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface VehicleBrandRepository extends JpaRepository<VehicleBrand, Long> {
+public interface VehicleBrandRepository extends JpaRepository<VehicleBrand, Long>, JpaSpecificationExecutor<VehicleBrand> {
 
     /**
      * Find Vehicle Brand by Brand UUID
