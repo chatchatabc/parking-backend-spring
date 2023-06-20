71f9f37a9a3ba024d17352ee7ed524cf3d0943df
Raphile14 <rllddalangin@gmail.com>
2023-06-20 14:13:41 +0800

    feat(admin): created VehicleBrandSpecification.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/specification/VehicleBrandSpecification.java
@@ -0,0 +1,15 @@
+package com.chatchatabc.parking.domain.specification;
+
+import com.chatchatabc.parking.domain.model.VehicleBrand;
+import org.springframework.data.jpa.domain.Specification;
+
+public class VehicleBrandSpecification extends GenericSpecification<VehicleBrand> {
+    public static Specification<VehicleBrand> withKeyword(String keyword) {
+        return (root, query, builder) -> {
+            String pattern = "%" + keyword.toLowerCase() + "%";
+            return builder.or(
+                    builder.like(builder.lower(root.get("brandName")), pattern)
+            );
+        };
+    }
+}
