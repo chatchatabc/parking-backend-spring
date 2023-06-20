d5fbc22f065c489b353bd2675afc395e1ff8a26f
Raphile14 <rllddalangin@gmail.com>
2023-06-20 09:45:22 +0800

    chore: commented out some code for test

--- parking-api/src/test/kotlin/com/chatchatabc/parking/api/application/rest/AuthorizedBaseTest.kt
+++ parking-api/src/test/kotlin/com/chatchatabc/parking/api/application/rest/AuthorizedBaseTest.kt
@@ -2,38 +2,28 @@ package com.chatchatabc.parking.api.application.rest
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.chatchatabc.parking.web.common.application.config.security.SecurityConfig
 import com.chatchatabc.parking.web.common.application.config.security.filter.JwtRequestFilter
 import com.chatchatabc.parking.web.common.application.rest.service.JwtService
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import java.util.*
 import javax.sql.DataSource
 
 @ExtendWith(SpringExtension::class)
-@ContextConfiguration(classes = [SecurityConfig::class])
-@WebAppConfiguration
+@ContextConfiguration
 @WithMockUser(username = "dfc3cd78-9c89-4da2-8749-253afed080af", password = "123456", roles = ["ADMIN"])
-@ActiveProfiles("test")
 open class AuthorizedBaseTest {
-    lateinit var mvc: MockMvc
+    @Autowired
+    lateinit var mockMvc: MockMvc
 
     @Autowired
     lateinit var context: WebApplicationContext
@@ -51,13 +41,26 @@ open class AuthorizedBaseTest {
 
     val objectMapper: ObjectMapper = ObjectMapper()
 
-    @BeforeEach
-    fun setup() {
-        mvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            .apply<DefaultMockMvcBuilder>(springSecurity())
-            .build()
-    }
+//    @BeforeEach
+//    fun setup() {
+//        val mockPayload = Mockito.mock(Payload::class.java)
+//        val mockClaim = Mockito.mock(Claim::class.java)
+//
+//        Mockito.`when`(mockPayload.getSubject()).thenReturn("dfc3cd78-9c89-4da2-8749-253afed080af")
+//        Mockito.`when`(mockClaim.asArray(String::class.java)).thenReturn(arrayOf("ADMIN"))
+//        Mockito.`when`(mockPayload.getClaim("role")).thenReturn(mockClaim)
+//        Mockito.`when`(jwtService.validateTokenAndGetPayload(ArgumentMatchers.anyString())).thenReturn(mockPayload)
+//
+//        // Mock JwtRequestFilter
+//        val mockJwtRequestFilter = JwtRequestFilter(jwtService)
+//
+//        // Configure MockMvc
+//        mvc = MockMvcBuilders
+//            .webAppContextSetup(context)
+//            .apply<DefaultMockMvcBuilder>(springSecurity())
+//            .addFilter<DefaultMockMvcBuilder>(mockJwtRequestFilter, "/*")
+//            .build()
+//    }
 
     fun get(urlTemplate: String, vararg uriVariables: Any?): MockHttpServletRequestBuilder {
         return MockMvcRequestBuilders.get(urlTemplate, *uriVariables).header("Authorization", "Bearer $token")
@@ -71,10 +74,11 @@ open class AuthorizedBaseTest {
     open fun generateToken(): String {
         val jwtSecret = "your-hmac512-secret"
         val hmac512 = Algorithm.HMAC512(jwtSecret)
-
-        val userDetails = SecurityContextHolder.getContext().authentication.principal as UserDetails
-        val username = userDetails.username // the uuid
-        val roles = userDetails.authorities.map { it.authority }
+//
+//        val userDetails = SecurityContextHolder.getContext().authentication.principal as UserDetails
+//        val username = userDetails.username // the uuid
+        val username = "dfc3cd78-9c89-4da2-8749-253afed080af" // the uuid
+        val roles = listOf("ADMIN")
 
         return JWT.create()
             .withSubject(username)
