bf7a088024513c32f6f3960f77b12622f6f8e273
Raphile14 <rllddalangin@gmail.com>
2023-06-20 15:55:41 +0800

    feat(domain): created VehicleTypeController.kt and implemented getAll and get apis

---
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleTypeController.kt
@@ -0,0 +1,27 @@
+package com.chatchatabc.parking.api.application.rest
+
+import com.chatchatabc.parking.domain.repository.VehicleTypeRepository
+import com.chatchatabc.parking.domain.vehicleType
+import com.chatchatabc.parking.web.common.application.toResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/vehicle-type")
+class VehicleTypeController(
+    private val vehicleTypeRepository: VehicleTypeRepository
+) {
+    /**
+     * Get vehicle types
+     */
+    @GetMapping("/")
+    fun getAll() = vehicleTypeRepository.findAll().toResponse()
+
+    /**
+     * Get vehicle type by identifier
+     */
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: String) = run { id.vehicleType.toResponse() }
+}
