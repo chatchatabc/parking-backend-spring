1033ee756eb1a01986c761b746487c4eb2928637
Raphile14 <rllddalangin@gmail.com>
2023-06-20 16:56:59 +0800

    feat(domain): created VehicleModelService.java and VehicleModelServiceImpl.java and implemented saveVehicleModel

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/VehicleModelService.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.parking.domain.service;
+
+import com.chatchatabc.parking.domain.model.VehicleModel;
+
+public interface VehicleModelService {
+
+    /**
+     * Save Vehicle Model
+     *
+     * @param vehicleModel Vehicle Model
+     */
+    void saveVehicleModel(VehicleModel vehicleModel);
+}
