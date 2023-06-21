cbd8fcd65c7c87d52468971e8ef1fabd91b3e817
Raphile14 <rllddalangin@gmail.com>
2023-06-21 17:30:52 +0800

    feat(domain): implemented findAllByIdIn

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/RouteNodeRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/RouteNodeRepository.java
@@ -4,6 +4,18 @@ import com.chatchatabc.parking.domain.model.RouteNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface RouteNodeRepository extends JpaRepository<RouteNode, Long> {
+
+    /**
+     * Find all route nodes by node id
+     *
+     * @param nodeIds node ids
+     * @return list of route nodes
+     */
+    List<RouteNode> findAllByIdIn(Set<Long> nodeIds);
+
 }
