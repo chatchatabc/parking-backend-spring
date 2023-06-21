c04723144452adc49595c804d854dd74024ac528
Raphile14 <rllddalangin@gmail.com>
2023-06-21 16:32:40 +0800

    feat(domain): created RouteEdgeService.java and RouteEdgeServiceImpl.java

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/RouteEdgeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.chatchatabc.parking.impl.domain.service;
+
+import com.chatchatabc.parking.domain.service.RouteEdgeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RouteEdgeServiceImpl implements RouteEdgeService {
+}
