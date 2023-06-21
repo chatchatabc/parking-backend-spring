18fc714ef30466e5ee2d16b9cdd57f8725168ae9
Raphile14 <rllddalangin@gmail.com>
2023-06-21 11:15:27 +0800

    refactor(api): updated routes for VehicleModelController.kt

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleModelController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleModelController.kt
@@ -4,6 +4,7 @@ import com.chatchatabc.parking.domain.repository.VehicleModelRepository
 import com.chatchatabc.parking.domain.vehicleBrand
 import com.chatchatabc.parking.domain.vehicleModel
 import com.chatchatabc.parking.web.common.application.toResponse
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,18 +19,30 @@ class VehicleModelController(
     /**
      * Get Vehicle Models
      */
+    @Operation(
+        summary = "Get Vehicle Models",
+        description = "Allow users to get Vehicle Models"
+    )
     @GetMapping("/")
     fun getAll(pageable: Pageable) = vehicleModelRepository.findAll(pageable).toResponse()
 
     /**
      * Get Vehicle Model by identifier
      */
+    @Operation(
+        summary = "Get Vehicle Model by identifier",
+        description = "Allow users to get Vehicle Model by identifier"
+    )
     @GetMapping("/{id}")
     fun get(@PathVariable id: String) = run { id.vehicleModel.toResponse() }
 
     /**
      * Get Vehicle Models by Brand identifier
      */
+    @Operation(
+        summary = "Get Vehicle Models by Brand identifier",
+        description = "Allow users to get Vehicle Models by Brand identifier"
+    )
     @GetMapping("/brand/{id}")
     fun getByBrand(@PathVariable id: String, pageable: Pageable) =
         vehicleModelRepository.findAllByBrandUuid(id.vehicleBrand.brandUuid, pageable).toResponse()
