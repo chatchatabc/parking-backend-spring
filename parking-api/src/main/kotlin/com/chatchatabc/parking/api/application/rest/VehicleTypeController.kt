13050d1170ff47e215b60b8ae8f35a5116360e2c
Raphile14 <rllddalangin@gmail.com>
2023-06-21 11:15:50 +0800

    refactor(api): updated routes for VehicleTypeController.kt

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleTypeController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleTypeController.kt
@@ -3,6 +3,7 @@ package com.chatchatabc.parking.api.application.rest
 import com.chatchatabc.parking.domain.repository.VehicleTypeRepository
 import com.chatchatabc.parking.domain.vehicleType
 import com.chatchatabc.parking.web.common.application.toResponse
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,12 +18,20 @@ class VehicleTypeController(
     /**
      * Get vehicle types
      */
+    @Operation(
+        summary = "Get vehicle types",
+        description = "Allow users to get vehicle types"
+    )
     @GetMapping("/")
     fun getAll(pageable: Pageable) = vehicleTypeRepository.findAll(pageable).toResponse()
 
     /**
      * Get vehicle type by identifier
      */
+    @Operation(
+        summary = "Get vehicle type by identifier",
+        description = "Allow users to get vehicle type by identifier"
+    )
     @GetMapping("/{id}")
     fun get(@PathVariable id: String) = run { id.vehicleType.toResponse() }
 }
