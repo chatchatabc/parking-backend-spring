d2099efb3a797ab6f2a364d8171c65eb13c3af2a
Raphile14 <rllddalangin@gmail.com>
2023-06-21 17:32:04 +0800

    feat(admin): implemented getRouteNodesAndEdges GQL api

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/RouteGQLController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/graphql/RouteGQLController.kt
@@ -1,9 +1,14 @@
 package com.chatchatabc.parking.admin.application.graphql
 
+import com.chatchatabc.parking.domain.model.RouteEdge
+import com.chatchatabc.parking.domain.model.RouteNode
+import com.chatchatabc.parking.domain.repository.RouteNodeRepository
 import com.chatchatabc.parking.domain.repository.RouteRepository
 import com.chatchatabc.parking.domain.route
+import com.chatchatabc.parking.domain.routeEdges
 import com.chatchatabc.parking.domain.specification.RouteSpecification
 import com.chatchatabc.parking.web.common.application.toPagedResponse
+import com.chatchatabc.parking.web.common.application.toResponse
 import org.springframework.data.domain.PageRequest
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -11,7 +16,8 @@ import org.springframework.stereotype.Controller
 
 @Controller
 class RouteGQLController(
-    private val routeRepository: RouteRepository
+    private val routeRepository: RouteRepository,
+    private val routeNodeRepository: RouteNodeRepository
 ) {
     @QueryMapping
     fun getRoutes(
@@ -35,4 +41,27 @@ class RouteGQLController(
      */
     @QueryMapping
     fun getRoute(@Argument id: String) = run { id.route }
+
+    /**
+     * Route Nodes and Edges data class
+     */
+    data class RouteNodesAndEdges(
+        val nodes: List<RouteNode>,
+        val edges: List<RouteEdge>
+    )
+
+    /**
+     * Get Route nodes and edges by route identifier
+     */
+    @QueryMapping
+    fun getRouteNodesAndEdges(@Argument id: String) = run {
+        val edges = id.route.id.routeEdges
+        val nodes = routeNodeRepository.findAllByIdIn(edges.flatMap { listOf(it.nodeFrom, it.nodeTo) }.toSet())
+        println("nodes: $nodes")
+        println("edges: $edges")
+        RouteNodesAndEdges(
+            nodes,
+            edges
+        )
+    }
 }
