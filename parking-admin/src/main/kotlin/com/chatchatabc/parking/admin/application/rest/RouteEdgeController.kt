0924513eb024bbb4b84e31915246a1d398840193
Raphile14 <rllddalangin@gmail.com>
2023-06-22 10:43:19 +0800

    refactor(admin): used mappers instead and fixed some bugs and implemented updateEdge

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/RouteEdgeController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/RouteEdgeController.kt
@@ -1,33 +1,34 @@
 package com.chatchatabc.parking.admin.application.rest
 
+import com.chatchatabc.parking.admin.application.mapper.RouteEdgeMapper
 import com.chatchatabc.parking.domain.model.RouteEdge
+import com.chatchatabc.parking.domain.routeEdge
 import com.chatchatabc.parking.domain.routeNode
 import com.chatchatabc.parking.domain.service.RouteEdgeService
 import com.chatchatabc.parking.web.common.application.toErrorResponse
 import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.mapstruct.factory.Mappers
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/route-edge")
 class RouteEdgeController(
     private val routeEdgeService: RouteEdgeService,
 ) {
+    private val routeEdgeMapper = Mappers.getMapper(RouteEdgeMapper::class.java)
 
     /**
      * Create Route Edges data class
      */
-    data class RouteEdgeCreateRequest(
-        val edges: List<RouteEdges>
+    data class RouteEdgesCreateRequest(
+        val edges: List<RouteEdgeCreateRequest>
     )
 
     /**
      * Route Edges data class
      */
-    data class RouteEdges(
+    data class RouteEdgeCreateRequest(
         val routeId: Long,
         val nodeFrom: Long,
         val nodeTo: Long
@@ -42,19 +43,16 @@ class RouteEdgeController(
     )
     @PostMapping
     fun createEdge(
-        @RequestBody request: RouteEdges,
+        @RequestBody request: RouteEdgeCreateRequest,
     ) = runCatching {
-        routeEdgeService.saveRouteEdge(
-            RouteEdge().apply {
-                this.routeId = request.routeId
-                this.nodeFrom = request.nodeFrom
-                this.nodeTo = request.nodeTo
-                this.distance = routeEdgeService.calculateDistanceBetweenNodes(
-                    request.nodeFrom.routeNode,
-                    request.nodeTo.routeNode
-                )
-            }
-        )
+        val edge = RouteEdge().apply {
+            this.distance = routeEdgeService.calculateDistanceBetweenNodes(
+                request.nodeFrom.routeNode,
+                request.nodeTo.routeNode
+            )
+        }
+        routeEdgeMapper.createRouteEdgeFromCreateRequest(request, edge)
+        routeEdgeService.saveRouteEdge(edge)
     }.getOrElse { it.toErrorResponse() }
 
     /**
@@ -66,20 +64,50 @@ class RouteEdgeController(
     )
     @PostMapping("/many")
     fun createEdges(
-        @RequestBody request: RouteEdgeCreateRequest,
+        @RequestBody request: RouteEdgesCreateRequest,
     ) = runCatching {
         routeEdgeService.saveRouteEdges(
             request.edges.map {
-                RouteEdge().apply {
-                    this.routeId = it.routeId
-                    this.nodeFrom = it.nodeFrom
-                    this.nodeTo = it.nodeTo
+                val edge = RouteEdge().apply {
                     this.distance = routeEdgeService.calculateDistanceBetweenNodes(
                         it.nodeFrom.routeNode,
                         it.nodeTo.routeNode
                     )
                 }
+                routeEdgeMapper.createRouteEdgeFromCreateRequest(it, edge)
+                edge
             }
         ).toResponse()
+    }.getOrElse { it.toErrorResponse() }
+
+    /**
+     * Update Route Edges data class
+     */
+    data class RouteEdgeUpdateRequest(
+        val routeId: Long?,
+        val nodeFrom: Long?,
+        val nodeTo: Long?
+    )
+
+    /**
+     * Update Route Edge
+     */
+    @Operation(
+        summary = "Update Route Edge",
+        description = "Update Route Edge"
+    )
+    @PutMapping("{id}")
+    fun updateEdge(
+        @RequestBody request: RouteEdgeUpdateRequest,
+        @PathVariable id: Long,
+    ) = runCatching {
+        val edge = id.routeEdge.apply {
+            this.distance = routeEdgeService.calculateDistanceBetweenNodes(
+                request.nodeFrom?.routeNode,
+                request.nodeTo?.routeNode
+            )
+        }
+        routeEdgeMapper.updateRouteEdgeFromUpdateRequest(request, edge)
+        routeEdgeService.saveRouteEdge(edge).toResponse()
     }.getOrElse { it.toErrorResponse() }
 }
