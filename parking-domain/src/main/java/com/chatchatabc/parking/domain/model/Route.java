de094566c722e7ef37d85b467cbf2715ae3bb0ff
Raphile14 <rllddalangin@gmail.com>
2023-06-22 10:43:49 +0800

    chore(domain): removed unused import

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/Route.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/Route.java
@@ -1,6 +1,5 @@
 package com.chatchatabc.parking.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
