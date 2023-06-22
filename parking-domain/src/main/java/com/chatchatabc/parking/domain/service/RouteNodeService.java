22f1165d42c89746a12ff27feade9bfc190ba618
Raphile14 <rllddalangin@gmail.com>
2023-06-22 09:18:36 +0800

    feat(domain): implemented saveRouteNodes

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/service/RouteNodeService.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/service/RouteNodeService.java
@@ -1,4 +1,15 @@
 package com.chatchatabc.parking.domain.service;
 
+import com.chatchatabc.parking.domain.model.RouteNode;
+
+import java.util.List;
+
 public interface RouteNodeService {
+
+    /**
+     * Save route nodes
+     *
+     * @param routeNodes route nodes
+     */
+    void saveRouteNodes(List<RouteNode> routeNodes);
 }
--- parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/RouteNodeServiceImpl.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/impl/domain/service/RouteNodeServiceImpl.java
@@ -1,8 +1,27 @@
 package com.chatchatabc.parking.impl.domain.service;
 
+import com.chatchatabc.parking.domain.model.RouteNode;
+import com.chatchatabc.parking.domain.repository.RouteNodeRepository;
 import com.chatchatabc.parking.domain.service.RouteNodeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RouteNodeServiceImpl implements RouteNodeService {
+    private final RouteNodeRepository routeNodeRepository;
+
+    public RouteNodeServiceImpl(RouteNodeRepository routeNodeRepository) {
+        this.routeNodeRepository = routeNodeRepository;
+    }
+
+    /**
+     * Save route nodes
+     *
+     * @param routeNodes route nodes
+     */
+    @Override
+    public void saveRouteNodes(List<RouteNode> routeNodes) {
+        routeNodeRepository.saveAll(routeNodes);
+    }
 }
