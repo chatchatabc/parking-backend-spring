36efea87c1557a5d035f653332345086d12844f9
Raphile14 <rllddalangin@gmail.com>
2023-06-22 10:42:57 +0800

    refactor(admin): used mappers instead and fixed some bugs

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/RouteNodeController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/RouteNodeController.kt
@@ -1,10 +1,13 @@
 package com.chatchatabc.parking.admin.application.rest
 
+import com.chatchatabc.parking.admin.application.mapper.RouteNodeMapper
 import com.chatchatabc.parking.domain.model.RouteNode
+import com.chatchatabc.parking.domain.routeNode
 import com.chatchatabc.parking.domain.service.RouteNodeService
 import com.chatchatabc.parking.web.common.application.toErrorResponse
 import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
+import org.mapstruct.factory.Mappers
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,18 +15,19 @@ import org.springframework.web.bind.annotation.*
 class RouteNodeController(
     private val routeNodeService: RouteNodeService
 ) {
+    private val routeNodeMapper = Mappers.getMapper(RouteNodeMapper::class.java)
 
     /**
      * Create Route Nodes data class
      */
-    data class RouteNodeCreateRequest(
-        val nodes: List<RouteNodes>
+    data class RouteNodesCreateRequest(
+        val nodes: List<RouteNodeCreateRequest>
     )
 
     /**
      * Route Node data class
      */
-    data class RouteNodes(
+    data class RouteNodeCreateRequest(
         val latitude: Double,
         val longitude: Double,
         val poi: String
@@ -38,14 +42,11 @@ class RouteNodeController(
     )
     @PostMapping
     fun createNode(
-        @RequestBody request: RouteNode,
+        @RequestBody request: RouteNodeCreateRequest,
     ) = runCatching {
-        val routeNode = RouteNode().apply {
-            this.latitude = request.latitude
-            this.longitude = request.longitude
-            this.poi = request.poi
-        }
-        routeNodeService.saveRouteNode(routeNode).toResponse()
+        val node = RouteNode()
+        routeNodeMapper.createRouteNodeFromCreateRequest(request, node)
+        routeNodeService.saveRouteNode(node).toResponse()
     }.getOrElse { it.toErrorResponse() }
 
     /**
@@ -57,17 +58,24 @@ class RouteNodeController(
     )
     @PostMapping("/many")
     fun createNodes(
-        @RequestBody request: RouteNodeCreateRequest,
+        @RequestBody request: RouteNodesCreateRequest,
     ) = runCatching {
         routeNodeService.saveRouteNodes(
             request.nodes.map {
-                RouteNode().apply {
-                    this.latitude = it.latitude
-                    this.longitude = it.longitude
-                    this.poi = it.poi
-                }
+                val node = RouteNode()
+                routeNodeMapper.createRouteNodeFromCreateRequest(it, node)
+                node
             }).toResponse()
     }.getOrElse { it.toErrorResponse() }
+
+    /**
+     * Update Route Node data class
+     */
+    data class RouteNodeUpdateRequest(
+        val latitude: Double?,
+        val longitude: Double?,
+        val poi: String?
+    )
 
     /**
      * Update Route Node
@@ -76,16 +84,13 @@ class RouteNodeController(
         summary = "Update Route Node",
         description = "Update Route Node"
     )
-    @PutMapping
+    @PutMapping("/{id}")
     fun updateNode(
-        @RequestBody request: RouteNode,
+        @RequestBody request: RouteNodeUpdateRequest,
+        @PathVariable id: Long,
     ) = runCatching {
-        val node = RouteNode().apply {
-            this.id = request.id
-            this.latitude = request.latitude
-            this.longitude = request.longitude
-            this.poi = request.poi
-        }
+        val node = id.routeNode
+        routeNodeMapper.updateRouteNodeFromUpdateRequest(request, node)
         routeNodeService.saveRouteNode(node).toResponse()
     }.getOrElse { it.toErrorResponse() }
 }
