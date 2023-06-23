7990db6d0b4227909fb9493a29659ac0f4c937e2
Raphile14 <rllddalangin@gmail.com>
2023-06-23 11:40:02 +0800

    feat(domain): implemented findClosestNode

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/RouteNodeRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/RouteNodeRepository.java
@@ -1,7 +1,10 @@
 package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.domain.model.RouteNode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +21,20 @@ public interface RouteNodeRepository extends JpaRepository<RouteNode, Long> {
      */
     List<RouteNode> findAllByIdIn(Set<Long> nodeIds);
 
+
+    /**
+     * Find the closest node using Haversine formula
+     *
+     * @param latitude  latitude
+     * @param longitude longitude
+     * @param pageable  pageable
+     * @return page of route nodes
+     */
+    @Query("""
+            SELECT n
+            FROM RouteNode n
+            ORDER BY (6371 * acos(cos(radians(:latitude)) * cos(radians(n.latitude)) * cos(radians(n.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(n.latitude))))
+            ASC
+            """)
+    Page<RouteNode> findClosestNode(double latitude, double longitude, Pageable pageable);
 }
