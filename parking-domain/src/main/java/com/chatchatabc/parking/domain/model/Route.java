066f2e5ae8f2ccc8d1779bfbfd96059af9be7386
Raphile14 <rllddalangin@gmail.com>
2023-06-29 13:26:23 +0800

    refactor(domain): removed Lombok annotations and adjusted Route.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/Route.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/Route.java
@@ -2,22 +2,14 @@ package com.chatchatabc.parking.domain.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
 @Entity
 @Table(name = "route")
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public class Route extends FlagEntity {
     public static class RouteStatus {
         public static final int INACTIVE = -1;
@@ -53,4 +45,91 @@ public class Route extends FlagEntity {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    public Route() {
+    }
+
+    public Route(Long id, String routeUuid, String slug, String name, String description, String color, Integer status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.routeUuid = routeUuid;
+        this.slug = slug;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRouteUuid() {
+        return routeUuid;
+    }
+
+    public void setRouteUuid(String routeUuid) {
+        this.routeUuid = routeUuid;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
