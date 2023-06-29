59372b3d792c662908ee4d8fcc04cbd83e011767
Raphile14 <rllddalangin@gmail.com>
2023-06-29 13:17:31 +0800

    refactor(domain): removed Lombok annotations and adjusted UserLogoutLog.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/log/UserLogoutLog.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/log/UserLogoutLog.java
@@ -2,18 +2,12 @@ package com.chatchatabc.parking.domain.model.log;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
-@Data
 @Entity
 @Table(name = "user_logout_log")
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserLogoutLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +29,55 @@ public class UserLogoutLog {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public UserLogoutLog() {
+    }
+
+    public UserLogoutLog(Long id, Long user, Integer type, String ipAddress, LocalDateTime createdAt) {
+        this.id = id;
+        this.user = user;
+        this.type = type;
+        this.ipAddress = ipAddress;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUser() {
+        return user;
+    }
+
+    public void setUser(Long user) {
+        this.user = user;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
