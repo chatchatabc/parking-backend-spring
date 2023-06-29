dbddac5a3ae25ad5ffc7bf73b0cebbaf3cefc9c9
Raphile14 <rllddalangin@gmail.com>
2023-06-29 13:21:53 +0800

    refactor(domain): removed Lombok annotations and adjusted JeepneyRide.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/JeepneyRide.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/JeepneyRide.java
@@ -1,21 +1,13 @@
 package com.chatchatabc.parking.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-@Data
 @Entity
 @Table(name = "jeepney_ride")
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public class JeepneyRide extends FlagEntity {
     public static class JeepneyRideStatus {
         public static final int INACTIVE = -1;
@@ -52,4 +44,100 @@ public class JeepneyRide extends FlagEntity {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    public JeepneyRide() {
+    }
+
+    public JeepneyRide(Long id, Long jeepneyId, Long routeId, Long driverId, LocalDateTime startAt, LocalDateTime endAt, Integer status, Integer totalPassengers, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.jeepneyId = jeepneyId;
+        this.routeId = routeId;
+        this.driverId = driverId;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.status = status;
+        this.totalPassengers = totalPassengers;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getJeepneyId() {
+        return jeepneyId;
+    }
+
+    public void setJeepneyId(Long jeepneyId) {
+        this.jeepneyId = jeepneyId;
+    }
+
+    public Long getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(Long routeId) {
+        this.routeId = routeId;
+    }
+
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(Long driverId) {
+        this.driverId = driverId;
+    }
+
+    public LocalDateTime getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(LocalDateTime startAt) {
+        this.startAt = startAt;
+    }
+
+    public LocalDateTime getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(LocalDateTime endAt) {
+        this.endAt = endAt;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getTotalPassengers() {
+        return totalPassengers;
+    }
+
+    public void setTotalPassengers(Integer totalPassengers) {
+        this.totalPassengers = totalPassengers;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
