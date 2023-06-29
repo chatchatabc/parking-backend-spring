536dbe77d654cd13e5072aec949fa1ae158314cb
Raphile14 <rllddalangin@gmail.com>
2023-06-29 13:27:10 +0800

    refactor(domain): removed Lombok annotations and adjusted RouteNode.java

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/model/RouteNode.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/RouteNode.java
@@ -1,18 +1,12 @@
 package com.chatchatabc.parking.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
-@Data
 @Entity
 @Table(name = "route_node")
-@AllArgsConstructor
-@NoArgsConstructor
 public class RouteNode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +23,55 @@ public class RouteNode {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public RouteNode() {
+    }
+
+    public RouteNode(Long id, Double latitude, Double longitude, String poi, LocalDateTime createdAt) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.poi = poi;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getPoi() {
+        return poi;
+    }
+
+    public void setPoi(String poi) {
+        this.poi = poi;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
