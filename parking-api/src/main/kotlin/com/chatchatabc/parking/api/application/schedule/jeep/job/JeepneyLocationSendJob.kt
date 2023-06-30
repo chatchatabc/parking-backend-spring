5e95d18ee5c6e8715d247616ba0d7ffe6f5bd867
Raphile14 <rllddalangin@gmail.com>
2023-06-30 17:44:11 +0800

    temp(api): temporarily changed the output JeepneyLocationSendJob.kt for Admin and Mobile test development

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/schedule/jeep/job/JeepneyLocationSendJob.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/schedule/jeep/job/JeepneyLocationSendJob.kt
@@ -1,5 +1,7 @@
 package com.chatchatabc.parking.api.application.schedule.jeep.job
 
+import com.chatchatabc.parking.domain.repository.JeepneyRepository
+import com.chatchatabc.parking.domain.route
 import com.chatchatabc.parking.web.common.application.NatsMessage
 import com.chatchatabc.parking.web.common.application.enums.NatsPayloadTypes
 import com.chatchatabc.parking.web.common.application.nats.NatsPayload
@@ -10,39 +12,70 @@ import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import kotlin.random.Random
 
 @Component
 class JeepneyLocationSendJob(
     private val natsConnection: Connection,
-    private val gpsRestService: GpsRestService
+    private val gpsRestService: GpsRestService,
+    private val jeepneyRepository: JeepneyRepository
 ) : Job {
     private val log = LoggerFactory.getLogger(this::class.java)
-
-    /**
-     * Send Jeepney Location to NATS channel
-     */
-    override fun execute(context: JobExecutionContext?): Unit = runCatching {
-        // API Call get location of jeepney via Azliot
-        val data = gpsRestService.carHomeTF
+    override fun execute(p0: JobExecutionContext?) = runCatching {
+        val jeepneys = jeepneyRepository.findAll()
         log.info("JeepneyLocationSendJob Executed")
 
-        // Send location to NATS channel
-        data.results.forEach { jeep ->
+        // Generate random location for testing, 7.110345,125.635304
+        // random latitude
+        val lat = Random.nextDouble(7.11, 7.12)
+        // random longitude
+        val lng = Random.nextDouble(125.63, 125.64)
+        val dir = Random.nextInt(0, 360)
+
+        jeepneys.forEach { jeep ->
             NatsMessage(
                 NatsPayloadTypes.JEEPNEY_LOCATION_UPDATE,
                 NatsPayload.JeepneyPayload(
-                    jeep.vkey,
-                    jeep.lat,
-                    jeep.lng,
-                    jeep.dir
+                    jeep.jeepneyUuid,
+                    lat,
+                    lng,
+                    dir
                 )
             )
                 .toJson().toByteArray().runCatching {
-                    natsConnection.publish("jeepney-${jeep.vkey}", this)
-                    natsConnection.publish("route-${jeep.groupName}", this)
+                    natsConnection.publish("jeepney-${jeep.jeepneyUuid}", this)
+                    natsConnection.publish("route-${jeep.routeUuid.route.slug}", this)
                 }
         }
     }.getOrElse { e ->
         log.error("Error in JeepneyLocationSendJob: ${e.message}")
     }
+
+//    /**
+//     * Send Jeepney Location to NATS channel
+//     */
+//    override fun execute(context: JobExecutionContext?): Unit = runCatching {
+//        // API Call get location of jeepney via Azliot
+//        val data = gpsRestService.carHomeTF
+//        log.info("JeepneyLocationSendJob Executed")
+//
+//        // Send location to NATS channel
+//        data.results.forEach { jeep ->
+//            NatsMessage(
+//                NatsPayloadTypes.JEEPNEY_LOCATION_UPDATE,
+//                NatsPayload.JeepneyPayload(
+//                    jeep.vkey,
+//                    jeep.lat,
+//                    jeep.lng,
+//                    jeep.dir
+//                )
+//            )
+//                .toJson().toByteArray().runCatching {
+//                    natsConnection.publish("jeepney-${jeep.vkey}", this)
+//                    natsConnection.publish("route-${jeep.groupName}", this)
+//                }
+//        }
+//    }.getOrElse { e ->
+//        log.error("Error in JeepneyLocationSendJob: ${e.message}")
+//    }
 }
