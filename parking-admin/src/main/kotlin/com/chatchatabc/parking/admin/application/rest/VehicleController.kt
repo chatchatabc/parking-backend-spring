d30d63057f6f7b3fbbcf5ec0710c8fbecb76717c
Raphile14 <rllddalangin@gmail.com>
2023-07-04 17:00:53 +0800

    feat(admin): implemented getVehicleImage endpoint

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleController.kt
@@ -2,12 +2,15 @@ package com.chatchatabc.parking.admin.application.rest
 
 import com.chatchatabc.parking.admin.application.mapper.VehicleMapper
 import com.chatchatabc.parking.domain.model.Vehicle
+import com.chatchatabc.parking.domain.model.file.CloudFile
 import com.chatchatabc.parking.domain.service.VehicleService
 import com.chatchatabc.parking.domain.user
 import com.chatchatabc.parking.domain.vehicle
+import com.chatchatabc.parking.infra.service.FileStorageService
 import com.chatchatabc.parking.web.common.application.toErrorResponse
 import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
+import jakarta.servlet.http.HttpServletResponse
 import org.mapstruct.factory.Mappers
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
@@ -16,7 +19,8 @@ import java.time.LocalDateTime
 @RestController
 @RequestMapping("/api/vehicle")
 class VehicleController(
-    private val vehicleService: VehicleService
+    private val vehicleService: VehicleService,
+    private val fileStorageService: FileStorageService
 ) {
     private val vehicleMapper = Mappers.getMapper(VehicleMapper::class.java)
 
@@ -99,4 +103,39 @@ class VehicleController(
         vehicleMapper.mapRequestToVehicle(req, vehicle)
         vehicleService.updateVehicle(vehicle).toResponse()
     }.getOrElse { it.toErrorResponse() }
+
+    /**
+     * Get Vehicle Image
+     */
+    @Operation(
+        summary = "Get Vehicle Image",
+        description = "Get Vehicle Image. Side value: Front = 1, Back = 2, Left = 3, Right = 4"
+    )
+    @GetMapping("/image/{id}/{side}")
+    fun getVehicleImage(
+        @PathVariable id: String,
+        @PathVariable side: Int,
+        principal: Principal,
+        response: HttpServletResponse
+    ) = runCatching {
+        val vehicle = id.vehicle
+        var cloudFile: CloudFile? = null
+
+        when (side) {
+            Vehicle.VehicleImageType.FRONT -> cloudFile = vehicle.imageFront
+            Vehicle.VehicleImageType.BACK -> cloudFile = vehicle.imageBack
+            Vehicle.VehicleImageType.LEFT -> cloudFile = vehicle.imageLeft
+            Vehicle.VehicleImageType.RIGHT -> cloudFile = vehicle.imageRight
+        }
+
+        if (cloudFile == null) {
+            throw Exception("Vehicle image not found")
+        }
+        response.contentType = cloudFile.mimeType
+        // Add 1 day cache
+        response.setHeader("Cache-Control", "max-age=86400")
+        val inputStream = fileStorageService.downloadFile(cloudFile.key)
+        inputStream.copyTo(response.outputStream)
+        response.flushBuffer()
+    }.getOrElse { response.sendError(HttpServletResponse.SC_NOT_FOUND) }
 }
