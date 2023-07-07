3715a3bad931f99c2db06c8e8b997a68af63495e
Raphile14 <rllddalangin@gmail.com>
2023-07-07 14:07:06 +0800

    feat(domain): implemented findAllByReport

--- parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ReportStatusRepository.java
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/repository/ReportStatusRepository.java
@@ -1,9 +1,20 @@
 package com.chatchatabc.parking.domain.repository;
 
 import com.chatchatabc.parking.domain.model.ReportStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportStatusRepository extends JpaRepository<ReportStatus, String> {
+
+    /**
+     * Find all by report
+     *
+     * @param report   report
+     * @param pageable pageable
+     * @return page of report status
+     */
+    Page<ReportStatus> findAllByReport(Long report, Pageable pageable);
 }
