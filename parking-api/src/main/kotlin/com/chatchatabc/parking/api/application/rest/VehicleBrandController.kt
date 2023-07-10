cda884766aa76fd0e46f70785a8a574efef8f331
Raphile14 <rllddalangin@gmail.com>
2023-07-10 13:53:44 +0800

    feat(api): implemented getVehicleBrandLogo api

--- parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleBrandController.kt
+++ parking-api/src/main/kotlin/com/chatchatabc/parking/api/application/rest/VehicleBrandController.kt
@@ -2,8 +2,10 @@ package com.chatchatabc.parking.api.application.rest
 
 import com.chatchatabc.parking.domain.repository.VehicleBrandRepository
 import com.chatchatabc.parking.domain.vehicleBrand
+import com.chatchatabc.parking.infra.service.FileStorageService
 import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/vehicle-brand")
 class VehicleBrandController(
-    private val vehicleBrandRepository: VehicleBrandRepository
+    private val vehicleBrandRepository: VehicleBrandRepository,
+    private val fileStorageService: FileStorageService
 ) {
     /**
      * Get vehicle brands
@@ -34,4 +37,25 @@ class VehicleBrandController(
     )
     @GetMapping("/{id}")
     fun get(@PathVariable id: String) = run { id.vehicleBrand.toResponse() }
+
+    /**
+     * Get Vehicle Brand Logo
+     */
+    @Operation(
+        summary = "Get Vehicle Brand Logo",
+        description = "Get Vehicle Brand Logo"
+    )
+    @GetMapping("/logo/{id}")
+    fun getVehicleBrandLogo(
+        @PathVariable id: String,
+        response: HttpServletResponse
+    ) = runCatching {
+        val vehicleBrand = id.vehicleBrand
+        // Add 1 day cache
+        response.setHeader("Cache-Control", "max-age=86400")
+        response.contentType = vehicleBrand.logo.mimeType
+        val inputStream = fileStorageService.downloadFile(vehicleBrand.logo.key)
+        inputStream.copyTo(response.outputStream)
+        response.flushBuffer()
+    }.getOrElse { response.sendError(HttpServletResponse.SC_NOT_FOUND) }
 }
