ca7eec70009affb8acbcbbaaa4012f0863539b95
Raphile14 <rllddalangin@gmail.com>
2023-07-12 14:02:39 +0800

    feat(admin): implemented getVehicleTypes and getVehicleType

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleTypeController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleTypeController.kt
@@ -2,21 +2,54 @@ package com.chatchatabc.parking.admin.application.rest
 
 import com.chatchatabc.parking.admin.application.mapper.VehicleTypeMapper
 import com.chatchatabc.parking.domain.model.VehicleType
+import com.chatchatabc.parking.domain.repository.VehicleTypeRepository
 import com.chatchatabc.parking.domain.service.VehicleTypeService
+import com.chatchatabc.parking.domain.specification.VehicleTypeSpecification
 import com.chatchatabc.parking.domain.user
 import com.chatchatabc.parking.domain.vehicleType
 import com.chatchatabc.parking.web.common.application.toErrorResponse
+import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
 import org.mapstruct.factory.Mappers
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 
 @RestController
 @RequestMapping("/api/vehicle-type")
 class VehicleTypeController(
-    private val vehicleTypeService: VehicleTypeService
+    private val vehicleTypeService: VehicleTypeService,
+    private val vehicleTypeRepository: VehicleTypeRepository
 ) {
     private val vehicleTypeMapper = Mappers.getMapper(VehicleTypeMapper::class.java)
+
+    /**
+     * Get Vehicle Types
+     */
+    @GetMapping
+    fun getVehicleTypes(
+        pageable: Pageable,
+        @RequestParam params: Map<String, String>
+    ) = run {
+        val spec = VehicleTypeSpecification()
+            .withParams(params)
+
+        // Search with Keyword
+        if (params.containsKey("keyword")) {
+            val keyword = params["keyword"]
+            spec.or(VehicleTypeSpecification().withKeyword(keyword))
+        }
+
+        vehicleTypeRepository.findAll(spec, pageable).toResponse()
+    }
+
+    /**
+     * Get Vehicle Type by Identifier
+     */
+    @GetMapping("/{id}")
+    fun getVehicleType(
+        @PathVariable id: String
+    ) = runCatching { id.vehicleType.toResponse() }.getOrElse { it.toErrorResponse() }
 
     /**
      * Admin create Vehicle Type
@@ -34,7 +67,7 @@ class VehicleTypeController(
             this.createdBy = principal.name.user.id
         }
         vehicleTypeMapper.mapRequestToVehicleType(req, createdVehicleType)
-        vehicleTypeService.saveVehicleType(createdVehicleType)
+        vehicleTypeService.saveVehicleType(createdVehicleType).toResponse()
     }.getOrElse { it.toErrorResponse() }
 
     /**
@@ -52,6 +85,6 @@ class VehicleTypeController(
     ) = runCatching {
         val updatedVehicleType = id.vehicleType
         vehicleTypeMapper.mapRequestToVehicleType(req, updatedVehicleType)
-        vehicleTypeService.saveVehicleType(updatedVehicleType)
+        vehicleTypeService.saveVehicleType(updatedVehicleType).toResponse()
     }.getOrElse { it.toErrorResponse() }
 }
