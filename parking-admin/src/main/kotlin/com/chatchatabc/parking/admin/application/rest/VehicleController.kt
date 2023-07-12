e857e87e168593c2abb819883fb883072ffb7609
Raphile14 <rllddalangin@gmail.com>
2023-07-12 15:01:16 +0800

    feat(admin): implemented getVehicle, getVehicles, and getVehiclesByOwner APIs

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleController.kt
@@ -3,7 +3,9 @@ package com.chatchatabc.parking.admin.application.rest
 import com.chatchatabc.parking.admin.application.mapper.VehicleMapper
 import com.chatchatabc.parking.domain.model.Vehicle
 import com.chatchatabc.parking.domain.model.file.CloudFile
+import com.chatchatabc.parking.domain.repository.VehicleRepository
 import com.chatchatabc.parking.domain.service.VehicleService
+import com.chatchatabc.parking.domain.specification.VehicleSpecification
 import com.chatchatabc.parking.domain.user
 import com.chatchatabc.parking.domain.vehicle
 import com.chatchatabc.parking.infra.service.FileStorageService
@@ -12,6 +14,7 @@ import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.HttpServletResponse
 import org.mapstruct.factory.Mappers
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 import java.time.LocalDateTime
@@ -20,9 +23,69 @@ import java.time.LocalDateTime
 @RequestMapping("/api/vehicle")
 class VehicleController(
     private val vehicleService: VehicleService,
-    private val fileStorageService: FileStorageService
+    private val fileStorageService: FileStorageService,
+    private val vehicleRepository: VehicleRepository
 ) {
     private val vehicleMapper = Mappers.getMapper(VehicleMapper::class.java)
+
+    /**
+     * Get Vehicle by identifier
+     */
+    @Operation(
+        summary = "Get Vehicle",
+        description = "Get Vehicle"
+    )
+    @GetMapping("/{id}")
+    fun getVehicle(@PathVariable id: String) = id.vehicle.toResponse()
+
+    /**
+     * Get Vehicles
+     */
+    @Operation(
+        summary = "Get Vehicles",
+        description = "Get Vehicles"
+    )
+    @GetMapping
+    fun getVehicles(
+        pageable: Pageable,
+        @RequestParam params: Map<String, String>
+    ) = run {
+        val spec = VehicleSpecification()
+            .withParams(params)
+
+        // Search with Keyword
+        if (params.containsKey("keyword")) {
+            val keyword = params["keyword"]
+            spec.or(VehicleSpecification().withKeyword(keyword))
+        }
+
+        vehicleRepository.findAll(spec, pageable).toResponse()
+    }
+
+    /**
+     * Get Vehicles by Owner
+     */
+    @Operation(
+        summary = "Get Vehicles by Owner",
+        description = "Get Vehicles by Owner"
+    )
+    @GetMapping("/owner/{id}")
+    fun getVehiclesByOwner(
+        @PathVariable id: String,
+        pageable: Pageable
+    ) = run {
+        vehicleRepository.findAllByOwner(id.user.id, pageable).toResponse()
+    }
+
+    /**
+     * Get Owner by Vehicle identifier
+     */
+    @Operation(
+        summary = "Get Owner by Vehicle",
+        description = "Get Owner by Vehicle"
+    )
+    @GetMapping("/vehicle-user/{id}")
+    fun getUserByVehicle(@PathVariable id: String) = run { id.vehicle.owner.user.toResponse() }
 
     /**
      * Request to register a vehicle data class
