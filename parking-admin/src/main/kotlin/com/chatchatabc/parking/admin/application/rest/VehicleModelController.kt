241098bd0156037a24f0d3188d129656dbcaf945
Raphile14 <rllddalangin@gmail.com>
2023-07-12 15:31:08 +0800

    feat(admin): implemented getVehicleModels, getVehicleModel, and getVehicleModelByBrandId APIs

--- parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleModelController.kt
+++ parking-admin/src/main/kotlin/com/chatchatabc/parking/admin/application/rest/VehicleModelController.kt
@@ -2,21 +2,62 @@ package com.chatchatabc.parking.admin.application.rest
 
 import com.chatchatabc.parking.admin.application.mapper.VehicleModelMapper
 import com.chatchatabc.parking.domain.model.VehicleModel
+import com.chatchatabc.parking.domain.repository.VehicleModelRepository
 import com.chatchatabc.parking.domain.service.VehicleModelService
+import com.chatchatabc.parking.domain.specification.VehicleModelSpecification
 import com.chatchatabc.parking.domain.user
+import com.chatchatabc.parking.domain.vehicleBrand
 import com.chatchatabc.parking.domain.vehicleModel
 import com.chatchatabc.parking.web.common.application.toErrorResponse
+import com.chatchatabc.parking.web.common.application.toResponse
 import io.swagger.v3.oas.annotations.Operation
 import org.mapstruct.factory.Mappers
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 
 @RestController
 @RequestMapping("/api/vehicle-model")
 class VehicleModelController(
-    private val vehicleModelService: VehicleModelService
+    private val vehicleModelService: VehicleModelService,
+    private val vehicleModelRepository: VehicleModelRepository
 ) {
     private val vehicleModelMapper = Mappers.getMapper(VehicleModelMapper::class.java)
+
+    /**
+     * Get Vehicle Models
+     */
+    @GetMapping
+    fun getVehicleModels(
+        pageable: Pageable,
+        @RequestParam params: Map<String, String>
+    ) = run {
+        val spec = VehicleModelSpecification()
+            .withParams(params)
+
+        // Search with Keyword
+        if (params.containsKey("keyword")) {
+            val keyword = params["keyword"]
+            spec.or(VehicleModelSpecification().withKeyword(keyword))
+        }
+
+        vehicleModelRepository.findAll(spec, pageable).toResponse()
+    }
+
+    /**
+     * Get Vehicle Model by Identifier
+     */
+    @GetMapping("/{id}")
+    fun getVehicleModel(@PathVariable id: String) = run { id.vehicleModel.toResponse() }
+
+    /**
+     * Get Vehicle Model by Brand Identifier
+     */
+    @GetMapping("/brand/{id}")
+    fun getVehicleModelByBrandId(
+        @PathVariable id: String,
+        pageable: Pageable
+    ) = run { vehicleModelRepository.findAllByBrandUuid(id.vehicleBrand.brandUuid, pageable).toResponse() }
 
     /**
      * Admin create Vehicle Model
